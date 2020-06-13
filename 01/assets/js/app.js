56a2880544409aeb79a2774aed580c14fff487cf
Martyna Kiljan <kmartynak@icloud.com>
2020-06-13 17:02:41 +0200

    zadanie1_glowneZadaniaJS

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -1,4 +1,42 @@
 const a = '4.2';
 const b = 9;
 
-console.log(a, b);
+// console.log(a, b);
+
+
+
+console.log(typeof a)
+console.log(typeof b)
+
+
+
+const add = Number(a) + b;
+console.log(add)
+
+const multiplication = Number(a) * b;
+console.log(multiplication.toFixed(2))
+
+const divide = Number(a) / b;
+console.log(divide.toFixed(2))
+
+const modulo = Number(a) % b;
+console.log(modulo)
+
+
+const checkNumbers = function () {
+    const checkAdd = add > 20 ? 'wynik add jest większy od 20' : 'wynik add jest mniejszy od 20';
+    console.log(checkAdd)
+
+    const checkMultiplication = multiplication > 20 ? 'wynik multiplication  jest większy od 20' : 'wynik multiplication jest mniejszy od 20';
+    console.log(checkMultiplication)
+
+    const checkDivide = divide > 20 ? 'wynik divide  jest większy od 20' : 'wynik divide  jest mniejszy od 20';
+    console.log(checkDivide)
+
+    const checkModulo = modulo > 20 ? 'wynik modulo jest większy od 20' : 'wynik modulo jest mniejszy od 20';
+    console.log(checkModulo)
+
+}
+
+
+checkNumbers()
