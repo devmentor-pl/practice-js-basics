5a1f0684b167ae7f8373cfa47a1d1194b91f490e
Martyna Kiljan <kmartynak@icloud.com>
2020-06-29 15:22:39 +0200

    poprawione_5

--- 05/app.js
+++ 05/app.js
@@ -5,6 +5,9 @@ function Student(firstNameValue, lastNameValue) {
 }
 
 Student.prototype.addGrade = function (subject, grade) {
+
+
+
     //console.log(this.firstName, this.lastName);  
     if (typeof this.grades[subject] === 'undefined') {
         this.grades[subject] = [];
@@ -17,29 +20,32 @@ Student.prototype.addGrade = function (subject, grade) {
 
 Student.prototype.getAverageGrade = function (subject) {
 
-    if (typeof grades === 'undefined') {
-        return 0;
 
 
-    } else if (typeof subject === 'undefined') {
+
+    if (typeof subject === 'undefined') {
         // trzeba napisać srendia dla wszystkich
         // można wyukorzystać for in
 
         let sumSubjects = 0;
+        let numbers = 0;
 
         for (const key in this.grades) {
 
             const array = this.grades[key];
-
-            const result = array.reduce((a, b) => a + b);
-            const avarage = result / array.length;
-
-            sumSubjects += avarage;
-            console.log(avarage)
+            sumSubjects += array.reduce((a, b) => a + b);
+            numbers += array.length;
+            numbers++;
+            const resultAll = sumSubjects / numbers;
+            console.log(`Średnia ze wszystkich przedmiotów wynosi : ${resultAll}`)
+            return resultAll;
         }
+
+
+
+
+
     } else {
-
-
         const grades = this.grades[subject];
 
         // const sum = grades.reduce(function(acc, grade) {
@@ -57,22 +63,32 @@ Student.prototype.getAverageGrade = function (subject) {
         console.log(`średnia z ${subject} wynosi ${avgSubject}`)
 
 
+        if (typeof grades === 'undefined') {
+            return 0;
+        }
+        return avgSubject;
     }
 
-    return 0;
 }
+
+
+
+
 
 
 const student2 = new Student('Andrzej', 'Nowak');
 // console.log(student0, student1, student2);
 
 student2.addGrade('maths', 4);
-student2.addGrade('maths', 2);
+student2.addGrade('maths', 5);
 
 
-student2.addGrade('polish', 5);
-student2.addGrade('polish', 5);
-student2.addGrade('polish', 5);
+student2.addGrade('polish', 2);
+student2.addGrade('polish', 2);
+student2.addGrade('polish', 2);
 
 student2.getAverageGrade('maths');
 student2.getAverageGrade('polish');
+
+const studentAvg = student2.getAverageGrade();
+// console.log(studentAvg)
