59c4c082b8097ab312e27ae3c02095e751f3732e
jakubsmiarowski <48868566+jakubsmiarowski@users.noreply.github.com>
2020-07-03 20:04:36 +0200

    task 4 without slice, doesn't work

---
+++ 04/app.js
@@ -0,0 +1,24 @@
+const createArray = (min, max) => {
+    const randomNumbers = [];
+    for (let i = 1; i <= 20; i++) {
+        randomNumbers.push(Math.round((Math.random() * max - min) + min));
+    }
+    return randomNumbers;
+}
+
+const array = createArray(1, 100);
+console.log(array);
+
+const getLargest = arr => {
+    let n = 10;
+    arr.sort((a, b) => b - a);
+    arr.slice(0, n);
+    return arr;
+}
+const largest = getLargest(array);
+console.log(largest);
+
+let getAvg = arr => arr.reduce((a, b) => a + b) / arr.length;
+
+const avg = getAvg(largest);
+console.log(avg);
