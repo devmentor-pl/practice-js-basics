cf1dd90506086eab3a7d4e2bf5a625c312bee6a2
jakubsmiarowski <48868566+jakubsmiarowski@users.noreply.github.com>
2020-07-03 15:32:59 +0200

    task 1 done

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -1,4 +1,23 @@
 const a = '4.2';
 const b = 9;
+let c = 30
 
-console.log(a, b);
+console.log(typeof a, typeof b);
+
+const sum = a + b;
+const concat = b + 'Kuba';
+const diff = a - b;
+const multiply = a * b;
+const divide = a / b;
+const rest = b % 2;
+const increment = c++;
+const decrement = c--;
+
+console.log(sum > 20)
+console.log(concat > 20)
+console.log(diff > 20)
+console.log(multiply > 20)
+console.log(divide > 20)
+console.log(rest > 20)
+console.log(increment > 20)
+console.log(decrement > 20)
