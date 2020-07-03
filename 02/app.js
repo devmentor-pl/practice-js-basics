3cbd5ef0c88c00cdf1625e51b8cc5344ad244aeb
jakubsmiarowski <48868566+jakubsmiarowski@users.noreply.github.com>
2020-07-03 15:43:20 +0200

    task 2 done

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,20 @@
-
 /* rozwiązanie z pętlą for */
-const x = 4;
+const x = prompt('Podaj liczbę');
+if (x => 1 && x <= 9) {
+    for (let i = 1; i <= 9; i++) {
+        const result = i * x;
+        console.log(result)
+    }
+} else {
+    alert('Liczba musi być w przedziale od 1 do 9');
+}
 
 
 
 /* rozwiązanie z pętlą while  */
+
+const a = 3;
+const n = prompt('Podaj wykładnik');
+
+const power = Math.pow(x, n);
+console.log(power);
