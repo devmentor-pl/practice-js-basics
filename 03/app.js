a1e03a676ad8d5f7dc13346d358a5b2767a09634
AnnaBuller <aniabela@wp.pl>
2020-09-11 13:45:53 +0200

    solve 03 task

--- 03/app.js
+++ 03/app.js
@@ -5,14 +5,38 @@ const a = randomNumber(min, max);
 const b = randomNumber(min, max);
 const c = randomNumber(min, max);
 
-console.log(a, b, c);
+function getSum(x, y, z) {
+    x = parseInt(x);
+    y = parseInt(y);
+    z = parseInt(z);
+    const nums = [x, y, z].sort((a, b) => b - a);
+    return nums[0] + nums[1]
+}
 
+const isEven = (num) => {
+    if (typeof num !== 'number') {
+        return null
+    } else if (num % 2 === 0) {
+        return true
+    } else { return false }
+}
 
-
-
-
-
+function showInfo(toCheck, checker) {
+    switch (checker) {
+        case null: console.log(`Podany argument ${toCheck} nie jest liczbą`);
+            break;
+        case true: console.log(`Podany argument ${toCheck} jest parzysty`);
+            break;
+        case false: console.log(`Podany argument ${toCheck} jest nieparzysty`)
+    }
+}
 
 function randomNumber(min, max) {
     return Math.round((Math.random() * max - min) + min);
 }
+
+const sum = getSum(a, b, c);
+const even = isEven(sum);
+
+console.log(`Suma 2 największych liczb spośród: ${a}, ${b} i ${c} to ${sum}`);
+showInfo(sum, even);
