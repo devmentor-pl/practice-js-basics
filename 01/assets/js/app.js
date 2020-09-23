26c959d67bcf68614356a4b7ac4252a6b748594b
Grzegorz Błaszczyk <ktotok@gmail.com>
2020-09-23 20:52:35 +0200

    operators

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -1,4 +1,19 @@
 const a = '4.2';
 const b = 9;
+const numA = parseFloat(a);
 
 console.log(a, b);
+console.log(typeof a);
+console.log(typeof b);
+const adding = numA + b;
+console.log(adding);
+const substraction = numA - b;
+console.log(substraction);
+const multiplication = numA * b;
+console.log(multiplication);
+const division = numA / b;
+console.log(division);
+const exponentiation = numA ** b;
+console.log(exponentiation);
+const modulo = numA % b;
+console.log(modulo);
