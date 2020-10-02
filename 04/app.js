25a030b4b10da8cfbb5e2151c288bdda659afc4d
Grzegorz Błaszczyk <ktotok@gmail.com>
2020-10-02 08:15:57 +0200

    Done 4 task

--- 04/app.js
+++ 04/app.js
@@ -18,7 +18,7 @@ const arr = createArray(1, 100);
 function createArray(min, max) {
 var arr = [];
 for (var i=0, t=20,m = max,n = min; i<t; i++) {
-    arr.push(Math.round(Math.random() * m - n) + n);
+    arr.push(Math.round(Math.random() * (m - n)) + n);
     // nie wiem co zrobic, by losowalo od 1, probowalem z if oraz z math.random i dodawaniem badz odejmowanie w poszczegolnych miejscach nawiasu, ale nie dziala
 }
 console.log(arr);
@@ -26,7 +26,28 @@ return arr;
 }
 const largest = getLargest(arr);
 function getLargest(x) {
-    const y = x.sort();
+    const y = x.sort(function(a, b) {
+        return a - b;
+    })
     
-    return x;
+    console.log(y, 'y');
+    return y.slice(0, 10);
+}
+const numbers = [1, 2, 3, 4, 5];
+function getAvg(avg) {
+    const total = avg.reduce((acc, c) => acc + c, 0);
+    return total / avg.length;
+}
+const average = getAvg(numbers);
+console.log(average);
+
+function getAverage () {
+    const a = 10;
+    const b = 200;
+    const getArr = createArray(a, b);
+    console.log(getArr)
+    largestTen = getLargest(getArr);
+    const averageTen = getAvg(largestTen);
+    console.log(averageTen);
+    return averageTen;
 }
