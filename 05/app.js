bad47a691936914b550d4b2d2cb8659824a60137
Grzegorz Błaszczyk <ktotok@gmail.com>
2020-10-06 16:16:25 +0200

    update

--- 05/app.js
+++ 05/app.js
@@ -9,16 +9,24 @@ function Student(firstName, lastName) {
         this.grades[subject].push(grade);
         return this.grades;
     }
-    this.getAverageGrade = function(avg) {
-       
-        const total = avg.reduce((acc, c) => acc + c, 0);
-        return total / avg.length;
+    this.getAverageGrade = function(element, a) {
+        var sum = 0;
+        for( var i = 0; i < element[a].length; i++ ){
+            sum += parseInt( element[a][i], 10 );
+        }
+        return sum/element[a];
     }
     }
     
 
 
+
 const student1 = new Student('Grzegorz', 'Blaszczyk');
+student1.addGrade('maths', 4);
+student1.addGrade('maths', 6);
+student1.addGrade('english', 3);
+const avgMath = student1.getAverageGrade(this.grades,'math');
+console.log(avgMath)
 console.log(student1);
 /*function addGrade(subject, grade) {
     const grades = {subject, grade};
