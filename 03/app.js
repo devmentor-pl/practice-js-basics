aba52d21d2dc9eba1dcca2b5cb8d5615e7f380df
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2020-10-13 11:18:39 +0200

    Create 3 functions - Task 03

--- 03/app.js
+++ 03/app.js
@@ -7,10 +7,56 @@ const c = randomNumber(min, max);
 
 console.log(a, b, c);
 
+const sum = getSum(a, b, c);
 
 
+const isEven = function (sum) {
+    if (typeof (sum) !== 'number') {
+        return null;
+    } else if (sum % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const even = isEven(sum);
+showInfo(sum, even);
 
 
+function getSum(a, b, c) {
+    const numArray = [];
+    let sum = 0;
+
+    numArray.push(a);
+    numArray.push(b);
+    numArray.push(c);
+
+    const sortedNumArray = numArray.sort(function (a, b) {
+        return b - a;
+    })
+
+    sum = sortedNumArray[0] + sortedNumArray[1];
+    return sum;
+}
+
+
+function showInfo(sum, even) {
+    switch (even) {
+        case null:
+            console.log('Podany argument ' + sum + ' nie jest liczbą');
+            break;
+        case true:
+            console.log('Podany argument ' + sum + ' jest parzysty');
+            break;
+        case false:
+            console.log('Podany argument ' + sum + ' jest nieparzysty');
+            break;
+        default:
+            console.log('Nie wiem co to za argument: ' + sum);
+            break;
+    }
+}
 
 
 function randomNumber(min, max) {
