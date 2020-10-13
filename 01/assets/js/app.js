256dd5c0116afaf04760c65c7832104634ca41c1
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2020-10-13 06:50:21 +0200

    Link app.js to HTML, use basic arithmetic operators, show results higher than 20

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -1,4 +1,33 @@
 const a = '4.2';
 const b = 9;
 
+const results = [];
+
 console.log(a, b);
+console.log(typeof a, typeof b);
+
+
+const add = parseFloat(a) + b;
+results.push(add);
+
+const subtract = a - b;
+results.push(subtract);
+
+const multiply = a * b;
+results.push(multiply);
+
+const divide = a / b;
+results.push(divide);
+
+const modulo = a % b;
+results.push(modulo);
+
+console.log(results);
+
+const higherThan20Array = results.map(function (result) {
+    if (result > 20) {
+        return 'Większy od 20'
+    } else return 'Mniejszy od 20';
+})
+
+console.log(higherThan20Array);
