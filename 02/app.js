6ca09f317fd42a25b4fd2a3ab3bf169763637401
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2020-10-13 07:30:26 +0200

    Create multiply and pow function, create condition check - Task 02

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,50 @@
+const x = parseInt(prompt('Podaj liczbę całkowitą do mnożenia (1-9)'));
+const a = parseInt(prompt('Podaj podstawę potęgowania (1-9)'));
+const n = parseInt(prompt('Podaj wykładnik potęgowania'));
 
-/* rozwiązanie z pętlą for */
-const x = 4;
+checkX(x);
 
 
 
-/* rozwiązanie z pętlą while  */
+
+function multiplyX(num) {
+
+    /* rozwiązanie z pętlą for */
+    for (let i = 1; i <= num; i++) {
+        let result = 0;
+        result = i * num;
+        console.log(i + ' x ' + num + ' = ' + result);
+
+
+    }
+}
+
+
+function pow(a, n) {
+
+    /* rozwiązanie z pętlą while  */
+    const resultArray = [];
+
+    let j = 1;
+    let resultPow = 1;
+    while (j <= n) {
+        resultPow *= a;
+        resultArray.push(a);
+        j++;
+    }
+
+    console.log(resultArray.join(' * ') + " = " + resultPow);
+
+    return;
+}
+
+
+function checkX(num) {
+    if (!(num > 0 && num < 10) || !(a > 0 && a < 10)) {
+        alert('Podaj liczbę z przedziału od 1 do 9');
+        return;
+    } else {
+        multiplyX(num);
+        pow(a, n);
+    }
+}
