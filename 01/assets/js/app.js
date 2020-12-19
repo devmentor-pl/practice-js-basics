c069dd553b48949c26a2b82e420cb53cedc3753c
Ewa Skórska <ewaskorska94@gmail.com>
2020-12-19 12:02:12 +0000

    Add js ex 1

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -2,3 +2,13 @@ const a = '4.2';
 const b = 9;
 
 console.log(a, b);
+
+console.log(`Zmienna \'a\' jest typu ${typeof a}, a zmienna \'b\' typu ${typeof b}`);
+
+console.log(`${a} + ${b} = ${+a + b}`);
+console.log(`${a} - ${b} = ${+a - b}`);
+console.log(`${a} : ${b} = ${+a / b}`);
+console.log(`${a} * ${b} = ${+a * b}`);
+console.log(`${b} ^ ${a} = ${b ** +a}`);
+console.log(`${b} pierwiastek z ${a} = ${b ** ((+a)**(-1))}`);
+console.log(`${a} % ${b} = ${+a % b}`);
