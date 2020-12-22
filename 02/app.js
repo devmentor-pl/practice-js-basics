7d1b23fb17cca729943574fcd13b778d448090cd
Anna Kolodziej <kkolodziejanna@gmail.com>
2020-12-22 18:15:02 +0100

    Zad 02 petla for

--- 02/app.js
+++ 02/app.js
@@ -2,6 +2,12 @@
 /* rozwiązanie z pętlą for */
 const x = 4;
 
+for (i=0; i<10; i++) {
+
+        console.log('wynik mnożenia ' + i + ' przez 4, to ' + i * x);
+    
+}
+
 
 
 /* rozwiązanie z pętlą while  */
