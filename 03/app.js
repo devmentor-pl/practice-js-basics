dd1f9fd89b9b986e0f4e52b68fd2bd0c4776204d
Anna Kolodziej <kkolodziejanna@gmail.com>
2021-01-07 13:33:20 +0100

    Zad 03

--- 03/app.js
+++ 03/app.js
@@ -1,12 +1,11 @@
-const min = 1;
-const max = 100;
+// const min = 1;
+// const max = 100;
 
-const a = randomNumber(min, max);
-const b = randomNumber(min, max);
-const c = randomNumber(min, max);
+// const a = randomNumber(min, max);
+// const b = randomNumber(min, max);
+// const c = randomNumber(min, max);
 
-console.log(a, b, c);
-
+// console.log(a, b, c);
 
 function randomNumber(min, max) {
     return Math.round((Math.random() * (max - min)) + min);
@@ -14,14 +13,72 @@ function randomNumber(min, max) {
 
 
 function getSum(a, b, c) {
-    const aInt = Math.round(a);
-    const bInt = Math.round(b);
-    const cInt = Math.round(c);
+    const arr = [Math.round(a), Math.round(b), Math.round(c)];
+    arr.sort(function (a, b) {
+        return b - a;
+    });
 
-    //wybierz 2 najwieksze
-    const small =;
-    const large =;
-
-    if(a < b)
+    return arr[0] + arr[1];
 }
-getSum(1.1, 2.1, 3.2);
+getSum(4.1, 5.1, 3.2);
+
+// const aInt = Math.round(a);
+    // const bInt = Math.round(b);
+    // const cInt = Math.round(c);
+
+    // //wybierz 2 najwieksze
+    // let max1,max2;
+    // if(aInt > bInt) {
+    //     max1 = aInt;
+    //     if(bInt > cInt) {
+    //         max2 = bInt;
+    //     } else {
+    //         max2 = cInt;
+    //     }
+    // } else {
+    //     max1 = bInt;
+    //     if(aInt > cInt) {
+    //         max2 = aInt;
+    //     } else {
+    //         max2 = cInt;
+    //     }
+    // }
+
+    // console.log(max1, max2);
+
+
+const isEven = function (num) {
+    if (typeof num !== 'number') {
+        return null;
+    } else {
+        if (num % 2 === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+function showInfo(a, b) {
+    switch (b) {
+        case null:
+            console.log('Podany argument ' + a + ' nie jest liczbą');
+            break;
+        case false:
+            console.log('Podany argument ' + a + ' jest nie parzysty');
+            break;
+        case true:
+            console.log('Podany argument ' + a + ' jest parzysty');
+            break;
+    }
+}
+
+const a = 45;
+const b = 25;
+const c = 14;
+
+const sum = getSum(a, b, c);
+const even = isEven(sum);
+
+
+showInfo(sum, even);
