b060d2fd04cf3f2dc589f63414186d9e3c7a0095
Dominik Mozdzen <dominik.mozdzen1@gmail.com>
2021-04-18 19:56:06 +0200

    Task 02

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,30 @@
 
 /* rozwiązanie z pętlą for */
-const x = 4;
+const x = prompt("Podaj liczbę!");
 
-
+if(x > 0 && x <= 9) {
+   for(let i = 1; i <= 9; i++) {
+    result = i * x;
+    console.log(result);
+    } 
+} else {alert("Podana liczba nie mieści się w przedziale 1-9")}
 
 /* rozwiązanie z pętlą while  */
+
+const a = parseInt(prompt("Podaj podstawę!"));
+const n = parseInt(prompt("Podaj wykładnik!"));
+let j = 1;
+let exponentiation = ""
+
+if(a > 0 && a <= 9 && n > 0 && n <= 9) {
+    while (j <= n) {
+        exponentiation += a;
+        if(j < n) {
+            exponentiation += " * "
+        }
+        j++;
+    }
+    console.log(exponentiation + " = " + Math.pow(a, n))
+} else {
+    {alert("Podana liczba nie mieści się w przedziale 1-9")}
+}
