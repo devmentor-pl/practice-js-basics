8e3844162d858d6d4667d941550df3efeaf08bd2
Dominik Mozdzen <dominik.mozdzen1@gmail.com>
2021-04-18 21:20:03 +0200

    Task 04

---
+++ 04/app.js
@@ -0,0 +1,38 @@
+
+function createArray (min, max) {
+
+    const arr = []
+    for(let i = 0; i < 20; i++) {
+        arr.push(Math.round((Math.random() * (max - min)) + min))
+    }
+
+    return arr;
+}
+
+function getLargest (arr) {
+    
+    sortArr = arr.sort(function(a,b){
+        return b - a;
+    });
+
+    return sortArr.slice(0, 10);
+}
+
+function getAvg (arr) {
+
+    let sum = 0;
+    arr.forEach(function(element) {
+        sum += element / 10;
+    });
+
+    return sum;
+}
+
+const arr = createArray(10, 200);
+console.log(arr);
+
+const largest = getLargest(arr)
+console.log(largest);
+
+const avg = getAvg(sortArr);
+console.log(avg);
