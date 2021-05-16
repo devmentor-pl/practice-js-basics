16bfdd0eccf1f84fc509949db598e5786672fa18
Krzysztof Swol <krzysiek.swol@gmail.com>
2021-05-16 22:02:25 +0200

    task 5 done

--- 05/app.js
+++ 05/app.js
@@ -1,4 +1,3 @@
-
 function Student (name, surname) {
         this.name = name;
         this.surname = surname;
@@ -11,36 +10,37 @@ Student.prototype.addSubject = function (subject) {
 }
 
 Student.prototype.addRating = function (num, subject) {
-        this.grades[subject].push(num);
+        
+        if (typeof this.grades[subject] === 'undefined') {
+        console.log(`najpierw skorzystaj z funkcji addSubject aby dodać przedmiot`)
+        } else {
+                this.grades[subject].push(num);
+        }
 }
+        
 
-Student.prototype.averageOfSubject = function (subject) {
-        let result = 0;
-        subject = this.grades[subject];
-        subject.map(item => {
-                const grade = parseInt(item);
-                result = grade + result
-                
-        });
-        return result / subject.length;
-}
-Student.prototype.averageOfAllSubjects = function () {
+
+Student.prototype.averageOfSubjects = function (subject) {
         let result = 0;
         let length;
         let x = 0;
-        for(let key in this.grades) {
-                length = [this.grades[key].length].map(item => x = x + item)
-                this.grades[key].map(item => {
-                        item = parseInt(item);
-                        result = result + item
-                        result
-                         })
-                
+        subject = this.grades[subject];
+        if (subject) {
+                length = subject.length
+                subject.map(item => {
+                const grade = parseInt(item);
+                result = grade + result})
 
-        }
-        return result / length
-        
-        
+        } else {
+                for(let key in this.grades) {
+                        length = [this.grades[key].length].map(item => x = x + item)
+                        this.grades[key].map(item => {
+                                item = parseInt(item);
+                                result = result + item
+                        })
+                }
+        } 
+        return result / length    
 }
 
 
@@ -49,16 +49,21 @@ Student.prototype.averageOfAllSubjects = function () {
 
 
 
-const student1 = new Student ('Karol', 'Nowak');
-student1.addSubject('maths');
+const student1 = new Student ('Karol', 'Nowak'); //stworzenie instancji konstruktora
+student1.addSubject('maths'); // metoda dodawania przedmiotu
 student1.addSubject('history');
-student1.addRating('6', 'maths');
+student1.addRating('6', 'maths'); // dodawanie ocen do danego przemiotu
 student1.addRating('3', 'maths');
 student1.addRating('2', 'maths');
 student1.addRating('4', 'history');
 student1.addRating('3', 'history');
-console.log(student1.averageOfSubject('maths'));
-console.log(student1.averageOfAllSubjects());
+student1.addRating('6', 'biology'); // metoda nie zadziała bo nie ma wprowadzonego przedmiotu - info dla uzytkownika w consoli
+student1.addSubject('biology');
+student1.addRating('6', 'biology')
+
+console.log(student1.averageOfSubjects()); // średnia ze wszystkich przedmiotów
+console.log(student1.averageOfSubjects('history')); // średnia z danego przedmiotu 
+
 console.log(student1);
 
 
