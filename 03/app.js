78ed87853ff6a1b967f1334eaccc82f7a2737c2f
Krzysztof Swol <krzysiek.swol@gmail.com>
2021-05-16 14:06:29 +0200

    task 03 done

--- 03/app.js
+++ 03/app.js
@@ -7,6 +7,53 @@ const c = randomNumber(min, max);
 
 console.log(a, b, c);
 
+function getSum(num1, num2, num3) {
+    let x = parseInt(num1);
+    let y = parseInt(num2);
+    let z = parseInt(num3);
+
+    if (x > z && y > z) {
+        return x + y;
+    } else if (y > x && z > x) {
+        return y + z
+    } else {
+        return x + z
+    }
+}
+
+const sum = getSum(a, b, c);
+console.log(sum);
+
+
+const isEven = function (num) {
+    if (typeof num === 'number' && num % 2 === 0) {
+        return true
+    } else if (typeof num === 'number' && num % 2 !== 0) {
+        return false
+    } else {
+        return null;
+    }   
+}
+const even = isEven(sum)
+console.log(even);
+
+function showInfo (x, y) {
+    switch(y) {
+        case true:
+           console.log(`Podany argument ${x} jest parzysty`)
+            break;
+        case false:
+           console.log(`Podany argument ${x} jest nieparzysty`)
+            break;
+        default:
+            console.log(`Podany argument ${x} nie jest liczbą`)       
+    }
+}
+showInfo(sum, even)
+
+
+
+
 
 
 
