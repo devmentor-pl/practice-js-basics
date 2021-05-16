94776285ba42e523597f166c68c89fe5fc4e826d
Mateusz Kidawa <mateusz.kidawa@gmail.com>
2021-05-16 22:21:02 +0200

    all task in 01 done

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -1,4 +1,105 @@
-const a = '4.2';
-const b = 9;
+var a = '4.2';
+var b = 9;
 
 console.log(a, b);
+
+console.log(`variable a = ${a} is type "${typeof(a)}"
+and variable b = ${b} is type "${typeof(b)}"`); //grawis ` odwórcony apostrof
+
+
+let pa = parseFloat(a);
+
+const 
+addition = pa+b,
+subtraction = a-b,
+multiplicpation = a*b,
+division = a/b,
+modulus = a%b,
+exponentiation = a**b,
+postincrement = b++,
+postdecrement = b--,
+preincrement = --b,
+predecrement = ++b;
+
+
+console.log(
+`addition a+b = ${addition}
+subtraction a-b = ${subtraction}
+multiplicpation a*b = ${multiplicpation}
+division a/b = ${division}
+modulus a%b = ${modulus}
+exponentiation a**b = ${exponentiation}
+postincrement b++ = ${postincrement}
+postdecrement b-- = ${postdecrement}
+preincrement --b = ${preincrement}
+predecrement ++b = ${predecrement}
+
+`)
+
+console.log( addition<20 ? 'addition jest mniejsze od 20' : 'addition jest większe od 20');
+console.log( subtraction<20 ? 'subtraction jest mniejsze od 20' : 'subtraction jest większe od 20')
+console.log( multiplicpation<20 ? 'multiplicpation jest mniejsze od 20' : 'multiplicpation jest większe od 20')
+console.log( division<20 ? 'division jest mniejsze od 20' : 'division jest większe od 20')
+console.log( modulus<20 ? 'modulus jest mniejsze od 20' : 'modulus jest większe od 20')
+console.log( exponentiation<20 ? 'exponentiation jest mniejsze od 20' : 'exponentiation jest większe od 20')
+console.log( postincrement<20 ? 'postincrement jest mniejsze od 20' : 'postincrement jest większe od 20')
+console.log( postdecrement<20 ? 'postdecrement jest mniejsze od 20' : 'postdecrement jest większe od 20')
+console.log( preincrement<20 ? 'preincrement jest mniejsze od 20' : 'preincrement jest większe od 20')
+console.log( predecrement<20 ? 'predecrement jest mniejsze od 20' : 'predecrement jest większe od 20')
+
+
+
+// addition<20 ? console.log('addition jest mniejsze od 20') : console.log('addition jest większe od 20');
+// subtraction<20 ? console.log('subtraction jest mniejsze od 20') : console.log('subtraction jest większe od 20')
+// multiplicpation<20 ? console.log('multiplicpation jest mniejsze od 20') : console.log('multiplicpation jest większe od 20')
+// division<20 ? console.log('division jest mniejsze od 20') : console.log('division jest większe od 20')
+// modulus<20 ? console.log('modulus jest mniejsze od 20') : console.log('modulus jest większe od 20')
+// exponentiation<20 ? console.log('exponentiation jest mniejsze od 20') : console.log('exponentiation jest większe od 20')
+// postincrement<20 ? console.log('postincrement jest mniejsze od 20') : console.log('postincrement jest większe od 20')
+// postdecrement<20 ? console.log('postdecrement jest mniejsze od 20') : console.log('postdecrement jest większe od 20')
+// preincrement<20 ? console.log('preincrement jest mniejsze od 20') : console.log('preincrement jest większe od 20')
+// predecrement<20 ? console.log('predecrement jest mniejsze od 20') : console.log('predecrement jest większe od 20')
+
+// if(addition<20){
+//     console.log('addition jest mniejsze od 20')
+// }else{
+//     console.log('addition jest większe od 20')
+// }
+// if(subtraction<20){
+//     console.log('subtraction jest mniejsze od 20')
+// }else{
+//     console.log('subtraction jest większe od 20')
+// }if(multiplicpation<20){
+//     console.log('multiplicpation jest mniejsze od 20')
+// }else{
+//     console.log('multiplicpation jest większe od 20')
+// }if(division<20){
+//     console.log('division jest mniejsze od 20')
+// }else{
+//     console.log('division jest większe od 20')
+// }if(modulus<20){
+//     console.log('modulus jest mniejsze od 20')
+// }else{
+//     console.log('modulus jest większe od 20')
+// }if(exponentiation<20){
+//     console.log('exponentiation jest mniejsze od 20')
+// }else{
+//     console.log('exponentiation jest większe od 20')
+// }if(postincrement<20){
+//     console.log('postincrement jest mniejsze od 20')
+// }else{
+//     console.log('postincrement jest większe od 20')
+// }if(postdecrement<20){
+//     console.log('postdecrement jest mniejsze od 20')
+// }else{
+//     console.log('postdecrement jest większe od 20')
+// }if(preincrement<20){
+//     console.log('preincrement jest mniejsze od 20')
+// }else{
+//     console.log('preincrement jest większe od 20')
+// }if(predecrement<20){
+//     console.log('predecrement jest mniejsze od 20')
+// }else{
+//     console.log('predecrement jest większe od 20')
+// }
+
