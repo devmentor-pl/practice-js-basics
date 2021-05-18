cb40c00fef6949d85aa75aa44f283897b63b13f4
Maciej Zadylak <maciej.zadylak@gmail.com>
2021-05-18 20:31:38 +0200

    02 for

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,25 @@
 
 /* rozwiązanie z pętlą for */
-const x = 4;
+const x = prompt('Podaj liczbę: ');
+
+if(x >= 1 && x <= 9){
+for (let i = 0; i <= 9; i++) {
+    console.log(`${x}*${i} to ${x*i}`);
+}
+} else {
+    alert('Błąd!');
+}
 
 
 
 /* rozwiązanie z pętlą while  */
+
+let a = 3;
+let x = 4;
+let i = 1;
+let sum = 3;
+while(i <= x) {
+    console.log(sum);
+    sum = sum*a;
+    i++;
+}
