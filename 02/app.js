279a9bfaac122a8eaf590bd6204d836e53a8e0bd
Andrzej <andrzej.rakowski79@gmail.com>
2021-05-25 10:18:49 +0200

    Zadania 1 i 3 zrobione

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,12 @@
 
-/* rozwiązanie z pętlą for */
-const x = 4;
+const x = prompt('Podaj liczbę: ');
+const xint = parseInt(x);
 
+/* rozwiązanie z pętlą for */
+
+for(i = 1; i <= 10; i++) {
+    console.log(`${i} * ${x} = ${x * i}`);
+}
 
 
 /* rozwiązanie z pętlą while  */
