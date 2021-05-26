9ce40e08028d606fa2e67a2efaccf0fddc25a5e2
Andrzej <andrzej.rakowski79@gmail.com>
2021-05-26 16:25:08 +0200

    Zadania nr 1, 2, 3 i 4 zrobione, zadanie nr 5 czesciowo

--- 02/app.js
+++ 02/app.js
@@ -4,9 +4,14 @@ const xint = parseInt(x);
 
 /* rozwiązanie z pętlą for */
 
-for(i = 1; i <= 10; i++) {
+for(let i = 1; i <= 10; i++) {
     console.log(`${i} * ${x} = ${x * i}`);
 }
 
 
 /* rozwiązanie z pętlą while  */
+let i = 1;
+while(i <= 10) {
+    console.log(`${i} * ${x} = ${x * i}`);
+    i++;
+}
