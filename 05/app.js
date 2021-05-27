d01811642e8d12cf67c3ac019639a766a72c5538
Andrzej <andrzej.rakowski79@gmail.com>
2021-05-27 11:22:12 +0200

    Zadanie nr 5 zrobione

--- 05/app.js
+++ 05/app.js
@@ -11,18 +11,30 @@ Student.prototype.addGrade = function(subject, grade) {
     this.grades[subject].push(grade);
 }
 
-Student.prototype.getAverage = function(subject = 'undefined') {
-    if(subject !== 'undefined') {
+Student.prototype.getAverage = function(subject) {
+    if(typeof subject !== 'undefined') {
         for(const key in this.grades) {
             let sum = 0;
             const gradeArr = this.grades[subject];
+            console.log(gradeArr);
             for(let i = 0; i < gradeArr.length; i++) {
                 sum += gradeArr[i];
             }
             return sum / gradeArr.length;
         }
     } else {
-        console.log('hello')
+        const allGrades = [];
+        let sum = 0;
+        for(const item in this.grades) {
+            const gradesArr = this.grades[item];
+            gradesArr.forEach(function(grade) {
+                console.log(grade);
+                allGrades.push(grade);
+                sum += grade;
+            });
+        }
+        console.log(allGrades);
+        return sum / allGrades.length;
     }
 }
 
@@ -47,7 +59,7 @@ student2.addGrade('spanish', 4);
 console.log(student1);
 console.log(student2);
 
-console.log(student2.getAverage('spanish'));
+console.log(student2.getAverage());
 
 
 
