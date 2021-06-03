7ff07f0defec84ff3363910cbeec8de3b691cf4e
Malgorzata Kowacka <kowackag@gmail.com>
2021-06-03 22:11:20 +0200

    Finish task 03

--- 03/app.js
+++ 03/app.js
@@ -7,12 +7,56 @@ const c = randomNumber(min, max);
 
 console.log(a, b, c);
 
-
-
-
-
-
-
 function randomNumber(min, max) {
     return Math.round((Math.random() * (max - min)) + min);
 }
+
+function getSum(a, b, c) { // pewnie można było prościej :)
+    if (a > b && a > c) {
+        if (b > c) {
+            return a + b;
+        } else {
+            return a + c;
+        }
+    } else if (b > c) {
+        if (a > c) {
+            return a + b;
+        } else {
+            return b + c
+        }
+    } else {
+        if (a > b) {
+            return a + c;
+        } else {
+            return b + c;
+        }
+    }
+}
+const sum = getSum(a, b, c);
+
+
+const isEven = function (num) {
+    if (!(num * 1) && (+num !== 0)) {
+        return null;
+    } else {
+        return !(num % 2) ? "true" : "false"
+    }
+}
+
+const even = isEven(sum);
+
+function showInfo(sum, even) {
+    switch (even) {
+        case 'false':
+            console.log(`Podany argument ${sum} jest nieparzysty`);
+            break;
+        case 'true':
+            console.log(`Podany argument ${sum} jest parzysty`);
+            break;
+        default:
+            console.log(`Podany argument ${sum} nie jest liczbą`);
+            break;
+    }
+}
+
+showInfo(sum, even);
