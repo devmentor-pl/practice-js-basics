2314a5d9a12a2dc3818f71ed44b0860372f82c20
Malgorzata Kowacka <kowackag@gmail.com>
2021-06-04 17:38:56 +0200

    Finish task 05

---
+++ 05/app.js
@@ -0,0 +1,65 @@
+function Student(initFirstName, initLastName) {
+    this.firstName = initFirstName,
+        this.lastName = initLastName,
+        this.grades = {
+            // 'geo': [], // dodane do sprawdzenia średniej z przedmiotu bez ocen;
+        }
+}
+const student = new Student('Jan', 'Kowalski');
+
+Student.prototype.showFullName = function () {
+    console.log(this.firstName, this.lastName)
+}
+student.showFullName();
+
+Student.prototype.addGrade = function (sub, note) {
+    if (typeof this.grades[sub] === 'undefined') {
+        Object.defineProperty(this.grades, sub, {
+            value: [note],
+            enumerable: true,
+        });
+    } else {
+        this.grades[sub].push(note);
+    }
+}
+
+Student.prototype.getAverageGrade = function (sub) {
+    if (!(this.grades[sub]) || this.grades[sub].length === 0) {
+        return 'Brak ocen';
+    } else {
+        const avg = this.grades[sub].reduce((total, num) => total + num) / this.grades[sub].length;
+        return avg;
+    }
+}
+// ----------------PODDAJE SIĘ-------------------
+
+Student.prototype.getAverageGradeOfAll = function () {
+    let counter = 0;
+    let sum = 0;
+    for (const sub in this.grades) {
+        for (const note of this.grades[sub]) {
+            sum += note;
+            // sum += this.grades[sub][note];
+            counter++;
+            return sum / counter;
+        }
+    }
+
+}
+
+
+student.addGrade('math', 4);
+student.addGrade('math', 6);
+student.addGrade('english', 5);
+student.addGrade('english', 3);
+console.log(student);
+console.log(student);
+
+const avgMath = student.getAverageGrade('math');
+console.log(`średnia ocen z przedmiotu to: ${avgMath}`)
+
+const avgMath2 = student.getAverageGrade('geo');
+console.log(`średnia ocen z przedmiotu to: ${avgMath2}`)
+
+const avg = student.getAverageGradeOfAll();
+console.log(`średnia ocen ze wszystkich przedmiotów to: ${avg}`);
