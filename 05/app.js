0d7d34c46320e32b631fef58e9a8809f9447605d
Malgorzata Kowacka <kowackag@gmail.com>
2021-06-04 18:05:27 +0200

    Finish task 05

--- 05/app.js
+++ 05/app.js
@@ -24,35 +24,33 @@ Student.prototype.addGrade = function (sub, note) {
 }
 
 Student.prototype.getAverageGrade = function (sub) {
-    if (!(this.grades[sub]) || this.grades[sub].length === 0) {
-        return 'Brak ocen';
+    if (typeof this.grades[sub] === 'undefined') { // ----------------PODDAJE SIĘ-------------
+        let counter = 0;
+        let sumAll = 0;
+        for (const sub in this.grades) {
+            let sumOfSub = 0;
+            for (const note of this.grades[sub]) {
+                sumOfSub += note;
+                // sum += this.grades[sub][note];
+                counter++;
+            }
+            return (sumOfSub / counter).toFixed(2);
+        }
+
+        // sumAll += sumOfSub;
+        // return sumAll;
     } else {
-        const avg = this.grades[sub].reduce((total, num) => total + num) / this.grades[sub].length;
+        const avg = (this.grades[sub].reduce((total, num) => total + num) / this.grades[sub].length).toFixed(2);
         return avg;
     }
 }
-// ----------------PODDAJE SIĘ-------------------
 
-Student.prototype.getAverageGradeOfAll = function () {
-    let counter = 0;
-    let sumAll = 0;
-    for (const sub in this.grades) {
-        let sumOfSub = 0;
-        for (const note of this.grades[sub]) {
-            sumOfSub += note;
-            // sum += this.grades[sub][note];
-            counter++;
-        }
-        return (sumOfSub / counter).toFixed(2);
-    }
-    // sumAll += sumOfSub;
-    // return sumAll;
-}
+
 
 
 student.addGrade('math', 4);
 student.addGrade('math', 6);
-student.addGrade('math', 3);
+student.addGrade('math', 6);
 student.addGrade('english', 5);
 student.addGrade('english', 3);
 student.addGrade('english', 3);
@@ -62,8 +60,11 @@ console.log(student);
 const avgMath = student.getAverageGrade('math');
 console.log(`średnia ocen z przedmiotu to: ${avgMath}`)
 
-const avgMath2 = student.getAverageGrade('geo');
+const avgMath2 = student.getAverageGrade('english');
 console.log(`średnia ocen z przedmiotu to: ${avgMath2}`)
 
-const avg = student.getAverageGradeOfAll();
+const avgMath3 = student.getAverageGrade('geo');
+console.log(`średnia ocen z przedmiotu to: ${avgMath2}`)
+
+const avg = student.getAverageGrade();
 console.log(`średnia ocen ze wszystkich przedmiotów to: ${avg}`);
