7a2287295760e68a749ebd66c5a5d4279a54ef7c
Mateusz Kidawa <mateusz.kidawa@gmail.com>
2021-06-14 23:03:11 +0200

    task 05 done

--- 05/app.js
+++ 05/app.js
@@ -2,25 +2,57 @@ function Student(name,surname){
     this.name = name;
     this.surname = surname;
     this.grades = {};
-    this.gradespro = {}
 }
-var stud1 = new Student("matt","kid");
-//var subject="math";
-stud1.grades.math = [2,3,4];
-stud1.grades.chemistry = [4];
-stud1.grades.english = [3];
-
 Student.prototype.addGrade = function (subject, grade){
-   this.grades[subject]=[grade];
+    if(typeof this.grades[subject] === 'undefined') {
+        this.grades[subject] = [];
+    }
+    this.grades[subject].push(grade);
+};
+Student.prototype.getAverageGrade = function(subject) {
+    if(typeof subject==="undefined"){
+        let sum = 0;
+        const subjectGrades = [];
+        Object.keys(this.grades).forEach(element => {
+            this.grades[element].forEach(function(element) {
+                subjectGrades.push(element);
+            });
+            // console.log(element, this.grades[element]);
+        });
+        const average = subjectGrades.reduce(function(a,b){return a+b;}) /subjectGrades.length;
+        return average;
+    //     let sum = 0;
+    //     for(let i=0; i <= this.grades[subject].length -1; i++) {
+    //         sum = sum + this.grades[subject][i]
+    //         console.log(sum);
+    //     }
+    //     const average = sum / this.grades[subject].length;
+    //     return average;
+               
+    }
+    else{
+        const subjectGrades = [];
+        for (const key in this.grades[subject]) {
+
+            this.grades[subject].forEach( function(element) {
+                subjectGrades.push(element);
+            });
+        // const grades = [];
+        // for (const key in this.grades) {
+        //     this.grades[key].forEach(element => {grades.push(element);});
+        // };
+        // return grades.reduce((prev, curr) => prev + curr, 0)/grades.length;  
+        const average = subjectGrades.reduce(function(a,b){return a+b;}) /subjectGrades.length;
+        return average;
+        };
+    }
+
 }
-
-stud1.addGrade("physics",4);
-
-"ssaasa";
-
-stud1.gradespro.networks = {};
-stud1.gradespro.networks.project = [2, 5, 5];
-stud1.gradespro.networks.laboratory = [3, 4.5, 5];
-
-
-        
+const stud1 = new Student("Jan","Kowalski");
+stud1.addGrade('maths', 21);
+stud1.addGrade('maths', 6);
+stud1.addGrade('english', 3);
+//stud1.addGrade('maths', 6);
+// stud1.addGrade('english', 3);
+const avgMath = stud1.getAverageGrade('maths'); // 5
+const avg = stud1.getAverageGrade(); // 4.33
