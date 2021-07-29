16ca5efe95a38bb54b3739a60bee2b68c6d64cca
Szymon Suchanowski <szymonsuchanowski@gmail.com>
2021-07-29 21:26:00 +0200

    closes task 3

--- 03/app.js
+++ 03/app.js
@@ -7,12 +7,36 @@ const c = randomNumber(min, max);
 
 console.log(a, b, c);
 
+function getSum(x, y, z) {
+    const arr = [parseInt(x), parseInt(y), parseInt(z)].
+        sort((a, b) => a - b);
+    return arr[1] + arr[2];
+}
 
+const isEven = function (num) {
+    if (isNaN(num)) return null;
+    else if (num % 2 === 0) return true;
+    return false;
+}
 
-
-
-
+function showInfo(num, info) {
+    switch (info) {
+        case null:
+            console.log(`Podany argument ${num} nie jest liczbą`);
+            break;
+        case true:
+            console.log(`Podany argument ${num} jest parzysty`);
+            break;
+        case false:
+            console.log(`Podany argument ${num} jest nieparzysty`);
+            break;
+    }
+}
 
 function randomNumber(min, max) {
     return Math.round((Math.random() * (max - min)) + min);
 }
+
+const sum = getSum(a, b, c);
+const even = isEven(sum);
+showInfo(sum, even);
