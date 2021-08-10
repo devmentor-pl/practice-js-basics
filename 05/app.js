038acdfbcc9b84b9b888f74dc23ecb7e9939ad64
Jakub Chrobak <jchrobakk@gmail.com>
2021-08-10 15:19:40 +0200

    Finish task 05

---
+++ 05/app.js
@@ -0,0 +1,41 @@
+function Student(initFirstName, initLastName) {
+  this.firstName = initFirstName;
+  this.lastName = initLastName;
+  this.grades = {};
+}
+
+Student.prototype.addGrade = function (subject, grade) {
+  if (typeof this.grades[subject] === "undefined") {
+    this.grades[subject] = [];
+  }
+
+  this.grades[subject].push(grade);
+};
+
+Student.prototype.getAverageGrade = function (subject) {
+  if (typeof subject !== "undefined") {
+    return getAverage(this.grades[subject]);
+  } else {
+    const subjectAvg = [];
+    for (key in this.grades) {
+      subjectAvg.push(getAverage(this.grades[key]));
+    }
+    return getAverage(subjectAvg);
+  }
+};
+
+function getAverage(arr) {
+  const sum = arr.reduce(function (acc, curr) {
+    return acc + curr;
+  });
+
+  return sum / arr.length;
+}
+
+const student1 = new Student("Jan", "Kowalski");
+
+student1.addGrade("maths", 5);
+student1.addGrade("maths", 4);
+student1.addGrade("english", 4);
+
+console.log(student1);
