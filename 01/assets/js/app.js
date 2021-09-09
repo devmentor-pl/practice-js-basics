c97fd94c0c0165f865d959a311d929778bdc245f
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2021-09-09 20:16:14 +0200

    Finish task 1

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -1,8 +1,25 @@
 const a = '4.2';
-const b = 9;
+let b = 9;
 
 console.log(a, b);
 
-console.log(typeof a);
-console.log(typeof b);
+console.log(typeof a, typeof b);
 
+const sum = 2 + 5;
+const concat = 97 + '4.2';
+const sub = 5 - 120;
+const div = 10 / 5;
+const mult = 6 * 9;
+const mod = 5 % 3;
+const incrementation = b++;
+const decrementation = b--;
+
+const arr = [sum, concat, sub, div, mult, mod, incrementation, decrementation];
+
+console.log(arr);
+
+arr.forEach((num) =>
+	num > 20
+		? console.log(`${num} is greater than 20`)
+		: console.log(`${num} is lower than 20`)
+);
