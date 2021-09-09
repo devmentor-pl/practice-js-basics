132380326696f9d4d39b077ed1427a89ada5cd1e
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2021-09-09 20:32:31 +0200

    First case - task 2

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,11 @@
-
 /* rozwiązanie z pętlą for */
 const x = 4;
 
-
+function multTable(num) {
+	for (let i = 1; i < 10; i++) {
+		console.log(num * i);
+	}
+}
+multTable(x);
 
 /* rozwiązanie z pętlą while  */
