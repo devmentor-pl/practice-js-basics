33a218398bc065b245286d4229c786ec50e8b942
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2021-09-10 12:02:32 +0200

    Finish case with prompt and range between 1-9

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,9 @@
 /* rozwiązanie z pętlą for */
-const x = 4;
+const x = prompt('Enter your number');
 
 function multTable(num) {
+	const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+	if (!nums.includes(Number(num))) throw Error('Input must be between 1-9');
 	for (let i = 1; i < 10; i++) {
 		console.log(num * i);
 	}
