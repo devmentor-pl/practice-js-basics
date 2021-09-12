e456d3f0596626a896bb2012b11b67bfeda489ba
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2021-09-12 14:16:09 +0200

    Finish task 2

--- 02/app.js
+++ 02/app.js
@@ -1,5 +1,6 @@
 /* rozwiązanie z pętlą for */
-const x = prompt('Enter your number');
+
+// const x = prompt('Enter your number');
 
 function multTable(num) {
 	const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -8,6 +9,23 @@ function multTable(num) {
 		console.log(num * i);
 	}
 }
-multTable(x);
+// multTable(x);
+
+a = prompt('Enter the base of the number');
+n = prompt('Enter the number exponent');
 
 /* rozwiązanie z pętlą while  */
+function toPower(a, n) {
+	const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+	if (!nums.includes(Number(a))) throw Error('Input must be between 1-9');
+	if (!nums.includes(Number(n))) throw Error('Input must be between 1-9');
+	let i = 0;
+	let result = 1;
+	while (i < n) {
+		i++;
+		result *= a;
+	}
+	return result;
+}
+
+console.log(toPower(a, n));
