f5fa5e096a6a54be90696eb1c37f7e3fefc9ecad
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2021-09-14 12:03:53 +0200

    Add additional variable in task 2

--- 02/app.js
+++ 02/app.js
@@ -1,15 +1,16 @@
 /* rozwiązanie z pętlą for */
 
-// const x = prompt('Enter your number');
+const x = prompt('Enter your number');
 
 function multTable(num) {
 	const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-	if (!nums.includes(Number(num))) throw Error('Input must be between 1-9');
+	const value = Number(num);
+	if (!nums.includes(value)) throw Error('Input must be between 1-9');
 	for (let i = 1; i < 10; i++) {
 		console.log(num * i);
 	}
 }
-// multTable(x);
+multTable(x);
 
 a = prompt('Enter the base of the number');
 n = prompt('Enter the number exponent');
