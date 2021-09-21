9080d8ab5bd55b44e5045177789f2452947cc51f
Mary-cyber <maria.kubas@gmail.com>
2021-09-21 09:48:23 +0200

    zadania js

--- 03/app.js
+++ 03/app.js
@@ -7,11 +7,20 @@ const c = randomNumber(min, max);
 
 console.log(a, b, c);
 
+function getSum(a, b, c) {
+    let sum = parseInt(a);
+    sum += parseInt(b);
+    sum += parseInt(c);
+    return sum;}
+
+    /*1. Funkcja o nazwie `getSum`:
+    - przyjmuje 3 parametry. Ich wartość w ciele funkcji ma zostać przekonwertowana na liczby całkowite,
+    - z podanych 3 liczb wybierane są 2 największe,
+    - wartości tych 2 liczb są sumowane, a wynik jest zwracany przez funkcję.
 
 
 
-
-
+*/
 
 function randomNumber(min, max) {
     return Math.round((Math.random() * (max - min)) + min);
