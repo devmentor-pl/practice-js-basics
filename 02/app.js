bcaa5e0b0af22d6789e05c336f344d710eaeebe7
Dawid Krawczyk <dawidosss1998@gmail.com>
2021-09-26 19:52:26 +0200

    Third task

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,33 @@
 
 /* rozwiązanie z pętlą for */
-const x = 4;
 
+// const x = prompt('Podaj liczbę!');
+// const parsedX = parseInt(x);
+
+// if (parsedX <= 9 && parsedX >= 1) {
+
+//     for (let i=1; i<=parsedX; i++) {
+//         let result = 0;
+    
+//         result = parsedX * [i];
+    
+//         console.log([i] + ' x ' + parsedX + ' = ' + result);
+//     }
+// }
+// else {
+//     alert('Podana liczba nie mieści się w przedziale 1-9!')
+// }
 
 
 /* rozwiązanie z pętlą while  */
+
+let a=3;
+let n=4
+result = 1;
+
+while(result <= n) {
+
+ console.log(a);
+ a = a * a;
+ result++;
+}
--- 03/app.js
+++ 03/app.js
@@ -5,13 +5,80 @@ const a = randomNumber(min, max);
 const b = randomNumber(min, max);
 const c = randomNumber(min, max);
 
-console.log(a, b, c);
+const sumNumbers = getSum(4, 25, 14)
+// console.log(sumNumbers);
+
+function getSum(a, b, c) {
+    const convertedA = parseInt(a);
+    const convertedB = parseInt(b);
+    const convertedC = parseInt(c); 
+
+    if (convertedA>=convertedB && convertedA>convertedC && convertedB>=convertedC) {
+        return convertedA+convertedB;
+    }
+    else if (convertedA>convertedB && convertedA>=convertedC && convertedC>=convertedB) {
+        return convertedA+convertedC;
+    }
+    else if (convertedB>convertedA && convertedB>=convertedC && convertedC>=convertedA) {
+        return convertedB+convertedC;
+    }
+    else if (convertedB>=convertedA && convertedB>convertedC && convertedA>=convertedC) {
+        return convertedB+convertedA;
+    }
+    else if (convertedC>=convertedA && convertedC>convertedB && convertedA>=convertedB) {
+        return convertedC+convertedA;
+    }
+    else if (convertedC>convertedA && convertedC>=convertedB && convertedB>=convertedA) {
+        return convertedC+convertedB
+    }
+    else {
+        console.log('Wszystkie cyfry są takie same');
+    }
+
+}
+
+const isEvan = function evanNumbers(number) {
+
+    if (typeof number !== 'number') {
+        return null;
+    }
+    else if (number % 2 === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+const evenNumbers = isEvan(sumNumbers);
+// console.log(evenNumbers);
 
 
+function showInfo(someValue, boolean) {
 
+    switch(boolean) {
+        case null:
+            console.log('Podany argument ' + someValue + ' nie jest liczbą');
+            break;
 
+        case true:
+            console.log ('Podany argument ' + someValue + ' jest parzysty');
+            break;
+        
+        case false:
+            console.log ('Podany argument ' + someValue + ' jest nieparzysty');
+            break;
+    }
+}
 
+console.log(sumNumbers);
+console.log(evenNumbers);
 
+showInfo(sumNumbers, evenNumbers);
+
+// const communicat =
+ 
+// console.log(communicat);
 
 function randomNumber(min, max) {
     return Math.round((Math.random() * (max - min)) + min);
