c67a52344d071b8121a6d455665d929cc1646d0c
Dawid Krawczyk <dawidosss1998@gmail.com>
2021-09-26 20:07:46 +0200

    Second task

--- 02/app.js
+++ 02/app.js
@@ -1,33 +1,40 @@
 
 /* rozwiązanie z pętlą for */
 
-// const x = prompt('Podaj liczbę!');
-// const parsedX = parseInt(x);
+const x = prompt('Podaj liczbę!');
+const parsedX = parseInt(x);
 
-// if (parsedX <= 9 && parsedX >= 1) {
+if (parsedX <= 9 && parsedX >= 1) {
 
-//     for (let i=1; i<=parsedX; i++) {
-//         let result = 0;
+    for (let i=1; i<=parsedX; i++) {
+        let result = 0;
     
-//         result = parsedX * [i];
+        result = parsedX * [i];
     
-//         console.log([i] + ' x ' + parsedX + ' = ' + result);
-//     }
-// }
-// else {
-//     alert('Podana liczba nie mieści się w przedziale 1-9!')
-// }
+        console.log([i] + ' x ' + parsedX + ' = ' + result);
+    }
+}
+else {
+    alert('Podana liczba nie mieści się w przedziale 1-9!')
+}
 
 
 /* rozwiązanie z pętlą while  */
 
-let a=3;
-let n=4
-result = 1;
+const a= prompt('Podaj podstawę!');
+const parsedA = parseInt(a);
+const n= prompt('Podaj wykładnik!');
+const parsedN = parseInt(n);
 
-while(result <= n) {
+let sum = 1;
+let result = 1;
 
- console.log(a);
- a = a * a;
- result++;
+while (result <= parsedN) {
+
+
+sum = parsedA * sum;
+result++;
+// console.log(sum);
 }
+
+alert('Potęgowanie wynosi ' + sum);
