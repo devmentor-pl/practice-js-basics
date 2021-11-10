d58bd8628399ff297a931f0af51412a86187d4d4
Magdalena Nolde <twoj@email>
2021-11-10 23:01:27 +0100

    finish task 2

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,40 @@
 
 /* rozwiązanie z pętlą for */
-const x = 4;
+function multiplyCalculation () {
 
+const x = prompt("podaj liczbe ktora chcesz pomnozyc");
 
+if (x > 9) {
+    alert('Musisz podac liczbe w przedziale 0-9');
+  }
+  else {
+let i = 0;
+for(let i=0; i<=9; i++) {
+let multiplyResult = x * i;
+console.log(`${x} * ${i} = ${multiplyResult}`);
+}
+}
+}
+
+multiplyCalculation()
 
 /* rozwiązanie z pętlą while  */
+
+/* function powerCalculation () {
+const p = prompt ("podaj liczbe ktora chcesz podniesc do potegi");
+const w = prompt ("podaj wykladnik");
+
+if (isNaN(p) ||isNaN(w) ) {
+    alert('Musisz podac liczbe');
+  }
+else {
+    let l = 1;
+    let str = ` `;
+    while(l < w) {
+        l++;
+        str = str + `* ${p}`;
+    }
+console.log (`${p}${str} = ${Math.pow(p,w)}`);
+}
+}
+ powerCalculation() */
