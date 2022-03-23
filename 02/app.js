8c6e346c87a38229db563efeb4f3fdaf0ec526b6
Martyna Giee <martyna.giee@gmail.com>
2022-03-23 17:43:51 +0100

    done task 2

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,30 @@
-
-/* rozwiązanie z pętlą for */
 const x = 4;
 
+/* rozwiązanie z pętlą for */
 
+console.log(`For:`);
+multiplyUsingFor(x);
+
+function multiplyUsingFor(number) {
+
+    for (let i = 1; i < 10; i++) {
+        console.log(`${i} x ${number} = ${i * number}`);
+    }
+
+}
 
 /* rozwiązanie z pętlą while  */
+
+console.log(`While:`);
+multiplyUsingWhile(x);
+
+function multiplyUsingWhile(number) {
+
+    let i = 1;
+
+    while (i < 10) {
+        console.log(`${i} x ${number} = ${i * number}`);
+        i++;
+    }
+
+}
