79f681eb11d93c2231ca9d3ded9664c8b04e8911
Joanna Wierzbicka <joannawierzbicka@poczta.onet.pl>
2022-03-27 14:06:06 +0200

    task 3

--- 03/app.js
+++ 03/app.js
@@ -7,12 +7,60 @@ const c = randomNumber(min, max);
 
 console.log(a, b, c);
 
-
-
-
-
-
-
 function randomNumber(min, max) {
     return Math.round((Math.random() * (max - min)) + min);
 }
+
+function getSum(x, y, z) {
+    const xNum = parseInt(x);
+    const yNum = parseInt(y);
+    const zNum = parseInt(z);
+
+    const theSmallest = Math.min(xNum, yNum, zNum);
+    
+    let sum = xNum + yNum + zNum - theSmallest;
+    return sum;
+  
+}
+
+const sum = getSum(a, b, c);
+console.log(sum);
+
+
+const isEven = function(number){
+    if(typeof number != 'number'){
+        return "null"
+    } else if (number % 2 === 0){
+        return "true";
+    } else {
+        return "false";
+    }
+}
+
+const even = isEven(sum);
+console.log(even);
+
+
+function showInfo(sum, value){
+    console.log(sum);
+  
+    switch(value){
+        case 'null':
+            console.log(`Podany argument ${value} nie jest liczbą`);
+            break;
+        case 'true':
+            console.log(`Podany argument ${value} jest parzysty`);
+            break;
+        case 'false':
+            console.log(`Podany argument ${value} jest nieparzysty`);
+            break;    
+    }
+}
+
+showInfo(sum, even);
+
+
+
+
+
+
