d7e090806fa90f97a39862441321220547daeb9b
Martyna Giee <martyna.giee@gmail.com>
2022-03-27 11:22:34 +0200

    fix: comment class part

--- 05/app.js
+++ 05/app.js
@@ -1,104 +1,104 @@
-// function Student(initFirstName, initLastName) {
-//     this.firstName = initFirstName
-//     this.lastName = initLastName
-//     this.grades = {}
-// }
+function Student(initFirstName, initLastName) {
+    this.firstName = initFirstName
+    this.lastName = initLastName
+    this.grades = {}
+}
 
-// Student.prototype.addGrade = function (grade, subject) {
+Student.prototype.addGrade = function (grade, subject) {
 
-//     if (grade < 1 || grade > 6 || typeof grade != "number" || typeof subject !== "string" || subject === "") {
+    if (grade < 1 || grade > 6 || typeof grade != "number" || typeof subject !== "string" || subject === "") {
 
-//         console.log(`Nieprawidłowe dane`)
+        console.log(`Nieprawidłowe dane`)
 
-//     } else {
-//         if (!this.grades[subject]) {
-//             this.grades[subject] = []
-//         }
-//         this.grades[subject].push(grade)
-//     }
-// }
+    } else {
+        if (!this.grades[subject]) {
+            this.grades[subject] = []
+        }
+        this.grades[subject].push(grade)
+    }
+}
 
-// Student.prototype.getAverage = function (subject) {
+Student.prototype.getAverage = function (subject) {
 
-//     if (subject === undefined) {
+    if (subject === undefined) {
 
-//         const subjects = Object.getOwnPropertyNames(this.grades)
-//         let allGrades = [];
+        const subjects = Object.getOwnPropertyNames(this.grades)
+        let allGrades = [];
 
-//         subjects.forEach(element => allGrades = allGrades.concat(this.grades[element]))
+        subjects.forEach(element => allGrades = allGrades.concat(this.grades[element]))
 
-//         const initial = 0
-//         const sum = allGrades.reduce(
-//             (previousValue, currentValue) => previousValue + currentValue,
-//             initial)
+        const initial = 0
+        const sum = allGrades.reduce(
+            (previousValue, currentValue) => previousValue + currentValue,
+            initial)
 
-//         return (sum / allGrades.length).toFixed(2);
-//     } else if (typeof subject === "string") {
+        return (sum / allGrades.length).toFixed(2);
+    } else if (typeof subject === "string") {
 
-//         const initial = 0;
-//         const sum = this.grades[subject].reduce(
-//             (previousValue, currentValue) => previousValue + currentValue,
-//             initial)
+        const initial = 0;
+        const sum = this.grades[subject].reduce(
+            (previousValue, currentValue) => previousValue + currentValue,
+            initial)
 
-//         return (sum / this.grades[subject].length).toFixed(2)
-//     } else {
-//         console.log(`Błędne dane`)
-//     }
-// }
+        return (sum / this.grades[subject].length).toFixed(2)
+    } else {
+        console.log(`Błędne dane`)
+    }
+}
 
 
 // Same functionality but using  "class" declaration
 
-class Student {
+// class Student {
 
-    constructor(initFirstName, initLastName) {
-        this.firstName = initFirstName;
-        this.lastName = initLastName;
-        this.grades = {};
-    }
-    addGrade(grade, subject) {
+//     constructor(initFirstName, initLastName) {
+//         this.firstName = initFirstName;
+//         this.lastName = initLastName;
+//         this.grades = {};
+//     }
+//     addGrade(grade, subject) {
 
-        if (grade < 1 || grade > 6 || typeof grade != "number" || typeof subject !== "string" || subject === "") {
+//         if (grade < 1 || grade > 6 || typeof grade != "number" || typeof subject !== "string" || subject === "") {
 
-            console.log(`Nieprawidłowe dane`)
+//             console.log(`Nieprawidłowe dane`)
 
-        } else {
-            if (!this.grades[subject]) {
-                this.grades[subject] = []
-            }
-            this.grades[subject].push(grade)
-        }
-    }
+//         } else {
+//             if (!this.grades[subject]) {
+//                 this.grades[subject] = []
+//             }
+//             this.grades[subject].push(grade)
+//         }
+//     }
 
-    getAverage(subject) {
+//     getAverage(subject) {
 
-        if (subject === undefined) {
+//         if (subject === undefined) {
 
-            const subjects = Object.getOwnPropertyNames(this.grades)
-            let allGrades = [];
+//             const subjects = Object.getOwnPropertyNames(this.grades)
+//             let allGrades = [];
 
-            subjects.forEach(element => allGrades = allGrades.concat(this.grades[element]))
+//             subjects.forEach(element => allGrades = allGrades.concat(this.grades[element]))
 
-            const initial = 0
-            const sum = allGrades.reduce(
-                (previousValue, currentValue) => previousValue + currentValue,
-                initial)
+//             const initial = 0
+//             const sum = allGrades.reduce(
+//                 (previousValue, currentValue) => previousValue + currentValue,
+//                 initial)
 
-            return (sum / allGrades.length).toFixed(2);
-        } else if (typeof subject === "string") {
+//             return (sum / allGrades.length).toFixed(2);
+//         } else if (typeof subject === "string") {
 
-            const initial = 0;
-            const sum = this.grades[subject].reduce(
-                (previousValue, currentValue) => previousValue + currentValue,
-                initial)
+//             const initial = 0;
+//             const sum = this.grades[subject].reduce(
+//                 (previousValue, currentValue) => previousValue + currentValue,
+//                 initial)
 
-            return (sum / this.grades[subject].length).toFixed(2)
-        } else {
-            console.log(`Błędne dane`)
-        }
+//             return (sum / this.grades[subject].length).toFixed(2)
+//         } else {
+//             console.log(`Błędne dane`)
+//         }
 
-    }
-}
+//     }
+// }
 
 
 const student1 = new Student("Ola", "Nowak");
