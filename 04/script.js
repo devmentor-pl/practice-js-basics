fde107f9790c90e10c4bacc768b3ee5ba0e781c5
Andrzej <andis_as@wp.pl>
2022-04-01 16:29:45 +0200

    zad 4 -done

--- 04/script.js
+++ 04/script.js
@@ -1,18 +1,27 @@
 console.log('zad-4')
 const n = 20
 
+console.log('----- create Array of 20 random numbers from range 1-200 -----')
+const arr = createArr(1,200)
+console.log(arr)
+
+console.log('----- get 10 largest numbers -----')
+const largest = getLargest(arr) 
+console.log(largest)
+
+console.log('----- arithmetic average of the 10 larget numbers-----')
+const avg = getAvg(largest)
+console.log('average', avg)
+
+// functions
 function createArr(min, max) {
     let arr = []
     for(let i=0; i<n; i++) {
         let = random = Math.round((Math.random() * (max - min)) + min)
-        console.log(random)
         arr.push(random)
     }
     return arr
 }
-const arr = createArr(1,100)
-console.log(arr)
-
 
 function getLargest(arr) {
     arr.sort(function(a, b) {
@@ -21,11 +30,8 @@ function getLargest(arr) {
     console.log(arr)
     return arr.slice(0, 10)
 }
-const largest = getLargest(arr) 
-console.log(largest)
 
 function getAvg(arr) {
-    console.log('getAvg')
     console.log(arr)
     let sum = 0
     sum = arr.reduce(function(prev, next) {
@@ -33,6 +39,8 @@ function getAvg(arr) {
     }, 0)
     return sum / arr.length
 }
-const avg = getAvg([1,2,3,4,5])
-console.log(avg)
+
+
+
+
 
