516f69e4b1eb0e6d4a7927b46dacfa5e9c30133a
Andrzej <andis_as@wp.pl>
2022-04-01 14:39:14 +0200

    zad 3 - done

--- 03/app.js
+++ 03/app.js
@@ -7,7 +7,39 @@ const c = randomNumber(min, max);
 
 console.log(a, b, c);
 
+
+// getSum
+// ------------------------------
 console.log('----- getSum -----')
+const sum = getSum(a, b, c)
+console.log(sum)
+
+
+// isEven
+// ------------------------------
+console.log('----- isEven -----')
+const isEven = function isEven(param) {
+    console.log('typeof param:', typeof param)
+    if(typeof param !== 'number') return null
+    const result = param % 2 === 0 ? true : false
+    return result
+}
+const even = isEven(sum)
+console.log('sum='+sum, 'even:', even)
+
+
+// showInfo
+// ------------------------------
+console.log('----- showInfo -----')
+showInfo(sum, even)
+
+
+// functions
+// ------------------------------
+function randomNumber(min, max) {
+    return Math.round((Math.random() * (max - min)) + min);
+}
+
 function getSum(x, y, z) {
     let a = parseInt(x)
     let b = parseInt(y)
@@ -32,51 +64,6 @@ function getSum(x, y, z) {
     return sum
 }
 
-const sum = getSum(a, b, c)
-console.log(sum)
-
-
-console.log('----- isEven -----')
-const isEven = function isEven(param) {
-    console.log(typeof param)
-    if(typeof param !== 'number') return null
-    const result = param % 2 === 0 ? true : false
-    return result
-}
-const test1 = 'Ala'
-let result = isEven(test1)
-console.log(test1, 'result:', result)
-
-const test2 = false
-result = isEven(test2)
-console.log(test2, 'result:', result)
-
-const test3 = []
-result = isEven(test3)
-console.log(test3, 'result:', result)
-
-const test4 = 10
-result = isEven(test4)
-console.log(test4, 'result:', result)
-
-const test5 = 11
-result = isEven(test5)
-console.log(test5, 'result:', result)
-
-result = isEven(a)
-console.log('a='+a, 'result:', result)
-
-result = isEven(b)
-console.log('b='+b, 'result:', result)
-
-result = isEven(c)
-console.log('c='+c, 'result:', result)
-
-const even = isEven(sum)
-console.log('sum='+sum, 'even:', even)
-
-
-console.log('----- showInfo -----')
 function showInfo(x, y) {
     console.log('First arg - sum: ', x)
     console.log('Second arg - even: ', y)
@@ -96,10 +83,4 @@ function showInfo(x, y) {
             break
     }
     console.log(info)
-}
-
-showInfo(sum, even)
-
-function randomNumber(min, max) {
-    return Math.round((Math.random() * (max - min)) + min);
 }
