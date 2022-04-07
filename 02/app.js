7e61dacc318cc2cae9b17417c8824b108dfe9ea1
Dawid Drag <dede7d@wp.pl>
2022-04-07 21:43:21 +0200

    fixed js code

--- 02/app.js
+++ 02/app.js
@@ -1,18 +1,44 @@
 
 /* rozwiązanie z pętlą for */
 const x = prompt('Podaj liczbę!');
-
 function matchTable() {
+    if (x > 0 && x < 10) {
 
-    for (let i = 1; i <= 9; i++) {
+        for (let i = 1; i <= 9; i++) {
 
-        let result = i * x;
-        console.log(i + ' x', x + ' = ', result);
+            let result = i * x;
+            console.log(i + ' x', x + ' = ', result);
 
+        }
     }
 
+    else {
+        console.log('Złe dane!');
+    }
 }
 
 matchTable();
+
+
+
 /* rozwiązanie z pętlą while  */
 
+function matchTable2() {
+    const a = 3;
+    const n = 4;
+
+
+    let iter = 0;
+
+    while (iter < n) {
+        iter++;
+        console.log(iter);
+    }
+
+
+}
+
+
+
+
+matchTable2();
