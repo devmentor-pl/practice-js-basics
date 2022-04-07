617060e0a64d4a08e7390620282a223764019d4b
Dawid Drag <dede7d@wp.pl>
2022-04-07 20:15:01 +0200

    added for loop

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,18 @@
 
 /* rozwiązanie z pętlą for */
-const x = 4;
+const x = prompt('Podaj liczbę!');
 
+function matchTable() {
 
+    for (let i = 1; i <= 9; i++) {
 
+        let result = i * x;
+        console.log(i + ' x', x + ' = ', result);
+
+    }
+
+}
+
+matchTable();
 /* rozwiązanie z pętlą while  */
+
