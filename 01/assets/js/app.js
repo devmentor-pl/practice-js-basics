85e26e34c82b5bef391a020870485a77d1b1fe29
justyna <strawberka17@gmail.com>
2022-04-07 17:50:57 +0100

    finished task 1

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -1,4 +1,21 @@
-const a = '4.2';
-const b = 9;
+const a = Number('4.2');
+const b = 119;
+let sum=a+b
+console.log(sum);
+let minus=a-b
+console.log(minus);
+let multiplication=a*b
+console.log(multiplication);
+let divide=a/b
+console.log(divide);
+let modulo=a%b
+console.log(modulo);
 
-console.log(a, b);
+const resultsArray=[sum,minus,multiplication,divide,modulo]
+resultsArray.forEach(function(num) {
+    if(num>20){
+        console.log(`${num}+"wieksze"`)
+    }else{
+        console.log(`${num}+"mniejsze`)
+    }
+})
