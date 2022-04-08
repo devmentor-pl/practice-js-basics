14ca91db7df27fbb327a8ee33efd1a70e38433b4
Dawid Drag <dede7d@wp.pl>
2022-04-08 15:49:33 +0200

    added while loop

--- 02/app.js
+++ 02/app.js
@@ -27,15 +27,26 @@ function matchTable2() {
     const a = 3;
     const n = 4;
 
-
     let iter = 0;
+    let numbers = '';
+
+    let result = Math.pow(a, n);
 
     while (iter < n) {
+
+        if (iter > 0) {
+            numbers = numbers + '*';
+        }
+
+
+        numbers = numbers + a;
         iter++;
-        console.log(iter);
+
+
+
     }
 
-
+    console.log(numbers, ' = ', result);
 }
 
 
