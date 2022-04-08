b5d6c38f8fda9b898cbdd191ec2f8c6253afd015
Dawid Drag <dede7d@wp.pl>
2022-04-08 21:19:16 +0200

    added js code, getSum

--- 03/app.js
+++ 03/app.js
@@ -8,7 +8,7 @@ const c = randomNumber(min, max);
 console.log(a, b, c);
 
 
-
+getSum(a, b, c);
 
 
 
@@ -16,3 +16,30 @@ console.log(a, b, c);
 function randomNumber(min, max) {
     return Math.round((Math.random() * (max - min)) + min);
 }
+
+
+
+function getSum(param1, param2, param3) {
+    const param1Int = parseInt(param1);
+    const param2Int = parseInt(param2);
+    const param3Int = parseInt(param3);
+
+
+    const arr = [param1, param2, param3];
+    let max1 = 0;
+    let max2 = 0;
+
+
+    arr.sort(function (a, b) {
+        return b - a;
+    })
+
+    max1 = arr[0];
+    max2 = arr[1];
+
+
+    console.log('Suma dwóch największych liczb to: ', max1 + max2)
+}
+
+
+
