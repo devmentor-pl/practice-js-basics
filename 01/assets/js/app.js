c2205aa6227886b7d4089785922d828369fb3c5e
Marta Stelmach <marta.ms1961@gmail.com>
2022-04-10 20:52:41 +0200

    Finish task-1

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -2,3 +2,26 @@ const a = '4.2';
 const b = 9;
 
 console.log(a, b);
+console.log(typeof a, typeof b);
+
+const sum = parseFloat(a) + parseFloat(b);
+console.log(sum);
+
+const subtract = parseFloat(a) - parseFloat(b);
+console.log(subtract);
+
+const multiply = parseFloat(a) * parseFloat(b);
+console.log(Number(multiply.toFixed(1)));
+
+const divide = parseFloat(a) / parseFloat(b);
+console.log(divide);
+
+const results = [sum, subtract, Number(multiply.toFixed(1)), divide];
+
+for (let i = 0; i < results.length; i++) {
+    if (results[i] > 20) {
+        console.log(results[i] + " is greater than 20.");
+    } else {
+        console.log(results[i] + " is less or equal than 20.");
+    }
+}
