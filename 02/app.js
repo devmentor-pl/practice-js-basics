1b2478cf15f3474a5269f65b3afa8e287f299e58
Marcin Frackowiak <frackowiak.martin@gmail.com>
2022-04-11 23:15:39 +0200

    for & prompt

--- 02/app.js
+++ 02/app.js
@@ -1,11 +1,16 @@
 
 /* rozwiązanie z pętlą for */
-const x = 4;
+//const x = 4;
+const x = prompt('Podaj liczbę od 1-9');
+if (x < 1 || x > 9 ) {
+    console.log("Wpisana liczba nie mieści się w zakresie, przeładuj stronę!!!");
+} else {
+    for(let i=1; i<10; i++) {
+        let result = i * x;
+        console.log(i + " x " + x + ' = ' + result);
+    }
+} 
 
-for(let i=1; i<10; i++) {
-    let result = i * x;
-    console.log(i + " x " + x + ' = ' + result);
-}
 
 
 
