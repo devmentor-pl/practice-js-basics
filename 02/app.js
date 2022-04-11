f30dabf903a7d3926edbedd1fc47d585e82ac467
Marcin Frackowiak <frackowiak.martin@gmail.com>
2022-04-11 23:06:14 +0200

    for

--- 02/app.js
+++ 02/app.js
@@ -2,6 +2,11 @@
 /* rozwiązanie z pętlą for */
 const x = 4;
 
+for(let i=1; i<10; i++) {
+    let result = i * x;
+    console.log(i + " x " + x + ' = ' + result);
+}
+
 
 
 /* rozwiązanie z pętlą while  */
