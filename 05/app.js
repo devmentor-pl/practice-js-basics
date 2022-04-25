a699ba4fba1b2a0e9268441ca217914c45b894e0
Marcin Frackowiak <frackowiak.martin@gmail.com>
2022-04-25 18:17:26 +0200

    add avgGlobal function

--- 05/app.js
+++ 05/app.js
@@ -12,6 +12,9 @@ Student.prototype.addGrade = function(subject, grade) {
 }
 
 Student.prototype.avgSubject = function(subject) {
+    if(typeof this.grades[subject] === 'undefined'){
+        return 0;
+    }
     let avg = 0;
     let avgSubject = 0;
     for(let i = 0; i<this.grades[subject].length; i++) {
@@ -21,15 +24,25 @@ Student.prototype.avgSubject = function(subject) {
     return avgSubject;
 }
 
-// Student.prototype.avgGlobal = function() {
-//     let avg = 0;
-//     let avgGlobal = 0;
-//     for(let i = 0; i<this.grades[].length; i++) {
-//         avg += this.grades[][i];
-//     }
-//     avgGlobal = avg / this.grades[].length;
-//     return avgGlobal;
-// }
+Student.prototype.avgGlobal = function(subject) {
+    if(typeof subject === 'undefined'){
+        const grades = []
+        for(const key in this.grades) {
+            const arr = this.grades[key];
+        
+            arr.forEach(function(item){
+                grades.push(item)
+            })
+        }
+    let avg = 0;
+    let avgGlobal = 0;
+    for(let i = 0; i<grades.length; i++) {
+        avg += grades[i];
+    }
+    avgGlobal = avg / grades.length;
+    return avgGlobal;
+    }
+}
 
 
 const student1 = new Student('Jan', 'Nowak');
@@ -41,7 +54,7 @@ student1.addGrade('english', 3);
 student1.addGrade('english', 4);
 const avgMath = student1.avgSubject('maths');
 const avgEnglish = student1.avgSubject('english');
-// const avgGlobal = student1.avgGlobal();
+const avgGlobal = student1.avgGlobal();
 
 console.log(student1.grades);
 console.log(avgMath, avgEnglish);
