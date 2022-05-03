f5f03dfe219e80cc92a447ef4f470025b7ea5beb
Lukasz Szarkowski <lukas.szarkowski@gmail.com>
2022-05-03 13:43:16 +0200

    done 03 task

--- 03/app.js
+++ 03/app.js
@@ -7,12 +7,48 @@ const c = randomNumber(min, max);
 
 console.log(a, b, c);
 
+function getSum(num1, num2, num3) {
+	const arr = [parseInt(num1), parseInt(num2), parseInt(num3)];
+	arr.sort((a, b) => a - b);
+	return arr[1] + arr[2];
+}
 
 
 
+const isEven = function (number) {
+	if (number === null) {
+		console.log(null);
+	}
+	if (number % 2 === 0) {
+		console.log(true);
+	}
+
+	if (number % 2 !== 0) {
+		console.log(false);
+	}
+};
 
 
+
+function showInfo(val1, val2) {
+	switch (val2) {
+		case null:
+			console.log(`Podany argument ${val1} nie jest liczbą`);
+			break;
+		case true:
+			console.log(`Podany argument ${val1} jest parzysty`);
+			break;
+		case false:
+			console.log(`Podany argument ${val1} jest nieparzysty`);
+			break;
+	}
+}
 
 function randomNumber(min, max) {
-    return Math.round((Math.random() * (max - min)) + min);
+	return Math.round(Math.random() * (max - min) + min);
 }
+
+
+const sum = getSum(a, b, c);
+const even = isEven(sum);
+showInfo(sum, even);
