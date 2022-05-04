7a4ad0c1ef6d557242e36094a8b15a4906365c53
Patryk Kropop <patryk.kropop@gmail.com>
2022-05-04 14:45:58 +0200

    added for loop

--- 02/app.js
+++ 02/app.js
@@ -1,6 +1,10 @@
 
 /* rozwiązanie z pętlą for */
-const x = 4;
+// const x = 4;
+const x = prompt('Podaj liczbę!')
+for(i = 1; i < 10; i++){
+    console.log(x + ' x ' + i + ' = ' + (x * i))
+}
 
 
 
