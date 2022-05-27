f52542ccb90070391017a1c069eb6922f5b1acaa
Ada <adriannazalewska1@gmail.com>
2022-05-27 15:17:15 +0200

    01 done

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -2,3 +2,24 @@ const a = '4.2';
 const b = 9;
 
 console.log(a, b);
+
+console.log(typeof a)
+console.log(typeof b)
+
+let res1 = Number(a) + b;
+let res2 = a - b;
+let res3 = a * b;
+let res4 = a / b;
+let res5 = a % b;
+
+console.log(res1, res2, res3, res4, res5);
+
+const equationResult = [res1, res2, res3, res4, res5]
+
+equationResult.forEach(function (res) {
+    if (res < 20) {
+        console.log( res + ' is less than 20')
+    } else {
+        console.log( res + ' is more than 20')
+    }
+})
