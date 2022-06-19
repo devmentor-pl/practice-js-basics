041e65096efb8c3dd5e0366f8b13cc6a916fd817
Szymon Ludwikowski <ludwikowski.szym1993@gmail.com>
2022-06-19 19:10:50 +0200

    task 1 done

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -1,4 +1,30 @@
-const a = '4.2';
+const a = "4.2";
 const b = 9;
 
 console.log(a, b);
+console.log(typeof a);
+console.log(typeof b);
+
+let addition = a + b;
+let subtraction = a - b;
+let multiplication = a * b;
+let division = a / b;
+let modulo = a % b;
+let exponent = a ** b;
+// console.log(addition, subtraction, multiplication, division, modulo, exponent);
+console.log("addition: " + addition);
+console.log("subtraction: " + subtraction);
+console.log("multiplication: " + multiplication);
+console.log("division: " + division);
+console.log("modulo: " + modulo);
+console.log("exponent: " + exponent);
+const sumArr = [addition, subtraction, multiplication, , division, modulo, exponent];
+const moreThenTwenty = sumArr.map(Number).filter(function (e) {
+	return e > 20;
+});
+const lessThenTwenty = sumArr.map(Number).filter(function (e) {
+	return e < 20;
+});
+
+console.log("More then 20: " + moreThenTwenty);
+console.log("Less then 20: " + lessThenTwenty);
