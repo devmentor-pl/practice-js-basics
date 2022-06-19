dec594bd9f3d8d9e871ba7888ba2b40d36c12839
Szymon Ludwikowski <ludwikowski.szym1993@gmail.com>
2022-06-19 20:01:44 +0200

    task 2 done

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,34 @@
-
 /* rozwiązanie z pętlą for */
-const x = 4;
+const x = parseInt(prompt("Podaj liczbę całkowitą od 1 do 9:"));
 
-
+if (x >= 1 && x <= 9 && Number.isInteger(x) === true) {
+	console.log("Mnożenie:");
+	for (let i = 1; i < 10; i++) {
+		console.log(`${i} x ${x} = ${i * x}`);
+	}
+} else {
+	alert("Coś poszło nie tak!");
+	console.log("Błędne dane");
+}
 
 /* rozwiązanie z pętlą while  */
+const a = parseInt(prompt("Podaj podstawę potęgowania (liczba całkowita > 0):"));
+const n = parseInt(prompt("Podaj wykładnik potęgi (liczba całkowita > 0):"));
+
+if (a > 0 && n > 0 && Number.isInteger(a) === true && Number.isInteger(n) === true) {
+	let index = 1;
+	let result = 1;
+	let stringResult = "";
+	console.log("Potęgowanie:");
+	while (index <= n) {
+		result *= a;
+		stringResult += a + " ";
+		if (index < n) stringResult += "* ";
+		if (index === n) stringResult += "= " + result;
+		index++;
+	}
+	console.log(stringResult);
+} else {
+	alert("Coś poszło nie tak!");
+	console.log("Błędne dane");
+}
