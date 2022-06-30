f8f40dbf879c7ebc18685c6a41050fd395868db1
Andrej Kaczanowski <andykaczanowski@gmail.com>
2022-06-30 11:04:57 +0200

    Fixed task 02

--- 02/app.js
+++ 02/app.js
@@ -13,15 +13,18 @@
 /* rozwiązanie z pętlą while  */
 
 let a = 3;
-let n = 2;
+let n = 0;
 
-while (a <= 9 && a >= 1 && n <= 9 && n >= 1) {
+while (a <= 9 && a >= 1 && n <= 9 && n >= 0) {
 	let result = a ** n;
 	let str = a;
-	for (let i = 1; i < n; i++) {
+	for (let i = 1; i <= n; i++) {
 		str = str + "*" + a;
 	}
 	str = str + "=" + result;
+	if (n === 0) {
+		str = `Potęgowanie przez 0 zawsze da wynik ${result}`;
+	}
 	if (result > 0) {
 		console.log(str);
 		break;
