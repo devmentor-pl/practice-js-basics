c503d7ac5e33c757de1ff6b5e0431cbb49ac80d2
Jacek Zielinski <gandi34@wp.pl>
2022-08-10 23:08:18 +0200

    mathematic operations

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -1,4 +1,24 @@
-const a = '4.2';
+const a = 4.2;
 const b = 9;
 
 console.log(a, b);
+
+
+console.log(typeof a);
+console.log(typeof b);
+
+let result = a+b;
+console.log (result);
+
+let subtraction = a-b;
+console.log (subtraction);
+
+let multiplication = a*b;
+console.log (multiplication);
+
+let split = a/b;
+console.log (split);
+
+
+
+
