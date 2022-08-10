49b0606a7474eb461e4e070d722c34739940c797
Jacek Zielinski <gandi34@wp.pl>
2022-08-10 23:36:54 +0200

    01 done

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -19,6 +19,43 @@ console.log (multiplication);
 let split = a/b;
 console.log (split);
 
+const test =('Wyniki są poprawne');
+console.log (test);
+
+const firstResult = 13.2;
+const secondResult = -4.8;
+const thirdResult = 37.800000000000004;
+const fourthResult = 0.4666666666666667;
+
+let first = 13.2;
+ if (first > 20){
+    console.log ('bigger');
+ }else{
+    console.log ('not bigger');
+ }
+
+ let second = -4.8;
+ if (second > 20){
+    console.log ('Fine');
+ }else{
+    console.log ('not Fine');
+ }
+
+ let third = 37.8;
+ if (third > 20){
+    console.log ('Yes');
+ }else{
+    console.log ('No');
+ }
+
+
+ let fourth = 0.46666;
+ if (fourth > 20){
+    console.log ('all right');
+ }else{
+    console.log ('not all right');
+ }
+
 
 
 
