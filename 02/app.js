04c19f9a57401b997ad00e3935e2570f1705947f
Jacek Zielinski <gandi34@wp.pl>
2022-08-15 18:58:12 +0200

    Multitable for done

--- 02/app.js
+++ 02/app.js
@@ -1,6 +1,14 @@
 
 /* rozwiązanie z pętlą for */
-const x = 4;
+const x=4;
+let result = 0;
+
+const number = parseInt(prompt('Enter an integer: '));
+
+for (let i = 1; i<=9;i++){
+    result = i*number;
+    console.log(`${number} * ${i} = ${result}`);
+}
 
 
 
