9a73800bceda5f72c528a1d46fcbde527767a296
Jacek Zielinski <gandi34@wp.pl>
2022-08-15 20:41:05 +0200

    Multitable wtih range for loop

--- 02/app.js
+++ 02/app.js
@@ -3,13 +3,17 @@
 const x=4;
 let result = 0;
 
-const number = parseInt(prompt('Enter an integer: '));
+const number = parseInt(prompt('Enter a number: '));
 
-for (let i = 1; i<=9;i++){
-    result = i*number;
+const range = parseInt(prompt('Enter a range: '));
+
+for (let i = 1; i<=range;i++){
+    result = number*i;
     console.log(`${number} * ${i} = ${result}`);
 }
 
 
 
-/* rozwiązanie z pętlą while  */
+/* rozwiązanie z pętlą while*/
+
+
