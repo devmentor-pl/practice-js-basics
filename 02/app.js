be5809dd61e24d3165d9d4ebc3f2dead72a515b9
Jacek Zielinski <gandi34@wp.pl>
2022-08-16 14:47:20 +0200

    02 done

--- 02/app.js
+++ 02/app.js
@@ -16,4 +16,21 @@ for (let i = 1; i<=range;i++){
 
 /* rozwiązanie z pętlą while*/
 
+let a = parseInt(prompt('Type number'))
+let b = parseInt(prompt('Type number 2'))
+if(isNaN(a) || isNaN(b)) {
+  console.log("Bad number")
+}
+else {
+  let expo = 1;
+  let i = 1;
+  while (i <= b) {
+        i++;
+        expo = expo * a
+    }
+  console.log(expo)
+}
+
+
+
 
