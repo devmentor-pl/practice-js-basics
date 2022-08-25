1e44f7305156f892d5d67d12b386804f1aed7573
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2022-08-25 15:48:03 +0200

    Add js code

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -2,3 +2,23 @@ const a = '4.2';
 const b = 9;
 
 console.log(a, b);
+
+console.log(typeof a);
+console.log(typeof b);
+
+const add = parseFloat(a)+ b;
+console.log(add);
+const sub = a - b;
+console.log(sub);
+const multi = parseFloat(a) * b;
+console.log(multi);
+const div = a / b;
+console.log(div);
+const modulo = a % b;
+console.log(modulo);
+
+if(a>b){
+    console.log(a);
+}else{
+    console.log(b);
+}
--- 02/app.js
+++ 02/app.js
@@ -2,6 +2,15 @@
 /* rozwiązanie z pętlą for */
 const x = 4;
 
-
+for(let i = 1; i<=9; i++){
+    console.log(x + ' x ' + i + ' = ' + i * x );
+}
 
 /* rozwiązanie z pętlą while  */
+
+let multiplier = 1;
+
+while(multiplier<10){
+    console.log(x + ' x ' + multiplier + ' = ' + x * multiplier);
+    multiplier++;
+}
