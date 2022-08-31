4d0b3ccc9bddebcda4a74b762ed4001a0ae8d9e3
Jacek Zielinski <gandi34@wp.pl>
2022-08-31 20:00:56 +0200

    04 numbers

--- 04/app.js
+++ 04/app.js
@@ -3,14 +3,22 @@ const max = 200;
 
 
 function arrRange(min,max) {
-    Math.floor(Math.random()*200);
+    
     let arr = [];
+    Math.floor(Math.random()*(min - max));
     for (let i=min; i<=max; i++)
     arr.push(i);
     return arr;
+    
 };
+    console.log (arrRange(min,max));
 
-console.log (arrRange(min,max));
+
+function getNumnbers () {
+    
+}
+
+
 
 
 
