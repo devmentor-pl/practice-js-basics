0fde97df37e36fce2256ec70b7e283336fa7de5f
Jacek Zielinski <gandi34@wp.pl>
2022-08-31 19:29:55 +0200

    04 arr

---
+++ 04/app.js
@@ -0,0 +1,23 @@
+const min = 10;
+const max = 200;
+
+
+function arrRange(min,max) {
+    Math.floor(Math.random()*200);
+    let arr = [];
+    for (let i=min; i<=max; i++)
+    arr.push(i);
+    return arr;
+};
+
+console.log (arrRange(min,max));
+
+
+
+
+
+  
+
+
+
+
