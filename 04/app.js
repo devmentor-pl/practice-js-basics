a515230f8aff1d70486c702e34fe2a481e53c1a7
Jacek Zielinski <gandi34@wp.pl>
2022-09-06 11:05:55 +0200

    04 functions and methods

--- 04/app.js
+++ 04/app.js
@@ -1,14 +1,22 @@
-const arr = [];
-
-for (let i=0;i<=200;i++){
-    arr.push(randomNumbers(10,200));
-}
-    console.log(arr);
+let arr = [];
 
 function randomNumbers(min,max) {
     return Math.floor(Math.random()*(max-min) + min)
 }
-   
+
+for (let i=0;i<=20;i++){
+    arr.push(randomNumbers(10,200));
+}
+
+const newArr = arr;
+console.log(newArr);
+
+
+
+
+
+
+
 
 
 
