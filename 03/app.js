5741e9b0e69fe1cc18fe5f01c979f72ccf19bf01
Anna <aniamatrackasadurek@gmail.com>
2022-11-10 11:22:19 +0100

    exercise 3 finished

--- 03/app.js
+++ 03/app.js
@@ -5,7 +5,7 @@ const a = randomNumber(min, max);
 const b = randomNumber(min, max);
 const c = randomNumber(min, max);
 
-console.log(a, b, c);
+console.log('Wylosowane liczby to: ' , a, b, c);
 
 function getSum(x, y, z) {
     x = parseInt(x);
@@ -22,22 +22,43 @@ function getSum(x, y, z) {
 }
 
 const isEven = function (num) {
-    if (typeof num === 'number' && num % 2 === 0) {
+    if (typeof num !== 'number') {
+        return null
+    } else if (num % 2 === 0) {
         return true
-    } else if (typeof num === 'number' && num % 2 !== 0) {
-        return false
-    } else {
-        return null;
-    }   
+    } else {return false}
 }
 
-
-
-
-
-
-
+function showInfo(d, e) {
+    switch(e) {
+        case null: 
+            console.log('Podany argument ', e , 'nie jest liczbą');
+            break;
+        case true:
+            console.log('Podany argument ', e , 'jest parzysty');
+            break;
+        case false:
+            console.log('Podany argument ', e , 'jest nieparzysty');
+            break;
+        default:
+            alert('Błąd danych!');
+            break;
+    }
+}
 
 function randomNumber(min, max) {
     return Math.round((Math.random() * (max - min)) + min);
 }
+
+const sum = getSum(a, b, c);
+const even = isEven(sum);
+
+console.log('Suma dwóch największych liczb to: ' , sum);
+
+showInfo(sum, even);
+
+
+
+
+
+
