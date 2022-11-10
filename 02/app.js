e0effcfc5041f023cd431e8793f1e6081508bcc0
Anna <aniamatrackasadurek@gmail.com>
2022-11-10 10:22:45 +0100

    exercise 2 finished only 1st part

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,25 @@
 
 /* rozwiązanie z pętlą for */
-const x = 4;
+const x = parseInt(prompt('Please enter a number from 1 to 9'))
+let resultMultiplication;
+
+if( x < 1 && x >= 10) {
+    alert('invalid number');
+} else {
+    multiplication()
+}
+
+function multiplication() {
+    for(let i = 1; i < 10; i++) { 
+        resultMultiplication = x * i
+        console.log(i + ' x ' + x + ' = ' + i * x)
+}};
 
 
 
 /* rozwiązanie z pętlą while  */
+
+const y = parseInt(prompt('Please enter a number from 1 to 9'));
+const z = parseInt(prompt('Enter an index'));
+
+
