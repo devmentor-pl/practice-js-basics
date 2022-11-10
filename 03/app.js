bc405730e707f254e27663a8a7974a80b5b4b707
Anna <aniamatrackasadurek@gmail.com>
2022-11-10 10:44:40 +0100

    exercise 3 finished 1st part

--- 03/app.js
+++ 03/app.js
@@ -7,6 +7,31 @@ const c = randomNumber(min, max);
 
 console.log(a, b, c);
 
+function getSum(x, y, z) {
+    x = parseInt(x);
+    y = parseInt(y);
+    z = parseInt(z);
+
+    if(x > z && y > z) {
+        return x + y;
+    } else if(y > x && z > x) {
+        return y + z
+    } else {
+        return x + z
+    }
+}
+
+const isEven = function (num) {
+    if (typeof num === 'number' && num % 2 === 0) {
+        return true
+    } else if (typeof num === 'number' && num % 2 !== 0) {
+        return false
+    } else {
+        return null;
+    }   
+}
+
+
 
 
 
