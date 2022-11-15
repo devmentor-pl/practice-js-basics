87dae6f1cd31ace7187698cf227b8cc763fb5dac
Tomek2112 <kaminski_tomasz@interia.pl>
2022-11-15 13:52:19 +0100

    done

--- 02/app.js
+++ 02/app.js
@@ -1,21 +1,55 @@
 
 /* rozwiązanie z pętlą for */
-// const x = prompt('Podaj liczbę!');
-// for(let i=0; i<=8; i++) {
-//     i++
-//     console.log(x, 'x', i, '=', x * i);
-//     i--;
+const x = prompt('Podaj liczbę!');
+if(isNaN(Number(x))) {
+    throw new Error('Podano błędną liczbę :/ ')
+}
+for(let i=0; i<=8; i++) {
+    i++
+    console.log(x, 'x', i, '=', x * i);
+    i--;
 
-// };
+};
 
 // /* rozwiązanie z pętlą while  */
 
 
-let i = prompt('Podaj liczbę!');
-
-while(i < 10) {
-    console.log(i);
-    i += 1;
+let a = prompt('Podaj podstawę:');
+let n = prompt('Podaj wykładnik:');
+if(isNaN(Number(a)) || (isNaN(Number(n)))) {
+    throw new Error('Podano błędne liczby');
+}
+if(Number(n) === 0) {
+    console.log('Liczba podniesiona do potęgi 0 wynosi 1') 
+} else {
+    let j = 1;
+    let someNumber = 0;
+    while (j < n) {
+        j++;
+        someNumber = someNumber * a;
+    }
+    console.log(Number(n));
 }
 
+// coś tu nie gra... 
+
+
+//rozwiązanie z stack
+// function exponentiate (base, power) {
+//     let count = 0
+//     let result = 1;
+//     if (power === 0){
+//       return 1
+//     }
+  
+//     // while loop 
+//     while (count < power){  
+  
+//       result *= base
+//       count += 1
+//     }
+//     return result 
+//   }
+  
+//   console.log(exponentiate(3, 3))
 
--- 03/app.js
+++ 03/app.js
@@ -7,33 +7,44 @@ const c = randomNumber(min, max);
 
 console.log(a, b, c);
 
-
-
-
-
-
 function randomNumber(min, max) {
     return Math.round((Math.random() * (max - min)) + min);
 }
 
-function getSum(x, y, z) {
-    console.log(x + y + z)
- }
- getSum(4, 6, 7);
+function getSum(a, b, c) {
+    if (a > b && a > c) {
+        return (b > c) ? (a + b) : (a + c);
+    } else if (b > c) {
+        return (a > c) ? (a + b) : (b + c);
+    } else {
+        return (a > b) ? (a + c) : (b + c);
+    }
+}
+const sum = getSum(a, b, c);
 
 
- const isEven = function {
+const isEven = function (num) {
+    if (isNaN(Number(num))) {
+        return null;
+    } else {
+        return !(num % 2) ? true : false;
+    }
+}
 
- }
- isEven();
+const even = isEven(sum);
 
- 
+function showInfo(num, par) {
+    switch (par) {
+        case false:
+            console.log(`Podany argument ${num} jest nieparzysty`);
+            break;
+        case true:
+            console.log(`Podany argument ${num} jest parzysty`);
+            break;
+        default:
+            console.log(`Podany argument ${num} nie jest liczbą`);
+            break;
+    }
+}
 
- showInfo() {
-
- }
- showInfo(); 
-
-
-
- 
+showInfo(sum, even);
