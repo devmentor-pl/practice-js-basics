dcf13818af57920ff24ac176e86adea471ca4765
zimiier <matmatwiej@gmail.com>
2022-11-24 11:10:03 +0100

    Finish task 3

--- 03/app.js
+++ 03/app.js
@@ -7,12 +7,48 @@ const c = randomNumber(min, max);
 
 console.log(a, b, c);
 
+const isEven = function (x) {
+	if (typeof x === 'number') {
+		if (x % 2 == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	} else {
+		return null;
+	}
+};
 
+const sum = getSum(a, b, c);
+const even = isEven(sum);
+showInfo(sum, even);
 
+function getSum(x, y, z) {
+	const xInt = parseInt(x);
+	const yInt = parseInt(y);
+	const zInt = parseInt(z);
+	if (xInt >= zInt && yInt >= zInt) {
+		return xInt + yInt;
+	} else if (zInt >= xInt && yInt >= xInt) {
+		return zInt + yInt;
+	} else if (xInt >= yInt && zInt >= yInt) {
+		return xInt + zInt;
+	}
+}
 
-
-
+function showInfo(x, y) {
+	switch (y) {
+		case true:
+			console.log(`Podany argument ${x} jest parzysty`);
+			break;
+		case false:
+			console.log(`Podany argument ${x} jest nieparzysty`);
+			break;
+		default:
+			console.log(`Podany argument ${x} nie jest liczbą`);
+	}
+}
 
 function randomNumber(min, max) {
-    return Math.round((Math.random() * (max - min)) + min);
+	return Math.round(Math.random() * (max - min) + min);
 }
