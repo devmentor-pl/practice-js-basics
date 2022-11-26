bb9e4e42182e7dadcf9f3c45f010028730625559
Miłosz Szmulewicz <miloszszmulewicz@gmail.com>
2022-11-26 12:10:41 +0100

    04 done

---
+++ 04/app.js
@@ -0,0 +1,39 @@
+const min = 10;
+const max = 200;
+const randomArray = createArray(min, max);
+const largest = getLargest(randomArray);
+const avg = getAvg(largest);
+
+console.log("losowa tablica: ",randomArray);
+console.log("10 największych liczb: ",largest);
+console.log("średnia 10 najwiękzych liczb wynosi: ",avg);
+
+function createArray(min, max) {
+    const tab = [];
+    for(let i = 0; i < 20; i++) {
+        tab.push(Math.floor(Math.random()*(max - min) + min + 1));
+    }
+    // console.log(tab);
+    return tab;
+}
+
+function getLargest(arr) {
+    arr.sort(function(a, b) {
+        return a - b;
+    });
+    return arr.slice(arr.length/2, arr.length);
+
+}
+
+
+function getAvg(arr) {
+    let sum = 0;
+    arr.forEach(function(num) {
+        sum += num;
+    });
+
+    return sum/arr.length;
+}
+
+// const avg = getAvg(largest);
+// console.log("średnia 10 najwiękzych liczb wynosi: ",avg);
