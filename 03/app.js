afca9ce3b2dc0f75bc8b446ea250e88789c25f0f
mateuszjansowa <mateusz.jan.sowa@gmail.com>
2022-12-06 10:01:34 +0100

    task 03

--- 03/app.js
+++ 03/app.js
@@ -7,12 +7,47 @@ const c = randomNumber(min, max);
 
 console.log(a, b, c);
 
+const isEven = function (a) {
+    if (a && typeof a !== 'number') {
+        return null;
+    }
 
-
-
-
-
+    return a % 2 === 0;
+};
 
 function randomNumber(min, max) {
-    return Math.round((Math.random() * (max - min)) + min);
+    return Math.round(Math.random() * (max - min) + min);
 }
+
+function getSum(a, b, c) {
+    const intA = parseInt(a);
+    const intB = parseInt(b);
+    const intC = parseInt(c);
+
+    const numbers = [intA, intB, intC].sort((a, b) => b - a);
+    return numbers[0] + numbers[1];
+}
+
+function showInfo(value, boolean) {
+    switch (boolean) {
+        case null: {
+            console.log(`Podany argument ${value} nie jest liczba`);
+            break;
+        }
+        case true: {
+            console.log(`Podany argument ${value} jest parzysty`);
+            break;
+        }
+        case false: {
+            console.log(`Podany argument ${value} jest nieparzysty`);
+            break;
+        }
+        default: {
+            break;
+        }
+    }
+}
+
+const sum = getSum(a, b, c);
+const even = isEven(sum);
+showInfo(sum, even);
