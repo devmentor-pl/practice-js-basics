ef0cc612bd1b44754ec75d0c3d7a397ff505d3ad
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2023-01-18 13:23:31 +0100

    Add ex 3 getSum function

--- 03/app.js
+++ 03/app.js
@@ -7,12 +7,21 @@ const c = randomNumber(min, max);
 
 console.log(a, b, c);
 
-
-
-
-
-
-
 function randomNumber(min, max) {
-    return Math.round((Math.random() * (max - min)) + min);
+	return Math.round(Math.random() * (max - min) + min);
+}
+
+function getSum(one, two, three) {
+	const oneParsed = parseFloat(one);
+	const twoParsed = parseFloat(two);
+	const threeParsed = parseFloat(three);
+
+	const numbersArr = [oneParsed, twoParsed, threeParsed];
+
+	numbersArr.sort(function (a, b) {
+		return a - b;
+	});
+
+	console.log(numbersArr[1] + numbersArr[2]);
+	return numbersArr[1] + numbersArr[2];
 }
