6915e1f71dc0c5ecda8fee7adb8173124df15663
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2023-01-18 12:46:53 +0100

    Add ex 2 for loop

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,19 @@
+/* rozwiązanie z pętlą for  */
 
-/* rozwiązanie z pętlą for */
-const x = 4;
+// const x = 4;
 
+// for (let i = 1; i <= 9; i++) {
+// console.log(x + " x " + i + " = " + x * i);
+// }
 
+const x = prompt("Podaj liczbę!");
+const parsedX = parseInt(x);
+console.log(parsedX);
+
+if (parsedX >= 1 && parsedX <= 9) {
+	console.log(parsedX + " is between 1 and 9");
+} else {
+	console.log(parsedX + " is less than 1 or more than 9");
+}
 
 /* rozwiązanie z pętlą while  */
