7576d218616fe28009f1c2cf20b2280a52530822
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2023-01-18 17:28:23 +0100

    Add ex 4 sortArray function

--- 04/app.js
+++ 04/app.js
@@ -3,15 +3,23 @@ function createArray(min, max) {
 
 	for (let i = 1; i <= 20; i++) {
 		let randomNumber = Math.round(Math.random() * (max - min + 1) + min);
-
 		arr.push(randomNumber);
 	}
-	const sortedArr = arr.sort(function (a, b) {
-		return a - b;
-	});
 
-	return sortedArr;
+	return arr;
 }
 
 const arr = createArray(1, 100);
 console.log(arr);
+
+function sortArray(array) {
+	const sorted = array.sort(function (a, b) {
+		return b - a;
+	});
+	console.log(sorted);
+
+	return sorted.slice(0, 10);
+}
+
+const sortedArray = sortArray(arr);
+console.log(sortedArray);
