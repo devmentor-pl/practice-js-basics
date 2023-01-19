d39760ddfd673ae35ed553e85731993413d49d4d
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2023-01-19 12:19:36 +0100

    Try to fix ex 5

--- 05/app.js
+++ 05/app.js
@@ -12,12 +12,35 @@ Student.prototype.addGrade = function (subject, grade) {
 	this.grades[subject].push(grade);
 };
 
-// Student.prototype.getSubjectAverage(subject){
-// }
+Student.prototype.getAverageGrade = function (subject) {
+	if (subject === "undefined") {
+		const allGrades = [];
+
+		for (const key in this.grades) {
+			allGrades.push(this.grades[key]);
+		}
+
+		let sum = 0;
+		allGrades.forEach(function (element) {
+			sum = sum + element;
+			return sum;
+		});
+
+		return sum / allGrades.length;
+	} else {
+		const grades = this.grades["subject"];
+		let sum = 0;
+		grades.forEach(function (element) {
+			return (sum = sum + element);
+		});
+	}
+
+	return sum / grades.length;
+};
 
 Student.prototype.getTotalAverage = function (arr) {
 	const sum = arr.reduce(function (acc, total) {
-		return acc + price;
+		return acc + total;
 	}, 0);
 
 	return sum / arr.length;
@@ -30,5 +53,6 @@ student.addGrade("maths", 6);
 student.addGrade("english", 3);
 console.log(student);
 
-// const avgMath = student.getAverageGrade("math"); // 5
-// const avg = student.getAverageGrade(); // 4.33
+const avgMath = student.getAverageGrade("math"); // 5
+const avg = student.getAverageGrade(); // 4.33
+console.log(avg);
