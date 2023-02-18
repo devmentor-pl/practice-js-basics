8a6528d98fdf377ae939662924b70e72f3959836
Wioleta Lipinska <hello@wiolipcreates.pl>
2023-02-19 00:40:23 +0100

    taks02

--- 02/app.js
+++ 02/app.js
@@ -1,6 +1,25 @@
 
 /* rozwiązanie z pętlą for */
-const x = 4;
+const userNumber = Number(prompt('Podaj liczbę!....'));
+
+function checkNumber(num) {
+    if (!typeof num === 'number' || Number.isNaN(num)) {
+
+        alert(' Wartość powinna być liczbą');
+        return;
+
+    } else {
+        for (let i = 0; i < 10; i++) {
+            const result = userNumber * i;
+            console.log('wynik mnożenia ' + userNumber + ' x ' + i + ' = ' + result)
+
+        }
+    }
+
+}
+checkNumber(userNumber);
+
+
 
 
 
