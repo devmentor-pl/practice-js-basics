ee0ca4dcbec480cb2738cb05281caa976ea873ec
Ewa Kulpinska <eva.kulpinska@gmail.com>
2023-02-24 15:01:40 +0100

    Completed for 'if' in task 2

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,13 @@
 
 /* rozwiązanie z pętlą for */
-const x = 4;
+const x = prompt('Podaj liczbę!');
 
-
+if( x <= 9 && x>0){
+    for(let i=1; i<=9; i++){
+        console.log(x + ' x ' + i + ' = ' + x*i);
+     }
+}else{
+    console.log('Podaj liczbę od 1 do 9');
+}
 
 /* rozwiązanie z pętlą while  */
