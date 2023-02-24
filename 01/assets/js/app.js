e1497908657583ded802020db622d30749f8d159
Ewa Kulpinska <eva.kulpinska@gmail.com>
2023-02-24 14:26:04 +0100

    Finished task 1

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -2,3 +2,30 @@ const a = '4.2';
 const b = 9;
 
 console.log(a, b);
+
+console.log(typeof a);
+console.log(typeof b);
+
+const c = parseInt(a);
+
+const res1 = c + b;
+const res2 = c - b;
+const res3 = c * b;
+const res4 = c / b;
+const res5 = c % b;
+
+console.log(res1, res2, res3, res4, res5);
+
+function compareTo20(param) {
+if(param > 20) {
+    console.log(param + ' is bigger than 20');
+}else{
+    console.log(param + ' is smaller than 20' );
+}
+}
+
+compareTo20(res1);
+compareTo20(res2);
+compareTo20(res3);
+compareTo20(res4);
+compareTo20(res5);
