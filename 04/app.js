30cb92f597daf7481898f4d065af397486892516
Ewa Kulpinska <eva.kulpinska@gmail.com>
2023-02-25 16:02:49 +0100

    Added getLargest function

--- 04/app.js
+++ 04/app.js
@@ -1,9 +1,26 @@
+const arr = createRandomArray(1,100);
+
 function createRandomArray(min, max){
     const arr = [];
-    const max = 20;
-    for(i=0; i<max; i++){
+    const elNum = 20;
+    for(i=0; i<elNum; i++){
         arr.push(Math.round(Math.random() * (max-min) + min ));
     }
 
     return arr;
 }
+
+console.log(arr);
+
+function getLargest(arr){
+    arr.sort(function(a, b){
+        return b - a;
+    });
+    const largestArr = arr.slice(1,11);
+
+    return largestArr;
+}
+
+const largest = getLargest(arr);
+
+console.log(largest);
