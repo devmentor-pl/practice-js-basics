d4a0b40585afff9e87f0ee4b40a84ded6bb80dd9
Paweł Niedużak <93704910+czarsbenemoriendi@users.noreply.github.com>
2023-03-11 00:22:45 +0100

    feat: finish first task'

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -1,4 +1,41 @@
-const a = '4.2';
-const b = 9;
+let a = 4.2;
+let b = 9;
 
 console.log(a, b);
+console.log(typeof a, typeof b);
+
+// const addition = (a, b) => {
+// 	return a + b;
+// };
+// const subtraction = (a, b) => {
+// 	return a - b;
+// };
+// const multiplication = (a, b) => {
+// 	return a * b;
+// };
+// const exponentiation = (a, b) => {
+// 	return a ** b;
+// };
+// const division = (a, b) => {
+// 	return a / b;
+// };
+// const modulus = (a, b) => {
+// 	return a % b;
+// };
+
+const addition = a + b;
+const subtraction = a - b;
+const multiplication = a * b;
+const exponentiation = a ** b;
+const division = a / b;
+const modulus = a % b;
+
+const checkNumber = (operation, operationName = 'operation') => {
+	console.log(`Result: ${operation}`);
+	if (operation >= 20) {
+		console.log(`Result of ${operationName} is greater or equal 20`);
+	} else {
+		console.log(`Result of ${operationName} is less than 20`);
+	}
+};
+checkNumber(subtraction, 'addition');
