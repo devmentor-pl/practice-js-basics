2a2ef75c63e2e02375760ac399aabe925d5f796c
Paweł Niedużak <93704910+czarsbenemoriendi@users.noreply.github.com>
2023-03-11 01:18:06 +0100

    feat: finish second task

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,17 @@
+const x = 4 || prompt(`Podaj liczbę!`);
 
 /* rozwiązanie z pętlą for */
-const x = 4;
 
-
-
+// for (let i = 1; i < 10; i++) {
+// 	console.log(`${x} x ${i} = ${x * i}`);
+// }
 /* rozwiązanie z pętlą while  */
+
+const exponentiate = (a, b) => {
+	while (a <= b || a >= b) {
+		let sum = a ** b;
+		console.log(`${a} * `.repeat(b - 1) + a + ` = ${sum}`);
+		break;
+	}
+};
+exponentiate(3, 1);
