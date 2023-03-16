e99f8491909360aa9107cb5d3ec57337b2a4f2f9
Piotr Mazurek <mazurekp93@gmail.com>
2023-03-16 23:25:15 +0100

    Finish task-01

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -1,17 +1,18 @@
 const a = '4.2';
 const b = 9;
+console.log(typeof a, typeof b)
 
 console.log(a, b);
-const sum = a + b
+const sum = parseFloat(a) + b
+console.log(sum)
 const minus = a - b
-const multiply = a * b
-const split = a / b
-const modulo = a % b
-const pow = Math.pow(a, b)
+console.log(minus)
+const multiply = (a * b).toFixed(1)
+console.log(multiply)
+const split = (a / b).toFixed(1)
+console.log(split)
 
-if(sum <20 ){
-    console.log('Mniejsze')
-}
-else {
-    console.log('Większe')
-}
+
+const arr = [sum, minus, multiply, split]
+
+arr.forEach(el => el > 20 ? console.log(`${el} jest większy od 20`) : console.log(`${el} jest mniejszy lub równy 20`))
