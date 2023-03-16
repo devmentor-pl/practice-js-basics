928ceaa11b432902721024d24f27926658df8629
Piotr Mazurek <mazurekp93@gmail.com>
2023-03-16 14:06:44 +0100

    Finish task-02

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -2,3 +2,16 @@ const a = '4.2';
 const b = 9;
 
 console.log(a, b);
+const sum = a + b
+const minus = a - b
+const multiply = a * b
+const split = a / b
+const modulo = a % b
+const pow = Math.pow(a, b)
+
+if(sum <20 ){
+    console.log('Mniejsze')
+}
+else {
+    console.log('Większe')
+}
