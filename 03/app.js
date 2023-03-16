9da05f6806a9304e6c561e8ea4f2bc3d6b4e0958
Piotr Mazurek <mazurekp93@gmail.com>
2023-03-16 15:59:16 +0100

    Finish task-03

--- 03/app.js
+++ 03/app.js
@@ -7,11 +7,32 @@ const c = randomNumber(min, max);
 
 console.log(a, b, c);
 
+function getSum(a, b, c) {
+    const sortedNum = [parseInt(a), parseInt(b), parseInt(c)].sort((a, b) => b - a)
+    return sortedNum[0] + sortedNum[1]
+}
+const sum = getSum(a, b, c)
 
+const isEven = function (a) {
+    return typeof a !== "number" ? null : a % 2 === 0
 
-
-
-
+}
+function showInfo(a, b) {
+    switch (b) {
+        case null:
+            console.log(`Podany argument ${a} nie jest liczbą`)
+            break
+        case true:
+            console.log(`Podany argument ${a} jest liczbą parzystą`)
+            break
+        case false:
+            console.log(`Podany argument ${a} jest liczbą nieparzystą`)
+            break
+        default:
+            console.log(`Nie spełniono warunków weryfikacji`)
+    }
+}
+showInfo(sum, isEven(sum))
 
 function randomNumber(min, max) {
     return Math.round((Math.random() * (max - min)) + min);
