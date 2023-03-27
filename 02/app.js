ee1dd84e5f630553da0343ddb976270f9a910d5c
Mikolaj Krzywacz <mikolajkrzywacz@gmail.com>
2023-03-27 15:30:26 +0200

    finished for loop task

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,14 @@
-
 /* rozwiązanie z pętlą for */
-const x = 4;
 
+let x;
 
+do {
+    x = parseInt(prompt('Podaj liczbę z przedziału 1 - 9!'));
+  } while (x < 1 || x > 9);
+
+for (let i = 1; i < 10; i++) {
+    console.log(`${x} x ${i} = ${x * i}`);
+}
 
 /* rozwiązanie z pętlą while  */
+
