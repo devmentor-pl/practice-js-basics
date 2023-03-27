afec151bcf450405dd93af59e99c914e15aa563c
Mikolaj Krzywacz <mikolajkrzywacz@gmail.com>
2023-03-27 16:05:34 +0200

    uncomment piece of code

--- 02/app.js
+++ 02/app.js
@@ -1,14 +1,14 @@
 /* rozwiązanie z pętlą for */
 
-// let x;
+let x;
 
-// do {
-//     x = parseInt(prompt('Podaj liczbę z przedziału 1 - 9!'));
-//   } while (x < 1 || x > 9);
+do {
+    x = parseInt(prompt('Podaj liczbę z przedziału 1 - 9!'));
+  } while (x < 1 || x > 9);
 
-// for (let i = 1; i < 10; i++) {
-//     console.log(`${x} x ${i} = ${x * i}`);
-// }
+for (let i = 1; i < 10; i++) {
+    console.log(`${x} x ${i} = ${x * i}`);
+}
 
 /* rozwiązanie z pętlą while  */
 
