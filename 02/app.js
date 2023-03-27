2e0c90fd9df5a0a6787721f0cbb3ddf5a9830985
Mikolaj Krzywacz <mikolajkrzywacz@gmail.com>
2023-03-27 15:43:59 +0200

    finished task 2

--- 02/app.js
+++ 02/app.js
@@ -1,14 +1,25 @@
 /* rozwiązanie z pętlą for */
 
-let x;
+// let x;
 
-do {
-    x = parseInt(prompt('Podaj liczbę z przedziału 1 - 9!'));
-  } while (x < 1 || x > 9);
+// do {
+//     x = parseInt(prompt('Podaj liczbę z przedziału 1 - 9!'));
+//   } while (x < 1 || x > 9);
 
-for (let i = 1; i < 10; i++) {
-    console.log(`${x} x ${i} = ${x * i}`);
-}
+// for (let i = 1; i < 10; i++) {
+//     console.log(`${x} x ${i} = ${x * i}`);
+// }
 
 /* rozwiązanie z pętlą while  */
 
+let base = parseInt(prompt("Podaj podstawę:"));
+let exponent = parseInt(prompt("Podaj wykładnik:"));
+let result = 1;
+let i = 0;
+let string ='';
+while (i < exponent) {
+    string += `${base} * `;
+    i++;
+}
+string.slice(0, string.length-1);
+console.log(`${string} = ${Math.pow(base,exponent)}`)
