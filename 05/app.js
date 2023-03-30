f62a96fb842d034e8937a0636e090d36f08071b8
Mikolaj Krzywacz <mikolajkrzywacz@gmail.com>
2023-03-30 16:10:07 +0200

    finished task 05 with commented lines

---
+++ 05/app.js
@@ -0,0 +1,50 @@
+function Student(firstName, lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.grades = {};
+}
+
+Student.prototype.addGrade = function (subject, grade){
+     if(typeof this.grades[subject] === 'undefined') {
+        this.grades[subject] = [];
+      }
+      this.grades[subject].push(grade);
+}
+
+Student.prototype.calcGradeAvg= function(subject){
+    if(subject !== undefined){
+        const grades = this.grades[subject];
+        let sum = 0;
+        for(i=0; i< grades.length; i++){
+            sum+= grades[i];
+        }
+        return sum/grades.length
+    }
+    let sum = 0;
+    let counter = 0;
+    const subjects = Object.keys(this.grades);
+    // console.log(typeof this.grades[subjects[0]][0]); number
+    // console.log(subjects)
+    // console.log(subjects.length);
+    // console.log(this.grades[subjects[i]][j])
+    for (let i = 0; i < subjects.length; i++) {
+        // console.log(`i:${i} length: `, this.grades[subjects[i]].length, subjects[i])
+        for (let j = 0; j < this.grades[subjects[i]].length; j++) {
+            sum += this.grades[subjects[i]][j];
+            console.log(`i: ${i} ; j: ${j}`)
+            console.log(typeof this.grades[subjects[i]][j])
+            counter++;
+        }
+    }
+    return sum/counter;
+}
+
+const student1 = new Student("Mikolaj","Krzywacz");
+student1.addGrade('math',5);
+student1.addGrade('math',3);
+student1.addGrade('science',4);
+student1.addGrade('science',6);
+student1.addGrade('science',2);
+student1.addGrade('science',3);
+console.log(student1.calcGradeAvg('science'));
+console.log(student1)
