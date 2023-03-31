5ab29c04054e28a7f151599237d373fdf8b2c6f9
Adam Czelej <admczelej@gmail.com>
2023-03-31 20:32:30 +0200

    task 2 done

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,34 @@
 
 /* rozwiązanie z pętlą for */
-const x = 4;
-
-
+const x = prompt('Podaj liczbe!');
+if(x < 10 && x > 0){
+    for(let i=1; i<10; i++){
+        console.log(x + 'x' + i + '=' + x*i);
+    }
+} else {
+    console.log('Błędne dane');
+}
 
 /* rozwiązanie z pętlą while  */
+
+let a = prompt('Podaj podstawę!');
+let n = prompt('Podaj wykładnik!');
+let i = 0;
+let result = 1;
+let info = '';
+
+if(a > 0 && a < 10 && n > 0 && n < 10){
+    while(i < n){
+        result *= a;
+        if(i > 0){
+            info += ' * ';
+        }
+        info = info + a;
+        i++;
+    }
+}
+else {
+    console.log('Błędne dane');
+}
+
+console.log(info + ' = ' + result);
