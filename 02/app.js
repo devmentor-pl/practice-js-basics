660deae8873e3cf2acfbe9524ca36d55d0198223
Ryuuken <wachnickiandrzej@gmail.com>
2023-04-04 11:26:01 +0200

    Exercise 2 solution with number 4 done

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,19 @@
 
 /* rozwiązanie z pętlą for */
 const x = 4;
+const multi = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+console.log(calculateResultForMultiplication(x, multi));
+
+function calculateResultForMultiplication(number, multipliers) {
+    for(let i = 0; i < multipliers[multipliers.length-1]; i++) {
+        showResultOfMultiplication(number, multipliers[i], number*multipliers[i]); 
+    }
+}
+
+function showResultOfMultiplication(num, multi, res) {
+    console.log(`${num} x ${multi} = ${res}`);
+}
 
 
 /* rozwiązanie z pętlą while  */
