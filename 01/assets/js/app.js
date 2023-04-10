ac910aa59af52e413848fa7c91b75727d028f44e
Maria Kardas <mariah.grze@gmail.com>
2023-04-10 10:04:54 +0200

    Did operations

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -3,3 +3,15 @@ const b = 9;
 
 console.log(a, b);
 console.log(typeof a, typeof b);
+
+const result1 = Number(a) + b
+console.log(result1);
+
+const result2 = Number(a) - b
+console.log(result2);
+
+const result3 = Number(a) * b
+console.log(Number(result3.toFixed(1)));
+
+const result4 = Number(a) / b
+console.log(result4);
