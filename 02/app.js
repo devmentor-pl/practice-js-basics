1afd35a38d6db3e373fa071ea494119f6b6b6122
Maria Kardas <mariah.grze@gmail.com>
2023-04-10 19:08:44 +0200

    Finished task 02

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,42 @@
 
 /* rozwiązanie z pętlą for */
-const x = 4;
+
+// const x = Number(prompt('Podaj liczbę!'))
+const x = 3
+if(x>=1 && x<10) {
+
+    for(let i=1; i<=9; i++) {
+        const result = x * i
+        console.log(x + ' x ' + i + ' = ' + result)
+    }
+
+} else {
+    console.error('Podałeś nie taką liczbę!')
+}
 
 
 
 /* rozwiązanie z pętlą while  */
+const a = 3
+const n = 4
+
+let result = 1
+let iter = 0
+let info = ''
+
+while(iter < n) {
+    result = result * a
+
+    if(iter > 0) {
+        info = info + ' * '
+    }
+
+    info = info + a
+
+    iter++
+}
+
+info = info + " = " + result 
+
+console.log (result)
+console.log(info)
