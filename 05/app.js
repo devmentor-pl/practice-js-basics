80c72b79dbae158deb760922e5d588e0b8954834
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-05-16 17:10:29 +0200

    Add changes in 5th task

--- 05/app.js
+++ 05/app.js
@@ -21,18 +21,31 @@ Student.prototype.addGrade = function (subject, grade) {
     this.grades[subject].push(grade)
 }
 
-Student.prototype.getAverageGrade = function (subject) {
+
+Student.prototype.countAvg = function (arr) {
     let sum = 0;
     let gradesAmount = 0
 
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+        gradesAmount++
+    }
+    return ((sum / gradesAmount).toFixed(2))
+}
+
+Student.prototype.getAverageGrade = function (subject) {
+    let avg
     const keys = Object.keys(this.grades)
 
     if (!subject) {
+        let allGradesArr = []
         for (const key in this.grades) {
-            for (let i = 0; i < this.grades[key].length; i++) {
-                sum += this.grades[key][i];
-                gradesAmount++
-            }
+            // for (let i = 0; i < this.grades[key].length; i++) {
+            //     sum += this.grades[key][i];
+            //     gradesAmount++
+            // }
+            allGradesArr.push(...this.grades[key])
+            avg = this.countAvg(allGradesArr)
         }
     } else if (!keys.includes(subject)) {
         console.log('Niepoprawna nazwa przedmiotu')
@@ -40,15 +53,15 @@ Student.prototype.getAverageGrade = function (subject) {
     } else {
         for (const key in this.grades) {
             if (subject === key) {
-                for (let i = 0; i < this.grades[subject].length; i++) {
-                    sum += this.grades[subject][i]
-                    gradesAmount++
-                }
+                // for (let i = 0; i < this.grades[subject].length; i++) {
+                //     sum += this.grades[subject][i]
+                //     gradesAmount++
+                // }
+                avg = this.countAvg(this.grades[subject])
             }
         }
     }
-
-    return ((sum / gradesAmount).toFixed(2))
+    return avg
 }
 
 
@@ -57,18 +70,12 @@ student.addGrade('maths', 4);
 student.addGrade('maths', 6);
 student.addGrade('polish', 3);
 // student.addGrade('', 4);
-// student.addGrade('maths', 5);
-// student.getAverageGrade('maths')
-// student.getAverageGrade('asfd')
-// student.getAverageGrade('')
-// student.getAverageGrade('polish')
-// student.getAverageGrade()
-
+student.addGrade('maths', 5);
+student.addGrade('geography', 3);
 
 const avgMath = student.getAverageGrade('maths'); // 5
 const avg = student.getAverageGrade(); // 4.33
 
 console.log(avg, avgMath)
 console.log(student)
-
 
