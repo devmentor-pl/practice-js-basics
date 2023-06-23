7534f150a7f7f74b867e85479669329e14c84583
anetasobanska <sannettka@gmail.com>
2023-06-23 10:43:46 +0200

    finish 03

--- 03/app.js
+++ 03/app.js
@@ -7,12 +7,42 @@ const c = randomNumber(min, max);
 
 console.log(a, b, c);
 
-
-
-
-
-
-
 function randomNumber(min, max) {
     return Math.round((Math.random() * (max - min)) + min);
 }
+
+function getSum(one, two, three){
+    const arr = [one, two, three].map(num => Math.floor(num));
+    const getMinNum = Math.min(...arr);
+    const getMaxArr = arr.filter(num => num !== getMinNum);
+    const result = getMaxArr.reduce((a, b) => a + b, 0);
+    return result;
+}
+
+function isEven(one) {
+    if (typeof one !== 'number') return null;
+    return one % 2 === 0 ? true : false;
+}
+
+function showInfo (one, two) {
+    switch (two) {
+    case null:
+        console.log(`Podany argument ${one} nie jest liczbą.`);
+        break;
+    case true:
+        console.log(`Podany argument ${one} jest parzysty.`);
+        break;
+    case false:
+        console.log(`Podany argument ${one} jest nieparzysty.`);
+        break;
+    default:
+        console.log(`Podano bledne dane. one: ${one}, two: ${two}`);
+        break;
+    }
+}
+
+
+const sum = getSum(a, b, c);
+const even = isEven(sum);
+
+showInfo(sum, even);
