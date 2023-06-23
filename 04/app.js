91a99542e75c01250c5186d2a592a42d4c603f71
anetasobanska <sannettka@gmail.com>
2023-06-23 14:42:32 +0200

    finish 04

---
+++ 04/app.js
@@ -0,0 +1,27 @@
+function createArray(min, max) {
+    const arr = new Array(20);
+    
+    for (let i = 0; i < arr.length; i++) {        
+        let numRandom = Math.round(Math.random() * (max - min) + min);
+        arr[i] = numRandom;
+    }
+
+    return arr;
+}
+
+function getLargest(arr) {
+    return arr.sort((a, b) => a-b).slice(10);
+}
+
+function getAvg(arr) {
+    return arr.reduce((a, b) => (a + b), 0) / arr.length;
+}
+
+const arr = createArray(10, 200);
+console.log(arr);
+
+const largest = getLargest(arr);
+console.log(largest);
+
+const avg = getAvg(arr);
+console.log(avg);
