e1c90b98de36c1be6b3e43cb111599c2c474e422
Natalia Gron <nanusza@gmail.com>
2023-09-03 17:56:27 +0200

    Finish task 02

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,32 @@
 
 /* rozwiązanie z pętlą for */
-const x = 4;
+const x = Number(prompt('Podaj liczbę!'));
 
-
+if(x >= 1 && x < 10) {
+    for(let i=1; i<=9; i++){
+        const result = x * i;
+        console.log(x + ' x ' + i + ' = ' + result);
+    }
+} else {
+    console.log('Wprowadzone dane są błędne');
+}
 
 /* rozwiązanie z pętlą while  */
+
+let a = Number(prompt('Potęgowanie: Podaj liczbę podstawy!'));
+let n = Number(prompt('Potęgowanie: Podaj liczbę wykładnika!'));
+let i = 0;
+let res = 1;
+let operation = '';
+
+while (i < n) {
+    res *= a;
+    if (i > 0) {
+        operation += ' x ';
+    }
+    operation += a;
+    i++;
+}
+
+operation += ' = ' + res;
+console.log(operation);
