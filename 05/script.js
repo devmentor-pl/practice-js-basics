a56724696995d9e57f6b1f9d943a53c2fee32f75
Piotr <you@example.com>
2023-09-06 19:43:14 +0200

    add task 5

--- 05/script.js
+++ 05/script.js
@@ -1,7 +1,7 @@
 function Student(initFirstName, initLastName) {
 	this.name = initFirstName
 	this.lastName = initLastName
-	this.marks = []
+	this.marks = {}
 }
 
 // dodanie oceny
@@ -14,32 +14,60 @@ Student.prototype.getMark = function (subject, mark) {
 
 // średnia z przedmiotu
 Student.prototype.getAverageGrade = function (subject) {
-	if (typeof subject === 'undefined') {
+
+	if (typeof this.marks[subject] === 'undefined') {
+		return 0
 	}
-	const grades = this.grades[subject]
+	const grades = this.marks[subject]
+
+	const sum = grades.reduce(function(acc, curr){
+		return acc + curr
+	}, 0)
+
+	return sum / grades.length
 }
 
-// srednia cała
 
 Student.prototype.avgAll = function () {
-	const gradesSums = this.marks[subject]
-	let sumMarks = 0
-	let sumSubject = 0
+    let totalMarks = 0;
+    let totalSubjects = 0;
 
-	for (const grade of gradesSums) {
-		const sumMarskAll = this.sumMarskAllThis(grade)
-		sumMarks += sumMarskAll
-		sumSubject++
-	}
-	return sumSubject === 0 ? 0 : sumMarks / sumSubject
-}
+    for (const subject in this.marks) {
+        const subjectMarks = this.marks[subject];
+        const subjectTotal = subjectMarks.reduce(function (acc, curr) {
+            return acc + curr;
+        }, 0);
+        totalMarks += subjectTotal;
+        totalSubjects += subjectMarks.length;
+    }
+
+    if (totalSubjects === 0) {
+        return 0;
+    }
+
+    return totalMarks / totalSubjects;
+};
 
 
+// 	for(const subject in this.marks) 
+// 		const subjectMarks = this.marks[subject];
+// 		for(const index  in subjectMarks ) {
+// 			const mark = subjectMarks[index];
+// 			console.log(mark);
+// 		}
 
+
+		
+// 	}
+// }
 const student1 = new Student ('Tomasz', 'Kowalski')
-student1.getMark('maths', 4)
-student1.getMark('maths', 6)
-student1.getMark('english', 3)
+student1.getMark('maths', 5)
+student1.getMark('maths', 3)
+student1.getMark('english', 2)
+student1.getMark('english', 1)
+student1.getMark('english', 6)
 
-console.log(student1.avgAll);
+console.log(student1.avgAll());
+console.log(student1.getAverageGrade("english"));
+
 
