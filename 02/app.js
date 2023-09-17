b829c3629ffc14ca9722f8a88348617b6fed2926
Maciej Necka <macieknecka@gmail.com>
2023-09-17 14:40:32 +0200

    Task done

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,36 @@
-
 /* rozwiązanie z pętlą for */
-const x = 4;
-
-
+let x = 4;
+const userNumber = parseInt(prompt('Podaj liczbę', 4));
+if (userNumber <= 9 && userNumber > 0) {
+  x = userNumber;
+} else {
+  alert('Podaj liczbę w zakresie od 1 do 9!');
+}
+for (let i = 1; i <= 9; i++) {
+  console.log(`${x} x ${i} = ${x * i}`);
+}
 
 /* rozwiązanie z pętlą while  */
+
+// const a = Number(prompt('Podaj liczbę podstawową'));
+// const b = Number(prompt('Podaj liczbę wykładniczą'));
+// let result = 1;
+// while (result <= b) {
+//   result++;
+//   if (a >= 1 && a <= 9 && b >= 1 && b <= 9) {
+//     let result = Math.pow(a, b);
+//     console.log(`${countLength(a, b)} = ${result}`);
+//   } else
+//     console.log(`Wprowadziłeś złe dane, wpisz liczby w przedziale od 1 do 9!`);
+// }
+
+// function countLength(base, b) {
+//   let result = '';
+//   for (let i = 1; i <= b; i++) {
+//     result += `${base}`;
+//     if (i < b) {
+//       result += ' * ';
+//     }
+//   }
+//   return result;
+// }
