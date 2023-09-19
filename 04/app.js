c9c1cd9c7e9fd87766514b95e9a1a0f5dc978819
Maciej Necka <macieknecka@gmail.com>
2023-09-19 17:11:19 +0200

    Task done

--- 04/app.js
+++ 04/app.js
@@ -1,3 +1,4 @@
+// Create X random numbers
 const createArr = function (min, max) {
   if (min > max) {
     throw new Error(
@@ -12,7 +13,59 @@ const createArr = function (min, max) {
   return numbersArr;
 };
 
-const arr = createArr(1, 100);
-console.log(arr);
+const arrLargest = createArr(1, 100);
+console.log('X losowych liczb');
+console.log(arrLargest);
 
-const getLargest = function (arr) {};
+// Sort X numbers from largest to smallest
+arrLargest.sort(function (a, b) {
+  return b - a;
+});
+console.log('Posortowane X liczb');
+console.log(arrLargest);
+
+// Get X largest numbers
+const firstTenElements = arrLargest.slice(0, 10);
+console.log('Posortowane max X liczb');
+console.log(firstTenElements);
+
+// Calculate the arithmetic
+
+const getAvg = function (arr) {
+  let sum = 0;
+  let arrElements = 0;
+
+  arr.forEach(function (element) {
+    sum += element;
+    arrElements++;
+  });
+
+  if (arrElements === 0) {
+    return 0;
+  } else {
+    return sum / arrElements;
+  }
+};
+console.log('Średnia arytmetyczna');
+console.log(getAvg(firstTenElements));
+
+// Zadanie 4
+console.log('<_______Zadanie 4_______>');
+console.log('X losowych liczb');
+const taskArr = createArr(10, 200);
+console.log(taskArr);
+
+// Sort X numbers from largest to smallest
+taskArr.sort(function (a, b) {
+  return b - a;
+});
+console.log('Posortowane X liczb');
+console.log(taskArr);
+
+// Get X largest numbers
+const firstTenTaskEle = taskArr.slice(0, 10);
+console.log('Posortowane max X liczb');
+console.log(firstTenTaskEle);
+
+// Calculate the arithmetic
+console.log(getAvg(taskArr));
