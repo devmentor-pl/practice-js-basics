9b089a98ee906fe85403b66f05b81d7c583bf596
Maciej Necka <macieknecka@gmail.com>
2023-09-24 22:10:34 +0200

    Line 5 loop fixes + while loop accept only numbers

--- 02/app.js
+++ 02/app.js
@@ -1,27 +1,36 @@
 /* rozwiązanie z pętlą for */
-let x = 4;
 const userNumber = parseInt(prompt('Podaj liczbę', 4));
 if (userNumber <= 9 && userNumber > 0) {
-  x = userNumber;
+  for (let i = 1; i <= 9; i++) {
+    console.log(`${userNumber} x ${i} = ${userNumber * i}`);
+  }
 } else {
   alert('Podaj liczbę w zakresie od 1 do 9!');
 }
-for (let i = 1; i <= 9; i++) {
-  console.log(`${x} x ${i} = ${x * i}`);
-}
 
-/* rozwiązanie z pętlą while  */
+// /* rozwiązanie z pętlą while  */
 
 // const a = Number(prompt('Podaj liczbę podstawową'));
 // const b = Number(prompt('Podaj liczbę wykładniczą'));
-// let result = 1;
-// while (result <= b) {
-//   result++;
-//   if (a >= 1 && a <= 9 && b >= 1 && b <= 9) {
+
+// if (
+//   !Number.isNaN(a) &&
+//   !Number.isNaN(b) &&
+//   a >= 1 &&
+//   a <= 9 &&
+//   b >= 1 &&
+//   b <= 9
+// ) {
+//   let iteration = 1;
+
+//   while (iteration <= b) {
+//     iteration++;
+
 //     let result = Math.pow(a, b);
 //     console.log(`${countLength(a, b)} = ${result}`);
-//   } else
-//     console.log(`Wprowadziłeś złe dane, wpisz liczby w przedziale od 1 do 9!`);
+//   }
+// } else {
+//   console.log(`Wprowadziłeś złe dane, wpisz liczby w przedziale od 1 do 9!`);
 // }
 
 // function countLength(base, b) {
