f58dda090de9e4a69ddbda81ec76966f19223118
Marcin Chechelski <marcin.chechelski@op.pl>
2023-09-25 22:07:07 +0200

    add fo

--- 02/app.js
+++ 02/app.js
@@ -1,5 +1,13 @@
 /* rozwiązanie z pętlą for */
-const x = 4;
+const x = Number(prompt("Podaj liczbę!"));
+if (x > 1 && x < 10) {
+    for (let i = 1; i <= 9; i++) {
+        const result = x * i;
+        console.log(x + " " + "X" + " ", i + " " + "=", result);
+    }
+} else {
+    console.error("Podałeś błędne dae");
+}
 
 /* rozwiązanie z pętlą while  */
 //test//
