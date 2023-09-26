188d923d4b5c43350877c4b6d0a06848a7e5a44d
Marcin Chechelski <marcin.chechelski@op.pl>
2023-09-26 23:24:24 +0200

    first function getSume done

--- 03/app.js
+++ 03/app.js
@@ -7,12 +7,20 @@ const c = randomNumber(min, max);
 
 console.log(a, b, c);
 
-
-
-
-
-
-
 function randomNumber(min, max) {
-    return Math.round((Math.random() * (max - min)) + min);
+    return Math.round(Math.random() * (max - min) + min);
 }
+function getSum(a, b, c) {
+    const aInt = Number(a);
+    const bInt = Number(b);
+    const cInt = Number(c);
+
+    const arr = [aInt, bInt, cInt];
+    arr.sort(function (a, b) {
+        return b - a;
+    });
+    return arr[0] + arr[1];
+}
+
+const sum = getSum(a, b, c);
+console.log(sum);
