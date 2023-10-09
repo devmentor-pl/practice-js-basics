94f6898f75cf5b3c7434889c4d3c09291d75a2f5
Kaczupinko <kaczupinko@gmail.com>
2023-10-09 13:14:05 +0100

    I changed the function's behavior

--- 05/app.js
+++ 05/app.js
@@ -14,28 +14,13 @@ function Student(name, surname) {
         this.grades[subject].push(grade);
     }
     
-    //Metoda przyjmuje jeden parametr subject. Uzyskuje dostep do tablicy ocen dla danego przedmiotu z obiektu 'grades' przez 'this.grades[subject].
-    //Sprawdza czy tablica ocen dla przedmiotu istnieje.
-    //Jesli istnieja oceny dla danego przedmiotu, metoda oblicza ich srednia arytmetyczna przez funkcje reduce.
-    //Wynik jest zaokraglony do dwoch miejsc po przecinku, korzystajac z metody 'toFixed(2).
-    Student.prototype.calculateSubjectAverage = function (subject) {
-        const subjectGrades = this.grades[subject];
-        if (!subjectGrades || subjectGrades.length === 0) {
-            return 0;
-        }
-        const getAverageGrade = subjectGrades.reduce((a, b) => a + b, 0) / subjectGrades.length;
-        return getAverageGrade.toFixed(2);
-    }
+    /*
+        Jesli podam parametr 'subject' funkcja obliczy srednia ocen tylko dla konkretnego przedmiotu.
+        Jesli nie podam parametru 'subject' funkcja obliczy srednia ze wszystkich ocen studenta.
 
-    //Metoda przyjmuje jeden parametr i sprawdza czy istnieja jakiekolwiek przedmioty z ocenami w obiekcie grades.
-    //Uzyskuje liste nazw przedmiotow jako tablice kluczy obiektu 'grades' za pomoca 'Object.keys(this.grades).
-    //Jesli nie ma zadnych przedmiotow metoda zwraca 0.00 jako wynik.
-    //Jesli istnieja oceny metoda inicjuje zmienne totalSum oraz totalGrades.
-    //Metoda iteruje przez liste przedmiotow za pomcoa forEach i sprawdza czy istnieja oceny dla kazdego z przedmiotow.
-    //Jesli tak iteruje przez oceny w danym przedmiocie za pomoca kolejnego forEach i sumuje je w zmiennej totalSum oraz zwieksza 'totalGrades'.
-    //Po zakonczeniu metoda sprawdza czy istnieja jakiekolwiek oceny. Jesli nie ma zadnych zwraca '0.00'.
-    //Wynik ogolnej sredniej oceny jest zaokraglany do dwoch miejsc po przecinku dzieki 'toFixed(2).
-    Student.prototype.calculateOverallAverage = function () {
+        
+    */ 
+    Student.prototype.getAverageGrade = function (subject) {
         const subjects = Object.keys(this.grades);
         if (subjects.length === 0) {
             return '0.00';
@@ -43,15 +28,25 @@ function Student(name, surname) {
         let totalSum = 0;
         let totalGrades = 0;
     
-        subjects.forEach((subject) => {
+        if (subject) {
             const subjectGrades = this.grades[subject];
             if (subjectGrades && subjectGrades.length > 0) {
-                subjectGrades.forEach((grade) => {
+                subjectGrades.forEach(grade => {
                     totalSum += grade;
                     totalGrades++;
                 });
             }
-        });
+        } else {
+            subjects.forEach((subject) => {
+                const subjectGrades = this.grades[subject];
+                if (subjectGrades && subjectGrades.length > 0) {
+                    subjectGrades.forEach((grade) => {
+                        totalSum += grade;
+                        totalGrades++;
+                    });
+                }
+            });
+        }
     
         if (totalGrades === 0) {
             return '0.00'; 
@@ -66,8 +61,8 @@ const student = new Student('Jan', 'Kowalski');
 student.addGrade('maths', 4);
 student.addGrade('maths', 6);
 student.addGrade('english', 3);
-const avgMath = student.calculateSubjectAverage('maths'); // 5
-const avg = student.calculateOverallAverage(); // '4.33'
+const avgMath = student.getAverageGrade('maths'); // 5
+const avg = student.getAverageGrade(); // '4.33'
 console.log(student);
 console.log(avgMath);
 console.log(avg);
