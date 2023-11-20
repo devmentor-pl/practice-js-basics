543eccbed10313dc54ade930ea1f9d5950014b6c
Kasia <k.kasia912@gmail.com>
2023-11-20 12:30:25 +0100

    change class for constructor and prototype - complete task 05

--- 05/app.js
+++ 05/app.js
@@ -1,42 +1,41 @@
-class Student {
-  constructor(name, surname) {
-    this.name = name;
-    this.surname = surname;
-    this.grade = {};
+function Student(name, surname) {
+  this.name = name;
+  this.surname = surname;
+  this.grade = {};
+}
+Student.prototype.addGrade = function (subject, mark) {
+  if (!this.grade.hasOwnProperty(subject)) {
+    this.grade[subject] = [];
   }
-  addGrade(subject, mark) {
-    if (!this.grade.hasOwnProperty(subject)) {
-      this.grade[subject] = [];
+  this.grade[subject].push(mark);
+};
+Student.prototype.calculateAverage = function (grades) {
+  const sum = grades.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
+  const avg = sum / grades.length;
+  return avg;
+};
+Student.prototype.getAverageGrade = function (subject) {
+  if (subject === undefined) {
+    let avgSum = 0;
+    let count = 0;
+    for (const key in this.grade) {
+      const grades = this.grade[key];
+      const avg = this.calculateAverage(grades);
+      avgSum += avg;
+      count++;
     }
-    this.grade[subject].push(mark);
-  }
-  calculateAverage(grades) {
-    const sum = grades.reduce(
-      (accumulator, currentValue) => accumulator + currentValue,
-      0
-    );
-    const avg = sum / grades.length;
+    const avgGeneral = avgSum / count;
+    return avgGeneral;
+  } else {
+    const grades = this.grade[subject];
+    const avg = this.calculateAverage(grades);
     return avg;
   }
-  getAverageGrade(subject) {
-    if (subject === undefined) {
-      let avgSum = 0;
-      let count = 0;
-      for (const key in this.grade) {
-        const grades = this.grade[key];
-        const avg = this.calculateAverage(grades);
-        avgSum += avg;
-        count++;
-      }
-      const avgGeneral = avgSum / count;
-      return avgGeneral;
-    } else {
-      const grades = this.grade[subject];
-      const avg = this.calculateAverage(grades);
-      return avg;
-    }
-  }
-}
+};
+
 const student = new Student('Jan', 'Kowalski');
 
 student.addGrade('maths', 4);
@@ -50,9 +49,8 @@ student.addGrade('polish', 4);
 student.addGrade('polish', 3);
 student.addGrade('polish', 3);
 student.addGrade('polish', 6);
-// console.log(student);
 
 const avgMath = student.getAverageGrade('maths');
 console.log(avgMath);
 const avg = student.getAverageGrade();
-console.log(`Ogólna średnia artymetyczna: ${avg}`);
+console.log(`Ogólna średnia artmetyczna: ${avg}`);
