0d8995b2d7bd5bc2fcf5cd09083c4c6b4fb4e34a
Kasia <k.kasia912@gmail.com>
2023-11-21 15:37:10 +0100

    add corrected task 05

--- 05/app.js
+++ 05/app.js
@@ -9,29 +9,28 @@ Student.prototype.addGrade = function (subject, mark) {
   }
   this.grade[subject].push(mark);
 };
-Student.prototype.calculateAverage = function (grades) {
-  const sum = grades.reduce(
-    (accumulator, currentValue) => accumulator + currentValue,
-    0
-  );
-  const avg = sum / grades.length;
-  return avg;
-};
+
 Student.prototype.getAverageGrade = function (subject) {
   if (subject === undefined) {
-    let avgSum = 0;
     let count = 0;
-    for (const key in this.grade) {
+    let sum = 0;
+    for (let key in this.grade) {
       const grades = this.grade[key];
-      const avg = this.calculateAverage(grades);
-      avgSum += avg;
-      count++;
+
+      for (let i = 1; i < grades.length; i++) {
+        sum += grades[i];
+        count++;
+      }
     }
-    const avgGeneral = avgSum / count;
+    const avgGeneral = sum / count;
     return avgGeneral;
   } else {
     const grades = this.grade[subject];
-    const avg = this.calculateAverage(grades);
+    const sum = grades.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      0
+    );
+    const avg = sum / grades.length;
     return avg;
   }
 };
@@ -53,4 +52,6 @@ student.addGrade('polish', 6);
 const avgMath = student.getAverageGrade('maths');
 console.log(avgMath);
 const avg = student.getAverageGrade();
-console.log(`Ogólna średnia artmetyczna: ${avg}`);
+console.log(`Ogólna średnia arytmetyczna: ${avg}`);
+
+console.log(student);
