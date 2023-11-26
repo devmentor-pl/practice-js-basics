5e48c9773d9baa1562f21e24203527b02daf8cff
Dawidtk <dawidtk@gmail.com>
2023-11-26 16:41:21 +0100

    typeof, res

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -1,4 +1,17 @@
+
 const a = '4.2';
 const b = 9;
-
 console.log(a, b);
+//typeof shows to me its str/number
+console.log(typeof a);
+console.log(typeof b);
+
+const c = parseInt(a);
+
+const res1 = c + b;
+const res2 = c - b;
+const res3 = c * b;
+const res4 = c / b;
+const res5 = c % b;
+
+console.log(res1, res2, res3, res4, res5);
