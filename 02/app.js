ea5255b368679551530742abf60162a74f811c67
Dawidtk <dawidtk@gmail.com>
2023-11-27 08:24:59 +0100

    for in progress

--- 02/app.js
+++ 02/app.js
@@ -1,6 +1,17 @@
 
 /* rozwiązanie z pętlą for */
-const x = 4;
+const getNumber = function () { 
+    let x = prompt('write number from 1 to 9')
+    if (x = null) //return
+    else if {
+        alert('its not a number!');
+
+        return getNumber ()
+    }
+    else if (x < 1 || x > 9)
+        alert('Number must be from 1 to 9')
+
+}
 
 
 
