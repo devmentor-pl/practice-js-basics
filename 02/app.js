3b3144a8885fd176d7bbf434d35e8afb38e47470
Dawidtk <dawidtk@gmail.com>
2023-11-28 09:39:49 +0100

    while

--- 02/app.js
+++ 02/app.js
@@ -4,16 +4,21 @@ zmiana_zmiennych) {  kod który zostanie wykonany pewną ilość razy} */
 /* rozwiązanie z pętlą for */
 const getNumber = function () { 
     let x = prompt('write number from 1 to 9')
-    if (x = null) //return
-    else if {
+    if (x === null || x.trim() === '') return
+    else if {}
         alert('its not a number!');
 
         return getNumber ()
     }
     else if (x < 1 || x > 9)
         alert('Number must be from 1 to 9')
+        return getNumber()
+    }
+    return x    
 
-}
+} 
+
+const number = getNumber()
 
 
 
@@ -23,3 +28,27 @@ const getNumber = function () {
 }
 */
 
+let a = Number(prompt('Number from 1 to 9'));
+let n = Number(prompt('wykładnik z przedziału 1 do 9'));
+
+if (a >= 1 && a < 10 && n >= 1 && n < 10) {
+
+    let i = 0;
+    let res = 1;
+    let operation = '';
+
+    while (i < n) {
+        res *= a;
+        if (i > 0) {
+            operation += ' x ';
+        }
+        operation += a;
+        i++;
+    }
+
+    operation += ' = ' + res;
+    console.log(operation);
+
+} else {
+    alert('Error ! Think again ;-)');
+}
