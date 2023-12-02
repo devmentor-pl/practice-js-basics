21a190be2c7fd46beb9b392976ec540dc9459fbf
Dawidtk <dawidtk@gmail.com>
2023-12-02 12:45:46 +0100

    getSum done

--- 03/app.js
+++ 03/app.js
@@ -5,9 +5,32 @@ const a = randomNumber(min, max);
 const b = randomNumber(min, max);
 const c = randomNumber(min, max);
 
-const sum =TwoNumbers(a, b, c)
-
 console.log(a, b, c);
+
+
+function getSum(d, e, f) {
+    const dInt = parseInt(d);
+    const eInt = parseInt(e);
+    const fInt = parseInt(f);
+
+    const arr = [dInt, eInt, fInt];
+    arr.sort(function(a, b) {
+        return b - a;
+    });
+
+    return arr[0] + arr[1];
+    
+}
+
+const sum = getSum(a, b, c);
+console.log(sum);
+
+
+function randomNumber(min, max) {
+    return Math.round((Math.random() * (max - min)) + min);
+}
+
+/* const sum =TwoNumbers(a, b, c)
 
 function TwoNumbers(a, b, c) {
     a = Number(a);
@@ -21,14 +44,15 @@ function TwoNumbers(a, b, c) {
 }
 console.log('Two biggest numbers: ' + sum);
 
+//isEven
 const isEven = function(num){
     if
 }
 
-
-
-
-
-function randomNumber(min, max) {
-    return Math.round((Math.random() * (max - min)) + min);
+//const isEven = function (x) {
+  //  if (isNaN(x)) return null
+    //else if (x % 2) return false
+    //else return true
 }
+
+const even = isEven(sum)*/ 
