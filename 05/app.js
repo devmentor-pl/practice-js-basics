b5addd9dc4d6aed82d289abf9b57d89f5dc4cb1b
Dawidtk <dawidtk@gmail.com>
2023-12-05 14:06:06 +0100

    prototypes in progress

--- 05/app.js
+++ 05/app.js
@@ -3,7 +3,16 @@
 function Student(firstName, lastName) {
     this.firstName = firstName;
     this.lastName = lastName;
-    this.grades = {}
+    this.grades = {};
 }
 
 //prototypy
+
+Student.prototype.addGrade = function(subject, grade) {
+    if (subject.trim() === '') {
+        console.log('Name of subject')
+        return
+    }
+
+    if
+}
