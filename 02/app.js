d9dbea3b44e80eed1d04debf72653191a0916a47
Robert Staniszewski <robstani.tech@gmail.com>
2023-12-22 21:21:38 +0100

    Add multiplication table and exponentiation solutions

--- 02/app.js
+++ 02/app.js
@@ -1,6 +1,6 @@
 
 /* rozwiązanie z pętlą for */
-const x = prompt('Podaj liczbę!')
+const x = prompt('Podaj liczbę!');
 
 for (var i = 1; i < 10; i++) {
     var wynik = x * i;
@@ -8,3 +8,14 @@ for (var i = 1; i < 10; i++) {
 }
 
 /* rozwiązanie z pętlą while  */
+let a = prompt('Podaj podstawe'); // podstawa
+let n = prompt('Podaj wykładnik'); // wykładnik
+let wynikWhile = 1;
+let czynniki = [];
+
+while (czynniki.length < n) {
+    wynikWhile *= a;
+    czynniki.push(a);
+}
+
+console.log(`${czynniki.join(' * ')} = ${wynikWhile}`);
