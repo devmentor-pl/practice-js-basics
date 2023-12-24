1df76a7b13fbcc766bec489b17a86b5a5a018883
Robert Staniszewski <robstani.tech@gmail.com>
2023-12-24 01:34:41 +0100

    Add isEven function to return true for even numbers

--- 03/app.js
+++ 03/app.js
@@ -16,16 +16,31 @@ console.log(a, b, c);
 function randomNumber(min, max) {
     return Math.round((Math.random() * (max - min)) + min);
 }
-
+/* Funkcja o nazwie getSum */
 function getSum (par1, par2, par3) {
-    var numbers = [parseInt(par1, 10), parseInt(par2, 10), parseInt(par3, 10)];
+    const numbers = [parseInt(par1, 10), parseInt(par2, 10), parseInt(par3, 10)];
     numbers.sort(function(a, b) {
         return b - a;
     });
-    var sum = numbers[0] + numbers[1];
+    const sum = numbers[0] + numbers[1];
     console.log(sum);
     return sum;
 
 }
+
+/* Funkcja o nazwie isEven */
+
+const isEven = function(num) {
+    if (isNaN(num)) {
+        return null;
+    } else {
+        return num % 2 === 0;
+    }
+};
+
+console.log(isEven(5));
+
+
+
 
 
