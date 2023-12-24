b2d69245bf7a90ee4ccfb401c04315fd70e71e62
Robert Staniszewski <robstani.tech@gmail.com>
2023-12-24 01:19:50 +0100

    Add function to sum 2 biggest numbers from 3 parametrs

--- 03/app.js
+++ 03/app.js
@@ -16,3 +16,16 @@ console.log(a, b, c);
 function randomNumber(min, max) {
     return Math.round((Math.random() * (max - min)) + min);
 }
+
+function getSum (par1, par2, par3) {
+    var numbers = [parseInt(par1, 10), parseInt(par2, 10), parseInt(par3, 10)];
+    numbers.sort(function(a, b) {
+        return b - a;
+    });
+    var sum = numbers[0] + numbers[1];
+    console.log(sum);
+    return sum;
+
+}
+
+
