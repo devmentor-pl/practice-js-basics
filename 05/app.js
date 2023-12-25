47b26be242bcd4c84d4825d1a0558d649227a8fa
Robert Staniszewski <robstani.tech@gmail.com>
2023-12-26 00:55:39 +0100

    Add method to calculate average grade for a specific subject or all subjects

--- 05/app.js
+++ 05/app.js
@@ -13,9 +13,42 @@ Student.prototype.addGrade = function(subject, grade) {
     this.grades[subject].push(grade);
 }
 
-const student1 = new Student('Alice', 'Smith');
+Student.prototype.getAverageGrade = function(subject) {
+    if (subject) {
+        const subjectGrades = this.grades[subject];
+        if (subjectGrades && subjectGrades.length > 0) {
+            let sum = 0;
+            subjectGrades.forEach(function(grade) {
+                sum += grade;
+            });
+            const avg = sum / subjectGrades.length;
+            return avg;
+        } else {
+            return 'Brak ocen dla tego przedmiotu.';
+        }
+    } else {
+        const allGrades = Object.values(this.grades).flat();
+        if (allGrades.length > 0) {
+            let totalSum = 0;
+            let totalCount = 0;
+            allGrades.forEach(function(grade) {
+                totalSum += grade;
+                totalCount++;
+            });
+            const avg = totalSum / totalCount;
+            return avg;
+        } else {
+            return 'Brak ocen.';
+        }
+    }
+};
 
-student1.addGrade('Math', 90);
-student1.addGrade('History', 85);
+const student = new Student('Jan', 'Kowalski');
+student.addGrade('math', 4);
+student.addGrade('math', 6);
+student.addGrade('english', 3);
+student.addGrade('chemistry', 4);
+const avgMath = student.getAverageGrade('math'); // 5
+const avg = student.getAverageGrade(); // 4.33
+console.log(student);
 
-console.log(student1.grades);
