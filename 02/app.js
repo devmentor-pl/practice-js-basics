404a8f5f3a966840e5470b8585105d417c2ff2f0
Typee8 <wojciechjasiak1324@gmail.com>
2024-01-09 10:24:51 +0100

    Finish 02

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,17 @@
 
 /* rozwiązanie z pętlą for */
-const x = 4;
+multiplication();
 
-
+function multiplication () {
+  const x = prompt('Podaj liczbę!');
+  if (!(1 <= x && x <= 9)) {
+    alert("Liczba powinna być z przedziału od 1 do 9.");
+    return multiplication();
+  } else {
+    for (let i = 1; i <= 9; i++) {
+      console.log(x + " x " + i + " = " + x * i);
+    }
+  }
+}
 
 /* rozwiązanie z pętlą while  */
