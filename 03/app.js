e8e51ba9d18199cb349e354216c32a0adccac5a9
Marcin Wabinski <wabinskim@gmail.com>
2024-01-26 10:43:21 +0100

    Create getSum function

--- 03/app.js
+++ 03/app.js
@@ -7,12 +7,18 @@ const c = randomNumber(min, max);
 
 console.log(a, b, c);
 
-
-
-
-
-
+function getSum(num1, num2, num3) {
+  const numbers = [parseInt(num1), parseInt(num2), parseInt(num3)];
+  numbers.sort(function (a, b) {
+    return b - a;
+  });
+  const sum = numbers[0] + numbers[1];
+  return sum;
+}
 
 function randomNumber(min, max) {
-    return Math.round((Math.random() * (max - min)) + min);
+  return Math.round(Math.random() * (max - min) + min);
 }
+
+const sum = getSum(a, b, c);
+console.log(sum);
