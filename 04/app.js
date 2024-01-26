f93626f94de731ce3b2e2c242552e36b4fe84158
Marcin Wabinski <wabinskim@gmail.com>
2024-01-26 12:49:50 +0100

    Create getLargest function

--- 04/app.js
+++ 04/app.js
@@ -7,5 +7,16 @@ function createArray(min, max) {
   return arr;
 }
 
+function getLargest(arr) {
+  arr.sort(function (a, b) {
+    return b - a;
+  });
+  const newArray = arr.slice(0, 10);
+  return newArray;
+}
+
 const arr = createArray(1, 200);
 console.log(arr);
+
+const largest = getLargest(arr);
+console.log(largest);
