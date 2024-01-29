9bbb1dccc4467e315e8a668cdeeacc4c1d3e5b59
Marcin Wabinski <wabinskim@gmail.com>
2024-01-29 01:46:17 +0100

    Create getAverageGrade function

--- 05/app.js
+++ 05/app.js
@@ -11,8 +11,36 @@ Student.prototype.addGrade = function (subject, grade) {
   this.grades[subject].push(grade);
 };
 
+Student.prototype.getAverageGrade = function (subject) {
+  if (typeof this.grades[subject] !== "undefined") {
+    const grades = this.grades[subject];
+    const sumOfGrades = grades.reduce(function (acc, num) {
+      return acc + num;
+    });
+    return sumOfGrades / grades.length;
+  }
+  const gradesTotal = [];
+  for (const key in this.grades) {
+    const grades = this.grades[key];
+    grades.forEach(function (el) {
+      gradesTotal.push(el);
+    });
+  }
+  const sumOfGrades = gradesTotal.reduce(function (acc, num) {
+    return acc + num;
+  });
+  return sumOfGrades / gradesTotal.length;
+};
+
 const student = new Student("Jan", "Kowalski");
 console.log(student);
 student.addGrade("maths", 4);
 student.addGrade("maths", 6);
 student.addGrade("english", 3);
+const avgMath = student.getAverageGrade("maths");
+const avgEnglish = student.getAverageGrade("english");
+const avg = student.getAverageGrade();
+
+console.log(avgMath);
+console.log(avgEnglish);
+console.log(avg);
