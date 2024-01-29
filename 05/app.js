16e9530f792d96039581f2ea20de1397f7a4fb49
Marcin Wabinski <wabinskim@gmail.com>
2024-01-29 01:44:07 +0100

    Create addGrade prototype function, add grades to object

--- 05/app.js
+++ 05/app.js
@@ -3,5 +3,16 @@ function Student(firstName, lastName) {
   this.lastName = lastName;
   this.grades = {};
 }
+
+Student.prototype.addGrade = function (subject, grade) {
+  if (typeof this.grades[subject] === "undefined") {
+    this.grades[subject] = [];
+  }
+  this.grades[subject].push(grade);
+};
+
 const student = new Student("Jan", "Kowalski");
 console.log(student);
+student.addGrade("maths", 4);
+student.addGrade("maths", 6);
+student.addGrade("english", 3);
