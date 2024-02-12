adf38a5da1b681e4b0853bb09f1299897534d5a7
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-02-12 10:31:13 +0100

    02 completed

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,30 @@
 
 /* rozwiązanie z pętlą for */
-const x = 4;
+// const x = 4;
 
+// const x = prompt('Podaj liczbę od 1 do 9');
+// let result;
+
+// for(let i = 1; i <= 9; i ++) {
+    // result = x * i;
+    // console.log(x + " * " + i + " = " + result);
+
+    // let multiplyNumber = parseInt(x);
+    // result = multiplyNumber * i;
+    // console.log(multiplyNumber + " * " + i + " = " + result);
+// }   
 
 
 /* rozwiązanie z pętlą while  */
+
+const a = prompt('Podaj liczbę');
+let n = 1;
+
+while(n <= 3) {
+    let result = Math.pow(a, n);
+    // Jak wyświetlić potęgę?
+    // console.log(a + " * " + a + " * " + a + " * " + a + " = " + result); 
+    console.log(result);
+
+    n ++;
+}
