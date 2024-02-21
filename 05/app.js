118394464676641a9db0dc9d4493a913e0963ef9
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-02-21 12:09:02 +0100

    05 partly completed

--- 05/app.js
+++ 05/app.js
@@ -2,20 +2,40 @@
 function Person(initialName, initialSurname) {
     this.name = initialName;
     this.surname = initialSurname;
+    this.math = [];
+    this.polish = [];
+    this.english = [];
+    this.biology = [];
 }
 
 const person = new Person('Kasia', 'Dworak');
 
 // METODY
-// dodawanie oceny
-Person.prototype.addScore = function(score) {
-    this.score = score;
-}
+// dodawanie ocen do przedmiotów
 
-person.addScore([10, 20, 30, 40]);
+// czy oceny mogę dodać za pomocą prototypów 
+// (obiekt w obiekcie)?
+// dodawanie oceny
+// Person.prototype.addScore = function(score) {
+//     this.score = score;
+// }
+
+// person.addScore([10, 20, 30, 40]);
+
+person.math.push(5, 6, 5, 5, 4);
+console.log(person);
+
+person.polish.push(4, 6, 5, 3, 4);
+console.log(person);
+
+person.english.push(5, 6, 6, 6, 3);
+console.log(person);
+
+person.biology.push(5, 6, 4, 4, 2);
 console.log(person);
 
 // obliczanie średniej arytmetycznej
+
 // Person.prototype.getAvg = function() {
 //     let sum3 = 0;
 //     let objLength = 0;
