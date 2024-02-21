b2e4775ac7b885065067d61841ca2bc1e77dfc4a
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-02-21 12:30:09 +0100

    05 partly completed

--- 05/app.js
+++ 05/app.js
@@ -23,31 +23,29 @@ const person = new Person('Kasia', 'Dworak');
 // person.addScore([10, 20, 30, 40]);
 
 person.math.push(5, 6, 5, 5, 4);
-console.log(person);
 
 person.polish.push(4, 6, 5, 3, 4);
-console.log(person);
 
 person.english.push(5, 6, 6, 6, 3);
-console.log(person);
 
 person.biology.push(5, 6, 4, 4, 2);
-console.log(person);
 
-// obliczanie średniej arytmetycznej
+// obliczanie średniej arytmetycznej (dla obiektu w obiekcie)
+const avg = getAvg(this.person.math);
+console.log(avg);
 
-// Person.prototype.getAvg = function() {
-//     let sum3 = 0;
-//     let objLength = 0;
-//     person.score.forEach(function(num) {
-//         sum3 += num;
-//         objLength ++;
-//     })
-//     const avg = sum3 / objLength;
-// }
-// const avgScore = person.score.getAvg();
-// console.log(avgScore);
+Person.prototype.getAvg = function() {
+    const scoreSum = 0;
+    const scoreNumbers = 0;
 
-// dodaj przedmioty + dodaj oceny z przedmiotów
-// wylicz średnią z jednego przedmiotu
-// wylicz średnią z wielu przedmiotów
+    this.forEach(function(scores) {
+        scoreSum += scores;
+        scoreNumbers ++;
+    })
+    
+    avg = scoreSum / scoreNumbers;
+    console.log(avg);
+
+}
+
+// obliczanie średniej arytmetycznej dla wszystkich przedmiotów
