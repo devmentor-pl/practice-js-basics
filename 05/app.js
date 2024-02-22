34eb36e76177d34b85f31443e932b19c48a2d286
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-02-22 14:40:07 +0100

    05 partly completed

--- 05/app.js
+++ 05/app.js
@@ -31,32 +31,59 @@ student.addScore('physics', 4);
 console.log(student);
 
 // obliczanie średniej arytmetycznej / BŁĄD funkcja getAverageGrade gnie jest zdefiniowana
-let sum = 0;
-const numberOfGrades = 0;
+// wydzielenie ocen [tablice]
+const gradesKeys = Object.keys(student.grades);
+const gradesValues = Object.values(student.grades);
+const gradesFive = student.grades; // 5 tablic
+const gradesOne = student.grades['math']; // 1 tablica 'math'
+console.log(gradesKeys, gradesValues, gradesFive, gradesOne);
 
-Person.prototype.getAverageGrade = function(subject) {
-    if(typeof this.grades[subject] === 'undefined') {
+// prawidłowo wylicza sumę ocen z matematyki
+let sum = 0;
+let numberOfGrades = 0;
+
+const gradesMath = student.grades['math'];
+gradesMath.forEach(function(grade) {
+    sum += grade;
+    numberOfGrades ++;
+})
+const avgMath = sum / numberOfGrades;
+console.log(avgMath);
+
+
+// Person.prototype.getAverageGrade = function(subject) {
+    
+//     if(typeof this.grades[subject] === 'undefined') {
         // średnia dla wszystkich przedmiotów
-        const gradesAll = this.grades;
         // const gradesAll = Object.values(this.grades); 
-        this.avgGradesAll = function() {
-            gradesAll.forEach(function(grade) {
-                sum += grade;
-                numberOfGrades = grade ++;
-            })
-        const avg = sum / numberOfGrades;
-        }
-    } else {
+        
+        // const gradesMath = student.grades['math'];
+        // gradesMath.forEach(function(grade) {
+        //     sum += grade;
+        //     numberOfGrades ++;
+        //     const avgMath = sum / numberOfGrades;
+        //     console.log(avgMath);
+        // })
+
+        // const gradesAll = this.grades;
+        // this.avgGradesAll = function() {
+        //     gradesAll.forEach(function(grade) {
+        //         sum += grade;
+        //         numberOfGrades = grade ++;
+        //     })
+        // const avg = sum / numberOfGrades;
+    //     }
+    // } else {
         // średnia dla wybranego subject
         // const gradesOneSubject = Object.values(this.grades); 
-        const gradesOneSubject = this.grades[subject];
-        gradesOneSubject.forEach(function(grade) {
-            sum += grade;
-            numberOfGrades = grade ++;
-        });
-        const avg = sum / numberOfGrades;
-    }
-}
+//         const gradesOneSubject = this.grades[subject];
+//         gradesOneSubject.forEach(function(grade) {
+//             sum += grade;
+//             numberOfGrades = grade ++;
+//         });
+//         const avg = sum / numberOfGrades;
+//     }
+// }
 
-const avg = getAverageGrade();
-console.log(avg);
+// const avg = getAverageGrade();
+// console.log(avg);
