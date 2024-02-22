c3cc284d303f630a16145720fe048bc6d1333845
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-02-22 11:28:01 +0100

    05 partly completed

--- 05/app.js
+++ 05/app.js
@@ -20,7 +20,7 @@ student.grades.polish.push(4, 6, 5, 3, 4);
 student.grades.english.push(5, 6, 6, 6, 3);
 student.grades.biology.push(5, 6, 4, 4, 2);
 
-// dodawanie przedmiotów funkcją
+// dodawanie przedmiotów przy uzyciu funkcji
 Person.prototype.addScore = function(subject, score) {
     if(typeof this.grades[subject] === 'undefined') {
         this.grades[subject] = [];
@@ -30,29 +30,33 @@ Person.prototype.addScore = function(subject, score) {
 student.addScore('physics', 4);
 console.log(student);
 
-// obliczanie średniej arytmetycznej 
-
+// obliczanie średniej arytmetycznej / BŁĄD funkcja getAverageGrade gnie jest zdefiniowana
 let sum = 0;
 const numberOfGrades = 0;
 
-Person.prototype.getAvg = function(subject) {
+Person.prototype.getAverageGrade = function(subject) {
     if(typeof this.grades[subject] === 'undefined') {
         // średnia dla wszystkich przedmiotów
-        const gradesAll = Object.values(this.grades); 
-        gradesAll.forEach(function(grade) {
-            sum += grade;
-            numberOfGrades = grade ++;
-        });
+        const gradesAll = this.grades;
+        // const gradesAll = Object.values(this.grades); 
+        this.avgGradesAll = function() {
+            gradesAll.forEach(function(grade) {
+                sum += grade;
+                numberOfGrades = grade ++;
+            })
         const avg = sum / numberOfGrades;
+        }
     } else {
         // średnia dla wybranego subject
-        const gradesSubject = Object.values(this.grades); 
-        gradesSubject.forEach(function(grade) {
+        // const gradesOneSubject = Object.values(this.grades); 
+        const gradesOneSubject = this.grades[subject];
+        gradesOneSubject.forEach(function(grade) {
             sum += grade;
             numberOfGrades = grade ++;
         });
         const avg = sum / numberOfGrades;
     }
 }
-const avg = getAvg('math');
+
+const avg = getAverageGrade();
 console.log(avg);
