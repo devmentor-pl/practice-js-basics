7d8efe7f1e00b0d8b7ab58741953580df7655b5c
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-02-22 22:33:39 +0100

    05 partly completed

--- 05/app.js
+++ 05/app.js
@@ -15,10 +15,10 @@ student.grades.biology = [];
 
 // METODY
 // dodawanie ocen do przedmiotów
-student.grades.math.push(5, 6, 5, 5, 4);
+student.grades.math.push(5, 6, 5, 4);
 student.grades.polish.push(4, 6, 5, 3, 4);
-student.grades.english.push(5, 6, 6, 6, 3);
-student.grades.biology.push(5, 6, 4, 4, 2);
+student.grades.english.push(5, 6, 3);
+student.grades.biology.push(5, 6, 4, 4, 2, 5);
 
 // dodawanie przedmiotów przy uzyciu funkcji
 Person.prototype.addScore = function(subject, score) {
@@ -30,60 +30,83 @@ Person.prototype.addScore = function(subject, score) {
 student.addScore('physics', 4);
 console.log(student);
 
-// obliczanie średniej arytmetycznej / BŁĄD funkcja getAverageGrade gnie jest zdefiniowana
-// wydzielenie ocen [tablice]
-const gradesKeys = Object.keys(student.grades);
+// obliczanie średniej arytmetycznej
 const gradesValues = Object.values(student.grades);
-const gradesFive = student.grades; // 5 tablic
-const gradesOne = student.grades['math']; // 1 tablica 'math'
-console.log(gradesKeys, gradesValues, gradesFive, gradesOne);
+// console.log(gradesValues);
 
-// prawidłowo wylicza sumę ocen z matematyki
+// wyliczamy średnią arytmetyczną z dowolnego przedmiotu
+// .forEach() DZIAŁA
 let sum = 0;
-let numberOfGrades = 0;
+let gradesNum = 0;
 
-const gradesMath = student.grades['math'];
-gradesMath.forEach(function(grade) {
-    sum += grade;
-    numberOfGrades ++;
-})
-const avgMath = sum / numberOfGrades;
-console.log(avgMath);
+Person.prototype.getAverageGrade = function(subject) {
+    const subjectGrades = this.grades[subject];
+    subjectGrades.forEach(function(grade) {
+        sum += grade;
+        gradesNum ++;
+    })
+    const avgSub = sum / gradesNum;
+    console.log(avgSub);
+}
 
+const cos = student.getAverageGrade('math');
 
-// Person.prototype.getAverageGrade = function(subject) {
-    
-//     if(typeof this.grades[subject] === 'undefined') {
-        // średnia dla wszystkich przedmiotów
-        // const gradesAll = Object.values(this.grades); 
+// PĘTLA for DZIAŁA
+let sum2 = 0;
+let gradeNum2 = 0;
+
+Person.prototype.getAverageGrade2 = function(subject) {
+    for(let i = 0; i < this.grades[subject].length; i ++) {
+        let grades = this.grades[subject][i];
+        sum2 += grades;
+        gradeNum2 ++;
+    }
+    return sum2 / gradeNum2;
+}
+
+avgSub2 = student.getAverageGrade2('math');
+// console.log(avgSub2);
+
+// wyliczamy średnią arytmetyczną ze wszystkich przedmiotów
+// .forEach() DZIAŁA
+let sumAll1 = 0;
+let count = 0;
+Person.prototype.getAverageGrade2 = function(subject) {
+    if(typeof this.grades[subject] === 'undefined') {
         
-        // const gradesMath = student.grades['math'];
-        // gradesMath.forEach(function(grade) {
-        //     sum += grade;
-        //     numberOfGrades ++;
-        //     const avgMath = sum / numberOfGrades;
-        //     console.log(avgMath);
-        // })
+        let gradeNumAll = Object.values(this.grades);
+        gradeNumAll.forEach(function(gradeSub1) {
+            gradeSub1.forEach(function(grade) {
+                sumAll1 += grade;
+                count ++;
+            })
+        })
+        if(count === 0) {
+            return 0;
+        }
+        return sumAll1 / count;
+    }
+}
 
-        // const gradesAll = this.grades;
-        // this.avgGradesAll = function() {
-        //     gradesAll.forEach(function(grade) {
-        //         sum += grade;
-        //         numberOfGrades = grade ++;
-        //     })
-        // const avg = sum / numberOfGrades;
-    //     }
-    // } else {
-        // średnia dla wybranego subject
-        // const gradesOneSubject = Object.values(this.grades); 
-//         const gradesOneSubject = this.grades[subject];
-//         gradesOneSubject.forEach(function(grade) {
-//             sum += grade;
-//             numberOfGrades = grade ++;
-//         });
-//         const avg = sum / numberOfGrades;
-//     }
-// }
+avgAll1 = student.getAverageGrade2();
+console.log(avgAll1.toFixed(1));
 
-// const avg = getAverageGrade();
-// console.log(avg);
+
+// PĘTLA for NIE DZIAŁA
+let sumAll2 = 0;
+let count2 = 0;
+Person.prototype.getAverageGrade2 = function(subject) {
+    if(typeof this.grades[subject] === 'undefined') {
+    for(let i = 0; i < this.grades.length; i ++) {
+        for(let j = 0; j < this.grades[j].length; j ++) {
+            let grades = this.grades[j];
+            console.log(this.grades[j])
+            sumAll2 += grades;
+            count2 ++;
+        }
+    }
+    return sumAll2 / count2;
+}
+}
+let avgSubAll = student.getAverageGrade2();
+console.log(avgSubAll);
