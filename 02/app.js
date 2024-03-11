d4aeda6d48304ca3f9d8f25a6d8aec603935fc5a
Jaro <jaroswidnik@interia.pl>
2024-03-11 13:53:25 +0100

    Added some line of code in ex 2 in js basics

--- 02/app.js
+++ 02/app.js
@@ -1,17 +1,20 @@
 
 /* rozwiązanie z pętlą for */
-const x = 4;
+
+
+const x = prompt('Podaj liczbę!');
 const arrOfNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-const arrOfResults = [];
-
-for (let i=0; i < arrOfNumbers.length; i++) {
-    let result = arrOfNumbers[i] * x;
-    let mathOperation = `${x} x ${arrOfNumbers[i]}`
-    
-    console.log(mathOperation, " = ", result);
+if (x > 0 && x <= 9) {
+    for (let i=0; i < arrOfNumbers.length; i++) {
+        let result = arrOfNumbers[i] * x;
+        let mathOperation = `${x} x ${arrOfNumbers[i]}`
+        
+        console.log(mathOperation, " = ", result);
+    }
+} else {
+    alert('Musisz wpisać liczbę z zakresu od 1 do 9!')
 }
-
 
 
 /* rozwiązanie z pętlą while  */
