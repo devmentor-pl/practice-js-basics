bc3fac424c111f32548b231a486e7e81018a945b
Jaro <jaroswidnik@interia.pl>
2024-03-11 14:31:25 +0100

    Added some lines of code to second part of task 2 in js basics

--- 02/app.js
+++ 02/app.js
@@ -2,19 +2,33 @@
 /* rozwiązanie z pętlą for */
 
 
-const x = prompt('Podaj liczbę!');
-const arrOfNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const x = prompt('Podaj liczbę!');
+// const arrOfNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-if (x > 0 && x <= 9) {
-    for (let i=0; i < arrOfNumbers.length; i++) {
-        let result = arrOfNumbers[i] * x;
-        let mathOperation = `${x} x ${arrOfNumbers[i]}`
+// if (x > 0 && x <= 9) {
+//     for (let i=0; i < arrOfNumbers.length; i++) {
+//         let result = arrOfNumbers[i] * x;
+//         let mathOperation = `${x} x ${arrOfNumbers[i]}`
         
-        console.log(mathOperation, " = ", result);
-    }
-} else {
-    alert('Musisz wpisać liczbę z zakresu od 1 do 9!')
-}
+//         console.log(mathOperation, " = ", result);
+//     }
+// } else {
+//     alert('Musisz wpisać liczbę z zakresu od 1 do 9!')
+// }
 
 
 /* rozwiązanie z pętlą while  */
+
+
+const a = 3; //podstawa
+const n = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+let i=0;
+
+while (i < n.length) {
+    const result2 = Math.pow(a,(n[i]))
+    // console.log(result2);
+    const mathOperation2 = `${a}^${n[i]} = ${result2}`;
+    console.log(mathOperation2);
+    i++
+}
