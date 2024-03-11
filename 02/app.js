944a0044cb9e69410a318ea8471123e10e59a716
Jaro <jaroswidnik@interia.pl>
2024-03-11 13:43:48 +0100

    Made 1st part of task 2 in js basics

--- 02/app.js
+++ 02/app.js
@@ -1,6 +1,16 @@
 
 /* rozwiązanie z pętlą for */
 const x = 4;
+const arrOfNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const arrOfResults = [];
+
+for (let i=0; i < arrOfNumbers.length; i++) {
+    let result = arrOfNumbers[i] * x;
+    let mathOperation = `${x} x ${arrOfNumbers[i]}`
+    
+    console.log(mathOperation, " = ", result);
+}
 
 
 
