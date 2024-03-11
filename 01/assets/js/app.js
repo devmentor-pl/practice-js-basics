53ef61b44777b1c5532ab8bb9b779a1b4df033db
Jaro <jaroswidnik@interia.pl>
2024-03-11 12:36:25 +0100

    Made 1st thing in ex 1 in JS

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -1,4 +1,30 @@
 const a = '4.2';
 const b = 9;
 
-console.log(a, b);
+console.log(typeof a);
+console.log(typeof b);
+
+
+// DODAWANIE
+const sum = parseFloat(a) + b;
+console.log(sum);
+
+// ODEJMOWANIE
+const substraction = parseFloat(a) - b;
+console.log(substraction);
+
+// MNOŻENIE
+const multiply = parseFloat(a) * b;
+console.log(multiply);
+
+// DZIELENIE
+const division = parseFloat(a) / b;
+console.log(division);
+
+// RESZTA Z DZIELENIA
+const mod = parseFloat(a) % b;
+console.log(mod);
+
+
+
+
