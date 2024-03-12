741a21764fb34428c7af469d7f52ebcdcf2ccc0f
Jaro <jaroswidnik@interia.pl>
2024-03-12 09:51:24 +0100

    Progress in ex 2 in js basics

--- 02/app.js
+++ 02/app.js
@@ -21,14 +21,28 @@
 
 
 const a = 3; //podstawa
-const n = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const n = 4; //wykładnik
 
 let i=0;
+let result2 = 1;
+let mathOperation2 = '';
 
-while (i < n.length) {
-    const result2 = Math.pow(a,(n[i]))
-    // console.log(result2);
-    const mathOperation2 = `${a}^${n[i]} = ${result2}`;
-    console.log(mathOperation2);
-    i++
+while (i < n) {
+
+    if (i > 0) {
+        result2 = result2 * a;
+        mathOperation2 = mathOperation2 + ' * ' + a;
+        i++;
+    } else {
+        result2 = result2 * a;
+        mathOperation2 = mathOperation2 + a;
+        i++;
+    }
+
 }
+// console.log(result2);
+console.log(mathOperation2, " = ", result2);
+
+
+
+
