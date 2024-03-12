6bd4e8b9b5d1384bc7fd050880369cf7cf65d99e
Jaro <jaroswidnik@interia.pl>
2024-03-12 11:14:27 +0100

    Finished 1st part of task 3 in js basics

--- 03/app.js
+++ 03/app.js
@@ -5,9 +5,34 @@ const a = randomNumber(min, max);
 const b = randomNumber(min, max);
 const c = randomNumber(min, max);
 
-console.log(a, b, c);
 
+function getSum(a, b, c) {
+    
+    a = a ?? 0;
+    b = b ?? 0;
+    c = c ?? 0;
 
+    let biggest = Math.max(a, b, c);
+    
+
+    if(a === biggest) {
+        a = -Infinity;
+    } else if (b === biggest) {
+        b = -Infinity;
+    } else {
+        c = -Infinity;
+    }
+
+    let secondBiggest = Math.max(a, b, c);
+
+    console.log(biggest, secondBiggest);    
+
+    return biggest + secondBiggest;
+
+}
+
+const sum = getSum(a, b, c);
+console.log("Suma dwój największych liczb:", sum);
 
 
 
