8c6dd339082e832619f3d4f9b60d372ad4141f12
Jaro <jaroswidnik@interia.pl>
2024-03-13 13:44:29 +0100

    Changes in ex 5 in js basics

--- 05/app.js
+++ 05/app.js
@@ -1,9 +1,26 @@
-function Student(initFirstName, initLastName, initGrades) {
+
+
+
+
+function Student(initFirstName, initLastName) {
     this.firstName = initFirstName;
     this.lastName = initLastName;
-    this.grades = initGrades;
+    this.grades = {};
 }
 
+
+
+
+Student.prototype.addGrade = function(subject, grade) {
+    if(typeof this.grades[subject] === 'undefined') {
+        this.grades[subject] = [];
+    }
+    this.grades[subject].push(grade);
+}
+
+
+
 const student1 = new Student('Bartosz', 'Zagaja', '1');
+student1.addGrade("Maths", 5);
 
 console.log(student1);
