5ff901ca2ae2262ad7c0a71f7413869f4c8ad5ac
Jaro <jaroswidnik@interia.pl>
2024-03-13 12:36:51 +0100

    Finished 2nd part of ex 4 in js basics

--- 04/app.js
+++ 04/app.js
@@ -11,7 +11,18 @@ for (let i=0; i < 20; i++) {
     arr.push(randomNumber(min, max));
 }
 
-console.log(arr);
+console.log("Tablica 20 losowych liczb", arr);
+
+arr.sort(function(a, b) {
+    return b - a;
+});
+
+console.log("Posortowana tablica od największej do namniejszej", arr);
+
+const arr2 = arr.slice(0, 10);
+
+console.log("10 największych wartości tablicy", arr2);
+
 
 
 
