bb7a379af007169f186f795e6cb6b507d15a363f
MarcinMorgan <morgangzit@gmail.com>
2024-03-21 13:16:28 +0100

    feat: t1 arithmetic operations on a and b

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -6,3 +6,22 @@ console.log('Zmienna a jest typu: '+typeof(a));
 console.log('Zmienna b jest typu: '+typeof(b));
 
 const aF = parseFloat(a);
+let tempNumber = 0;
+
+console.log(a+' + '+b+' = '+(aF+b));
+console.log(a+' - '+b+' = '+(aF-b));
+console.log(a+' * '+b+' = '+(aF*b));
+console.log(a+' / '+b+' = '+(aF/b));
+console.log(a+' % '+b+' = '+(aF%b));
+tempNumber = aF;
+tempNumber = ++tempNumber;
+console.log('++'+a+' = '+tempNumber);
+tempNumber = b;
+tempNumber = ++tempNumber;
+console.log('++'+b+' = '+tempNumber);
+tempNumber = aF;
+tempNumber = --tempNumber;
+console.log('--'+a+' = '+tempNumber);
+tempNumber = b;
+tempNumber = --tempNumber;
+console.log('--'+b+' = '+tempNumber);
