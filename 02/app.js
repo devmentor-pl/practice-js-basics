5d654052a0b905fb75727492665107fde1bc6be1
MarcinMorgan <morgangzit@gmail.com>
2024-03-21 18:06:19 +0100

    feat: t2 multiplication table for loop

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,10 @@
 
 /* rozwiązanie z pętlą for */
-const x = 4;
+const x = prompt('Podaj liczbę!');
 
-
+for(i=0;i<9;i++){
+    const multiplying = x * (i+1);
+    console.log(x+' * '+(i+1)+' = '+multiplying);
+}
 
 /* rozwiązanie z pętlą while  */
