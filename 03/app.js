8a572010708b87afdeccaf2548b4bdee20efb95e
Grzegorz Stepien <grzesiek94stepien@gmail.com>
2024-04-16 00:44:07 +0200

    finish 03

--- 03/app.js
+++ 03/app.js
@@ -7,9 +7,52 @@ const c = randomNumber(min, max);
 
 console.log(a, b, c);
 
+function getSum(x, y, z) {
+    const xInt = parseInt(x);
+    const yInt = parseInt(y);
+    const zInt = parseInt(z);
 
 
+    const arr = [xInt, yInt, zInt]
+    arr.sort(function (a, b) {
+        return b - a
+    })
 
+    return arr[0] + arr[1];
+}
+
+// const sum = getSum(a, b, c);
+// console.log(sum);
+
+const isEven = function (num) {
+    if (typeof num !== 'number') {
+        return null;
+    }
+
+    return num % 2 === 0;
+
+}
+
+// console.log(isEven(2), isEven(1), isEven('fsfasdasd'))
+
+function showInfo(value, isEven) {
+    switch (isEven) {
+        case null:
+            console.log('Podany argument ' + value + ' nie jest liczbą');
+            break;
+        case true:
+            console.log('Podany argument ' + value + ' jest parzysty');
+            break;
+        case false:
+            console.log('Podany argument ' + value + ' jest nieparzysty');
+    };
+}
+
+const sum = getSum(a, b, c);
+const even = isEven(sum);
+
+
+showInfo(sum, even);
 
 
 
