78483ec8bf4d0fbd0541c95d510279b33c0adc58
Sebastian Pietrzykowski <sebpie84@gmail.com>
2024-05-16 06:47:54 +0200

    execisese no 1 and 4

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,27 @@
-
 /* rozwiązanie z pętlą for */
-const x = 4;
 
+// const x = prompt('Podaj liczbe');
+// const y = prompt('podaj druga liczbe')
 
+// function result() {
+
+// 	for (let i = 0; i < x; i++) {
+//         console.log(result.i);
+// 		return x * y;
+// 	}
+// }
+// const sum = result();
+// console.log(alert(`Twoj wynik ${x} x ${y} = ${sum}`));
+
+// const x = 6
+
+function result(x) {
+	for (let i = 1; i <= 9; i++) {
+		console.log(`${x} x ${i} = ${x * i}`);
+	}
+}
+result(4);
+result(6);
+// console.log(`Twoj wynik ${x} x ${y} = ${result}`);
 
 /* rozwiązanie z pętlą while  */
