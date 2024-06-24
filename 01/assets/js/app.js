b6e871db9f6e383cedb7b6fdecbc24b3d2d68a3e
zero_jeden <zeroitjeden@gmail.com>
2024-06-24 22:49:48 +0200

    clean & save

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -8,8 +8,8 @@ console.log(typeof b);
 
 const a2 = parseFloat(a);
 
-const add = a2 + b; 
-const sub = a2 - b; 
+const add = a2 + b;
+const sub = a2 - b;
 const multi = a2 * b;
 const divi = a2 / b;
 
@@ -21,26 +21,25 @@ console.log(divi);
 console.log(a, a2, b);
 
 if (add > 20) {
-    console.log('add jest większe od 20')
+	console.log('add jest większe od 20');
 } else {
-    console.log('add nie jest większe od 20')
+	console.log('add nie jest większe od 20');
 }
 
 if (sub > 20) {
-    console.log('sub  jest większe od 20')
-} else { 
-    console.log('sub  nie jemultist większe od 20')
+	console.log('sub  jest większe od 20');
+} else {
+	console.log('sub  nie jemultist większe od 20');
 }
 
 if (multi > 20) {
-    console.log('multi jest większe od 20')
+	console.log('multi jest większe od 20');
 } else {
-    console.log('multi nie jest większe od 20')
+	console.log('multi nie jest większe od 20');
 }
 
 if (divi > 20) {
-    console.log('divi  jest większe od 20')
-} else { 
-    console.log('divi  nie jest większe od 20')
+	console.log('divi  jest większe od 20');
+} else {
+	console.log('divi  nie jest większe od 20');
 }
-
