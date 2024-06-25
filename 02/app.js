23036d8a4c0fcac472fe1d27b372916fb3b950a6
zero_jeden <zeroitjeden@gmail.com>
2024-06-25 23:50:47 +0200

    save & clean

--- 02/app.js
+++ 02/app.js
@@ -1,8 +1,7 @@
-
 /* rozwiązanie z pętlą for */
 const x = prompt('podaj liczbę z przedziału 1-9 wariacie:');
 let result = 0;
-console.log(`mnozymy liczbę ${x} przez liczby 1-9:`)
+console.log(`mnozymy liczbę ${x} przez liczby 1-9:`);
 if (x > 0 && x <= 9) {
 	for (let i = 1; i <= 9; i++) {
 		result = x * i;
@@ -18,17 +17,16 @@ const a = prompt('podaj liczbę:');
 const n = prompt('podaj n:');
 let i = 0;
 let pow = 1;
-let powPrint='';
+let powPrint = '';
 
 while (i < n) {
-    pow *= a;
+	pow *= a;
 
-    if (i > 0){
-        powPrint += ' * ';
-    }
-    powPrint += a;
-    i++;
-    
+	if (i > 0) {
+		powPrint += ' * ';
+	}
+	powPrint += a;
+	i++;
 }
-console.log(`podnosimy liczbę ${a} do potęgi ${n}:`)
+console.log(`podnosimy liczbę ${a} do potęgi ${n}:`);
 console.log(`${powPrint} = ${pow}`);
