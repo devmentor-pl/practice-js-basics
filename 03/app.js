3f151d9f920c89bfea247015e7773c9934f674a1
zero_jeden <zeroitjeden@gmail.com>
2024-07-04 00:36:26 +0200

    feat: solution 03 (getSum)

--- 03/app.js
+++ 03/app.js
@@ -7,12 +7,20 @@ const c = randomNumber(min, max);
 
 console.log(a, b, c);
 
+function getSum(a, b, c) {
+	const aInt = parseInt(a);
+	const bInt = parseInt(b);
+	const cInt = parseInt(c);
 
+	const arrDescending = [aInt, bInt, cInt];
+	arrDescending.sort((a, b) => b - a);
+	console.log(arrDescending);
+	return (result = arrDescending[0] + arrDescending[1]);
+}
 
-
-
-
+getSum(a, b, c);
+console.log(result);
 
 function randomNumber(min, max) {
-    return Math.round((Math.random() * (max - min)) + min);
+	return Math.round(Math.random() * (max - min) + min);
 }
