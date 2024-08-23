bdbc4056bf09aa5481e6bc60a15bc10b3e5d8b01
PatrickStojek <patryk.stojek7@gmail.com>
2024-08-23 23:24:07 +0200

    task 2 completed

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,34 @@
 
 /* rozwiązanie z pętlą for */
-const x = 4;
-
-
+const x = prompt("Podaj liczbę!")
+for(let i = 1; i < 10; i++) {
+    console.log(x + '*' + i +  '=' + x * i)
+}
 
 /* rozwiązanie z pętlą while  */
+
+
+function exponentiation() {
+    const a = parseFloat(prompt('podaj podstawę potęgi!'))
+    const n = parseFloat(prompt('podaj wykładnik potęgi!'))
+
+    if(isNaN(a) || isNaN(n)) {
+        console.log('wrong input format')
+        return;
+    }
+    let counter = 1
+    let output = ''
+    while(counter <= n) {
+        if(counter == 1) {
+            output += a
+        } else if(counter > 1 && counter !== n){
+            output += '*' + a
+        } else {
+            output += '*' + a + '=' + a**n
+        }
+        counter++
+    }
+    console.log(output)
+}
+
+exponentiation()
