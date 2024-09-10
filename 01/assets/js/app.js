c9eec5a85bcd593cf71852c170c5382d2205207d
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-09-10 16:35:18 +0200

    Added arithmetic operations on variables and conditional statements to check which result is greater or smaller than 20

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -1,4 +1,39 @@
-const a = '4.2';
+const a = +'4.2';
 const b = 9;
 
-console.log(a, b);
+console.log(typeof a);
+console.log(typeof b);
+
+const result = +a + b;
+console.log(result);
+const result1 = a - b;
+console.log(result1);
+const result2 = a * b;
+console.log(result2);
+const result3 = a / b;
+console.log(result3);
+
+if( result > 20){
+    console.log(`Wynik dodawania ${result} jest większy niż 20`);
+}else{
+    console.log(`Wynik dodawania ${result} jest mniejszy niż 20`);
+};
+
+if( result1 > 20){
+    console.log(`Wynik dodawania ${result1} jest większy niż 20`);
+}else{
+    console.log(`Wynik dodawania ${result1} jest mniejszy niż 20`);
+};
+
+if( result2 > 20){
+    console.log(`Wynik dodawania ${result2} jest większy niż 20`);
+}else{
+    console.log(`Wynik dodawania ${result2} jest mniejszy niż 20`);
+};
+
+if( result3 > 20){
+    console.log(`Wynik dodawania ${result3} jest większy niż 20`);
+}else{
+    console.log(`Wynik dodawania ${result3} jest mniejszy niż 20`);
+};
+
