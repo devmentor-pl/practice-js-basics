fba2221e23a7f48d83a4f55d72ac0d8538307aeb
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-09-15 16:10:44 +0200

    solutions added to tasks

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,30 @@
 
 /* rozwiązanie z pętlą for */
-const x = 4;
+const x = prompt("Podaj liczbe!");
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+if (x <= 10) {
+    for (const number of numbers) {
+        let result = x * number
+        console.log(`${x} * ${number} = ${result}`);
+    }
+} else {
+    alert("Twoja liczba musi mieścić sie w przedziale 1 - 9")
+}
 
 
+// /* rozwiązanie z pętlą while  */
 
-/* rozwiązanie z pętlą while  */
+let a = Number(prompt("podaj liczbę (a) - podstawe"));
+let n = Number(prompt("podaj liczbę (b) - wykładnik"));
+let result = a ** n;
+
+if (a === Number && b === Number) {
+    while (!isNaN(a) && !isNaN(n) && n > 0) {
+        console.log(`${Array(n).fill(a).join(` * `)} = ${result}`)
+        break;
+    };
+}else{
+    alert("Prosze podaj liczby nie litery!")
+}
+
