12bf86b33d773b7d0812b957f9d76c6d45bdd0a5
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-09-16 17:44:51 +0200

    solutions added to 3

--- 03/app.js
+++ 03/app.js
@@ -5,14 +5,42 @@ const a = randomNumber(min, max);
 const b = randomNumber(min, max);
 const c = randomNumber(min, max);
 
-console.log(a, b, c);
-
-
-
-
-
-
-
 function randomNumber(min, max) {
     return Math.round((Math.random() * (max - min)) + min);
+};
+
+const getSum = (a, b, c) => {
+    const numbers = [parseInt(a),parseInt(b),parseInt(c)];
+    const max1 = Math.max(...numbers);
+    const remaning = [a,b,c].filter(number => number !== max1);
+    const max2 = Math.max(...remaning);
+    const result = max1 + max2;
+    return result;
+};
+
+const isEven = (result) => {
+    if (typeof result !== "number"){
+        return null;
+    }return result % 2 === 0;
 }
+
+const showInfo = (value,result) =>{
+    switch(result){
+        case null:
+            console.log(`Podany argument [${value}] nie jest liczbą`);
+            break;
+        case true:
+            console.log(`Podany argument [${value}] jest liczbą parzystą`);
+            break;
+        case false:
+            console.log(`Podany argument [${value}] jest liczbą nieparzystą`)
+            break;
+        default:
+            console.log(`Nieznana Liczba`);
+    }
+}
+
+const sum = getSum(a,b,c);
+const evenResult = isEven(sum);
+
+showInfo(sum,evenResult);
