3f5f437a0f97ebcc3250c972aae684b9394ffc16
Marek Bełz <belzmarek@gmail.com>
2024-11-01 22:30:15 +0000

    Done

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -1,4 +1,27 @@
 const a = '4.2';
 const b = 9;
 
-console.log(a, b);
+
+console.log(typeof a);
+console.log(typeof b);
+
+const numberA = parseFloat(a);  
+
+const results = [
+    {name: "sumy", result: numberA + b},
+    {name: "różnicy", result: numberA - b},
+    {name: "iloczynu", result: numberA * b},
+    {name: "ilorazu", result: numberA / b},
+    {name: "modulo", result: numberA % b},
+    {name: "potęgi", result: numberA ** b},
+]
+
+results.forEach(item => {
+    if(item.result > 20) {
+        console.log('wynik ' + item.name + ' to ' + item.result + ' i jest większy niż dwadzieścia');
+    } else {
+        console.log('wynik ' + item.name + ' to ' + item.result + ' i jest mniejszy niż dwadzieścia');
+    }
+});
+
+
