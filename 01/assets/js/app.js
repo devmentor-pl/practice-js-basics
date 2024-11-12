5413e93bb8e25d0cc4c3cf7a7f935dcdb20d5849
Piotr <amanitus@gmail.com>
2024-11-12 10:37:14 +0100

    Część projektu

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -2,3 +2,59 @@ const a = '4.2';
 const b = 9;
 
 console.log(a, b);
+
+
+
+console.log(typeof a);  
+console.log(typeof b); 
+
+const sum = Number(a) + b;        
+const difference = Number(a) - b; 
+const multiplication = Number(a) * b;    
+const division = Number(a) / b; 
+const modulo = Number(a) % b; 
+const exponentiation = Math.pow(Number(a), b); 
+
+
+console.log("Suma: " + sum);           
+console.log("Różnica: " + difference); 
+console.log("Iloczyn: " + multiplication);   
+console.log("Iloraz: " + division);   
+console.log("Reszta: " + modulo);  
+console.log("Potęga: " + exponentiation); 
+
+if (sum > 20) {
+    console.log('Suma jest większa niż 20');
+} else {
+    console.log('Suma jest mniejsza lub równa 20');
+}
+
+if (difference > 20) {
+    console.log('Różnica jest większa niż 20');
+} else {
+    console.log('Różnica jest mniejsza lub równa 20');
+}
+
+if (multiplication > 20) {
+    console.log('Iloczyn jest większy niż 20');
+} else {
+    console.log('Iloczyn jest mniejszy lub równy 20');
+}
+
+if (division > 20) {
+    console.log('Iloraz jest większy niż 20');
+} else {
+    console.log('Iloraz jest mniejszy lub równy 20');
+}
+
+if (modulo > 20) {
+    console.log('Reszta jest większa niż 20');
+} else {
+    console.log('Reszta jest mniejsza lub równa 20');
+}
+
+if (exponentiation > 20) {
+    console.log('Potęga jest większa niż 20');
+} else {
+    console.log('Potęga jest mniejsza lub równa 20');
+}
--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,25 @@
+const x = prompt('Podaj liczbę');
 
-/* rozwiązanie z pętlą for */
-const x = 4;
-
-
+if (x >= 1 && x <= 9) {
+    
+    for (let i = 1; i <= 9; i++) {
+        console.log(x + " x " + i + " = " + (x * i));
+    }
+} else {
+    console.log('Podana liczba jest poza zakresem! Proszę podać liczbę od 1 do 9.');
+}
 
 /* rozwiązanie z pętlą while  */
+
+let a = prompt("Podaj podstawę");
+let n = prompt("Podaj wykładnik");
+
+let result = 1;
+
+let i = 0;
+
+while (i < n) {
+    result = result * a;
+}
+
+console.log(a + " do potęgi " + n + " to " + result);
