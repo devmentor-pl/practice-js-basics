a6eeafd51fecebe994fb2602325dd3d835736feb
Piotr <amanitus@gmail.com>
2024-11-18 13:18:42 +0100

    zadania

--- 02/app.js
+++ 02/app.js
@@ -1,13 +1,13 @@
-const x = prompt('Podaj liczbę');
+// const x = prompt('Podaj liczbę');
 
-if (x >= 1 && x <= 9) {
+// if (x >= 1 && x <= 9) {
     
-    for (let i = 1; i <= 9; i++) {
-        console.log(x + " x " + i + " = " + (x * i));
-    }
-} else {
-    console.log('Podana liczba jest poza zakresem! Proszę podać liczbę od 1 do 9.');
-}
+//     for (let i = 1; i <= 9; i++) {
+//         console.log(x + " x " + i + " = " + (x * i));
+//     }
+// } else {
+//     console.log('Podana liczba jest poza zakresem! Proszę podać liczbę od 1 do 9.');
+// }
 
 /* rozwiązanie z pętlą while  */
 
@@ -17,9 +17,16 @@ let n = prompt("Podaj wykładnik");
 let result = 1;
 
 let i = 0;
-
+let str='';
 while (i < n) {
     result = result * a;
+    i=i+1;
+    if (i===1) {
+        str=str+a
+    }
+    else {
+        str=str+'*'+a
+    }
 }
-
-console.log(a + " do potęgi " + n + " to " + result);
+console.log(str);
+console.log(str+'='+result);
--- 03/app.js
+++ 03/app.js
@@ -7,8 +7,8 @@ const c = randomNumber(min, max);
 
 console.log(a, b, c);
 
-
-
+const result = getSum(a,b,c);
+console.log(result);
 
 
 
@@ -16,3 +16,39 @@ console.log(a, b, c);
 function randomNumber(min, max) {
     return Math.round((Math.random() * (max - min)) + min);
 }
+function getSum(x,y,z) {
+    const intX=parseInt(x);
+    const intY=parseInt(y);
+    const intZ=parseInt(z);
+    const arr=[intX, intZ, intY];
+    arr.sort(function(a,b){
+        return a-b;
+    })
+    console.log(arr)
+    return arr[1]+arr[2];
+}
+const isEven = function(number) {
+
+    if (typeof number !== 'number') {
+        return null;
+    }
+    
+    return number % 2 === 0;
+};
+function showInfo(value, itResult) {
+    switch (itResult) {
+    
+            case null:
+                console.log("Podany argument " + value + " nie jest liczbą");
+                break;
+            case true:
+                console.log("Podany argument " + value + " jest parzysty");
+                break;
+            case false:
+                console.log("Podany argument " + value + " jest nieparzysty");
+                break;
+    }
+}
+const itResult = isEven(result);
+
+showInfo(result, itResult);
