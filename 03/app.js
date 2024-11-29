c9a81d7f68ebb107f39c13492dbbf5d0b9060a54
marrcelp <marcel.piaszczyk@gmail.com>
2024-11-29 16:21:06 +0100

    resolved exercise 3

--- 03/app.js
+++ 03/app.js
@@ -7,12 +7,61 @@ const c = randomNumber(min, max);
 
 console.log(a, b, c);
 
-
-
-
-
-
-
 function randomNumber(min, max) {
     return Math.round((Math.random() * (max - min)) + min);
 }
+
+function getSum(a, b, c) {
+
+    if (a > b && b > c){
+        return parseInt(a) + parseInt(b);
+
+    } else if (a > b && c > b){
+        return parseInt(a) + parseInt(c);
+
+    } else if (a < b && a < c){
+        return parseInt(b) + parseInt(c);
+
+    } else if (a < b && a >c){
+        return parseInt(a) + parseInt(b);
+    }
+}
+
+
+const isEven = function(number){
+
+    if (typeof(number) !== 'number'){
+        return null;
+    }
+
+    if(number%2 === 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function showInfo(value, boolean){
+
+    switch(value, boolean){
+        case null:
+            console.log(`Podany argument ${value} nie jest liczbą`);
+            break;
+
+        case true:
+            console.log(`Podany argument ${value} jest parzysty`);
+            break;
+
+        case false:
+            console.log(`Podany argument ${value} jest nieparzysty`);
+            break;
+    }
+}
+
+const sum = getSum(a, b, c);
+console.log(sum);
+
+const isSumEven = isEven(sum);
+console.log(isSumEven);
+
+showInfo(sum, isSumEven);
