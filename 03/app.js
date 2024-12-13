c8309af71d77d923944acb244bdec0757e47a6c9
Weronika Jankowska <wjankowska8@gmail.com>
2024-12-13 22:33:14 +0100

    task 3

--- 03/app.js
+++ 03/app.js
@@ -1,18 +1,43 @@
-const min = 1;
-const max = 100;
+function getSum(a, b, c) {
+    const aInt = parseInt(a);
+    const bInt = parseInt(b);
+    const cInt = parseInt(c);
 
-const a = randomNumber(min, max);
-const b = randomNumber(min, max);
-const c = randomNumber(min, max);
+    const arr = [aInt, bInt, cInt];
+    arr.sort(function (a, b) {
+        return b - a;
+    });
+    return arr[0] + arr[1];
+}
 
-console.log(a, b, c);
+function isEven(num) {
+    if (typeof num !== 'number') {
+        return null;
+    }
+    return num % 2 === 0;
+}
 
+console.log(isEven(2), isEven(1), isEven('r'));
 
+function showInfo(value, isEven) {
+    switch (isEven) {
+        case null:
+            console.log(`${value} nie jest liczbą`);
 
+            break;
+        case true:
+            console.log(`Liczba ${value} jest parzysta`);
 
+            break;
+        case false:
+            console.log(`Liczba ${value} jest nieparzysta`);
+            break;
+    }
+}
 
-
-
+const sum = getSum(a, b, c);
+const even = isEven(sum);
+showInfo(sum, even);
 function randomNumber(min, max) {
-    return Math.round((Math.random() * (max - min)) + min);
+    return Math.round(Math.random() * (max - min) + min);
 }
