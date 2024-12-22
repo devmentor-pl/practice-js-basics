406f4e6244f5938b668d4491c2664f779ee38c78
Bartosz Dudziak <bartoszdudziak.dev@gmail.com>
2024-12-22 13:21:27 +0100

    Task 02 completed

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,33 @@
-
 /* rozwiązanie z pętlą for */
-const x = 4;
+const x = prompt("Type number: ");
 
-
+if (x) {
+  for (let i = 1; i <= 9; i++) {
+    console.log(`${x} x ${i} = ${Number(x) * i}`);
+  }
+}
 
 /* rozwiązanie z pętlą while  */
+let a;
+let n;
+
+let counter = 0;
+let text = "";
+
+while (!a) {
+  a = Number(prompt("Podstawa: "));
+
+  while (!n || n <= 1) {
+    n = Number(prompt("Wykladnik:"));
+
+    while (counter < n) {
+      text += a;
+
+      if (counter !== n - 1) text += " * ";
+      else text = text + " = " + Math.pow(a, n);
+
+      counter++;
+    }
+  }
+}
+console.log(text);
