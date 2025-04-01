5aa97dac04ecb54b7487f3008a9ff653af8d2233
Daria Seńko <twoj@email>
2025-04-01 18:35:43 +0200

    zad 5

---
+++ 05/index.js
@@ -0,0 +1,44 @@
+function Student(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.grades = {};
+}
+
+Student.prototype.addGrade = function (subject, grade) {
+  if (typeof this.grades[subject] === 'undefined') {
+    this.grades[subject] = [];
+  }
+  this.grades[subject].push(grade);
+};
+
+Student.prototype.getAverageGrade = function (subject) {
+  if (typeof subject === 'undefined') {
+    const grades = [];
+    for (const key in this.grades) {
+      const arr = this.grades[key];
+
+      arr.forEach(function (item) {
+        grades.push(item);
+      });
+    }
+
+    if (grades.length === 0) {
+      return 0;
+    }
+
+    const sum = grades.reduce(function (acc, curr) {
+      return acc + curr;
+    }, 0);
+    return sum / grades.length;
+  }
+
+  if (typeof this.grades[subject] === 'undefined') {
+    return 0;
+  }
+
+  const grades = this.grades[subject];
+  const sum = grades.reduce(function (acc, curr) {
+    return acc + curr;
+  }, 0);
+  return sum / grades.length;
+};
