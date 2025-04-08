4494edc9295018663b46d3773efa6a7290590bba
Zaneta Sochon <twj email>
2025-04-08 14:01:56 +0200

    feat(2): Resolve task with loops

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,30 @@
-
 /* rozwiązanie z pętlą for */
-const x = 4;
+// const x = prompt('Podaj liczbę!');
 
+// for (let i = 1; i <= 9; i++) {
+//   const result = x * i;
 
+//   if (x > 0 && x < 10) {
+//     console.log(`${x} x ${i} = ${result}`);
+//   } else {
+//     console.log('Liczba nie mieści się w przedziale od 1 do 9');
+//   }
+// }
 
 /* rozwiązanie z pętlą while  */
+
+let a = 3; //podstawa
+let n = 4; //wykładnik
+
+let counter = 0;
+let result = 1;
+let displayString = '';
+
+while (counter < n) {
+  counter++;
+  result = result * a;
+
+  displayString = `${displayString} * ${a}`;
+}
+
+console.log(`${displayString} = ${result}`);
