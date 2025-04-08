d9a02bfe6b993a16404700757a3a995695d80561
Gabriel Macierzyński <gabrysm97@gmail.com>
2025-04-08 17:04:41 +0200

    solution to task 2

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,35 @@
 
 /* rozwiązanie z pętlą for */
-const x = 4;
+const x = prompt('Podaj liczbę!');
+if(x >= 1 && x <= 9){
+    for(let i = 1; i < 10; i++){
+        const res = x * i;
+        console.log(x + " * " + i + " = " +res); 
+    }
+}
+else{
+    console.log("Przekroczyłeś przedział! ");
+}
 
 
 
 /* rozwiązanie z pętlą while  */
+
+const a = prompt("Wpisz podstawę: ");
+let n = prompt("Wpisz wykładnik: ");
+let res = 1;
+let res1 = "";
+
+while(n > 0){
+    res *= a;
+    if(n === 1){
+        res1 += a + " = " + res;
+    }
+    else{
+        res1 += a + " * ";
+    }
+    console.log(res)
+    
+    n--;
+}
+console.log(res1)
