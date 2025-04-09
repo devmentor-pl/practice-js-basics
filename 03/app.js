43057eedaf3c4825dd9538defcf575bd3eec76a2
Gabriel Macierzyński <gabrysm97@gmail.com>
2025-04-09 20:01:59 +0200

    solution to task 3

--- 03/app.js
+++ 03/app.js
@@ -7,12 +7,51 @@ const c = randomNumber(min, max);
 
 console.log(a, b, c);
 
+const sum = getSum(a,b,c)
 
+const isEven = function(num){
+    if(typeof num !== "number"){
+        return null;
+    }else{
+        if(num % 2 === 0){
+            return true
+        }
+        else{
+            return false
+        }
+    }
+}
+const even = isEven(sum);
 
+showInfo(sum, even)
 
-
-
+function showInfo(par1, par2){
+    switch(par2){
+        case null:
+            console.log("Podany argument " + par1 + " nie jest liczbą");
+            break;
+        case true:
+            console.log("Podany argument " + par1 + " jest parzysty");
+            break;
+        case false:
+            console.log("Podany argument " + par1 + " jest nieparzysty");
+            break;
+        default: alert("Błędne dane")
+                
+    }
+}
 
 function randomNumber(min, max) {
     return Math.round((Math.random() * (max - min)) + min);
+}
+
+function getSum(num1, num2, num3){
+    const n1 = parseInt(num1);
+    const n2 = parseInt(num2);
+    const n3 = parseInt(num3);
+    
+    const numbers = [n1, n2, n3];
+    numbers.sort((a,b) => b - a);
+ 
+    return numbers[0] + numbers[1];
 }
